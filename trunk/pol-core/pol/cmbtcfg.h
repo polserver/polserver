adf8e939c300edc7f644193a2097431c8d169b71
muaddib_pol <muaddib_pol@11a62ffb-1e35-46b8-8505-d88fde283c85>
2009-09-04 03:18:29 +0000

    + Up to date Packets Zip. + Moved combat related ssopt entries to combat.cfg

--- trunk/pol-core/pol/cmbtcfg.h
+++ trunk/pol-core/pol/cmbtcfg.h
@@ -2,6 +2,7 @@
 History
 =======
 2005/23/11 MuadDib: Added warmode_wait timer for changing war mode.
+2009/09/03 MuadDib:   Moved combat related settings to Combat Config from SSOPT
 
 Notes
 =======
@@ -13,6 +14,12 @@ struct CombatConfig {
     bool warmode_inhibits_regen;
 	bool attack_self;
 	unsigned long warmode_delay;
+
+	bool core_hit_sounds;
+	bool scripted_attack_checks;
+	bool reset_swing_onturn;
+	bool send_swing_packet;
+
 };
 
 extern CombatConfig combat_config;
