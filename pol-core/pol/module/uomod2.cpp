c69f31b3e51d26051b3f703afdea78e932335b46
Turley <turley@polserver.com>
2009-09-12 13:25:56 +0000

    added isUOKR check to gump send

--- pol-core/pol/module/uomod2.cpp
+++ pol-core/pol/module/uomod2.cpp
@@ -776,7 +776,7 @@ BObjectImp* UOExecutorModule::mf_SendGumpMenu( )
 		return new BError( "Client already has an active gump" );
 	}
 */
-	if ((!(flags & SENDDIALOGMENU_FORCE_OLD)) && (chr->client->compareVersion(CLIENT_VER_50000)))
+	if ((chr->client->isUOKR) || ((!(flags & SENDDIALOGMENU_FORCE_OLD)) && (chr->client->compareVersion(CLIENT_VER_50000))))
 		return internal_SendCompressedGumpMenu(chr, layout_arr, data_arr, x, y);
 	else
 		return internal_SendUnCompressedGumpMenu(chr, layout_arr, data_arr, x, y);
