f25b1bc7238fd5263fe65ab0bcf9b497bf8dd637
turley <turleypol@users.noreply.github.com>
2009-12-15 13:16:25 +0000

    crypt... should work now for every client - Tomi

--- pol-core/pol/crypt/blowfish.cpp
+++ pol-core/pol/crypt/blowfish.cpp
@@ -1,47 +1,6 @@
-//////////////////////////////////////////////////////////////////////
-//
-// crypt/blowfish.cpp
-//
-// Author:    Beosil <beosil@swileys.com>
-// Date:      18. Apr. 2000
-//
-// Converted: Myrathi <tjh@myrathi.co.uk>
-// Date:      03. Jan. 2003
-//
-// Versions:  <= 2.0.0
-//
-// Copyright (C) 1999-2000 Bruno 'Beosil' Heidelberger
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-//
-// History:
-// - 03. Jan. 2003 : Updated for use alongside POL's multi-encryption code.
-// - 18. Apr. 2000 : Keys updated for client 2.0.0
-// - 27. Jan. 2000 : Keys updated for client 1.26.4
-// - 18. Jan. 2000 : Keys updated for client 1.26.3
-// - 23. Nov. 1999 : Keys updated for client 1.26.2 and some compatibility fixes
-// - 21. Sep. 1999 : Full redesign to fix the "21036 bytes"-bug
-// -  9. Sep. 1999 : Keys updated for client 1.26.1
-// -  2. Sep. 1999 : Keys and boxes updated for client 1.26.0b and minor bugfixes
-// - 13. Aug. 1999 : First release, working with client 1.26.0(a)
-//
-//////////////////////////////////////////////////////////////////////
-
 #include <memory.h>
 #include "blowfish.h"
+#include "cryptbase.h"
 
 // Crypt Boxes
 
@@ -50,15 +9,6 @@ static unsigned long p_box[18] =
 	0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344,	0xa4093822, 0x299f31d0, 0x082efa98, 0xec4e6c89,
 	0x452821e6, 0x38d01377, 0xbe5466cf, 0x34e90c6c,	0xc0ac29b7, 0xc97c50dd, 0x3f84d5b5, 0xb5470917,
 	0x9216d5d9, 0x8979fb1b
-};
-
-static unsigned char b_box[80] =
-{
-	0x45, 0x6e, 0x63, 0x72, 0x79, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x2d, 0x6d, 0x6f, 0x64, 0x75, 0x6c, 
-	0x65, 0x20, 0x63, 0x6F, 0x64, 0x65, 0x64, 0x20, 0x62, 0x79, 0x20, 0x42, 0x65, 0x6f, 0x73, 0x69, 
-	0x6c, 0x2e, 0x20, 0x54, 0x68, 0x69, 0x73, 0x20, 0x63, 0x6f, 0x64, 0x65, 0x20, 0x6d, 0x61, 0x79, 
-	0x20, 0x6f, 0x6e, 0x6c, 0x79, 0x20, 0x62, 0x65, 0x20, 0x75, 0x73, 0x65, 0x64, 0x20, 0x69, 0x6e, 
-	0x20, 0x65, 0x6d, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x73, 0x2e, 0x3d, 0x5e, 0x29, 0xff, 0xff
 };
 
 static unsigned long s_box[1024] = 
@@ -192,12 +142,6 @@ static unsigned long s_box[1024] =
 	0x85cbfe4e, 0x8ae88dd8, 0x7aaaf9b0, 0x4cf9aa7e,	0x1948c25c, 0x02fb8a8c, 0x01c36ae4, 0xd6ebe1f9,
 	0x90d4f869, 0xa65cdea0, 0x3f09252d, 0xc208e69f,	0xb74e6132, 0xce77e25b, 0x578fdfe3, 0x3ac372e6
 };
-
-// Crypt Tables
-
-static unsigned int p_table[CRYPT_GAMEKEY_COUNT][18];
-
-static unsigned int s_table[CRYPT_GAMEKEY_COUNT][1024];
 
 // Key Table
 
@@ -337,185 +281,81 @@ static unsigned char seed_table[2][CRYPT_GAMESEED_COUNT][2][CRYPT_GAMESEED_LENGT
 	0x22, 0x59, 0x30, 0x3B, 0xE4, 0x5F, 0x43, 0x1E
 };
 
-// Compression Table
+static unsigned int p_table[CRYPT_GAMEKEY_COUNT][18];
+static unsigned int s_table[CRYPT_GAMEKEY_COUNT][1024];
 
-static unsigned int bit_table[257][2] =
-{
-	0x02, 0x00, 	0x05, 0x1F, 	0x06, 0x22, 	0x07, 0x34, 	0x07, 0x75, 	0x06, 0x28, 	0x06, 0x3B, 	0x07, 0x32, 
-	0x08, 0xE0, 	0x08, 0x62, 	0x07, 0x56, 	0x08, 0x79, 	0x09, 0x19D,	0x08, 0x97, 	0x06, 0x2A, 	0x07, 0x57, 
-	0x08, 0x71, 	0x08, 0x5B, 	0x09, 0x1CC,	0x08, 0xA7, 	0x07, 0x25, 	0x07, 0x4F, 	0x08, 0x66, 	0x08, 0x7D, 
-	0x09, 0x191,	0x09, 0x1CE, 	0x07, 0x3F, 	0x09, 0x90, 	0x08, 0x59, 	0x08, 0x7B, 	0x08, 0x91, 	0x08, 0xC6, 
-	0x06, 0x2D, 	0x09, 0x186, 	0x08, 0x6F, 	0x09, 0x93, 	0x0A, 0x1CC,	0x08, 0x5A, 	0x0A, 0x1AE,	0x0A, 0x1C0, 
-	0x09, 0x148,	0x09, 0x14A, 	0x09, 0x82, 	0x0A, 0x19F, 	0x09, 0x171,	0x09, 0x120, 	0x09, 0xE7, 	0x0A, 0x1F3, 
-	0x09, 0x14B,	0x09, 0x100,	0x09, 0x190,	0x06, 0x13, 	0x09, 0x161,	0x09, 0x125,	0x09, 0x133,	0x09, 0x195, 
-	0x09, 0x173,	0x09, 0x1CA,	0x09, 0x86, 	0x09, 0x1E9, 	0x09, 0xDB, 	0x09, 0x1EC,	0x09, 0x8B, 	0x09, 0x85, 
-	0x05, 0x0A, 	0x08, 0x96, 	0x08, 0x9C, 	0x09, 0x1C3, 	0x09, 0x19C,	0x09, 0x8F, 	0x09, 0x18F,	0x09, 0x91, 
-	0x09, 0x87, 	0x09, 0xC6, 	0x09, 0x177,	0x09, 0x89, 	0x09, 0xD6, 	0x09, 0x8C, 	0x09, 0x1EE,	0x09, 0x1EB, 
-	0x09, 0x84, 	0x09, 0x164, 	0x09, 0x175,	0x09, 0x1CD, 	0x08, 0x5E, 	0x09, 0x88, 	0x09, 0x12B,	0x09, 0x172, 
-	0x09, 0x10A,	0x09, 0x8D, 	0x09, 0x13A,	0x09, 0x11C, 	0x0A, 0x1E1,	0x0A, 0x1E0, 	0x09, 0x187,	0x0A, 0x1DC, 
-	0x0A, 0x1DF,	0x07, 0x74, 	0x09, 0x19F,	0x08, 0x8D,		0x08, 0xE4, 	0x07, 0x79, 	0x09, 0xEA, 	0x09, 0xE1, 
-	0x08, 0x40, 	0x07, 0x41, 	0x09, 0x10B,	0x09, 0xB0, 	0x08, 0x6A, 	0x08, 0xC1, 	0x07, 0x71, 	0x07, 0x78, 
-	0x08, 0xB1, 	0x09, 0x14C, 	0x07, 0x43, 	0x08, 0x76, 	0x07, 0x66, 	0x07, 0x4D, 	0x09, 0x8A, 	0x06, 0x2F, 
-	0x08, 0xC9,		0x09, 0xCE, 	0x09, 0x149,	0x09, 0x160, 	0x0A, 0x1BA, 	0x0A, 0x19E, 	0x0A, 0x39F, 	0x09, 0xE5, 
-	0x09, 0x194, 	0x09, 0x184, 	0x09, 0x126, 	0x07, 0x30, 	0x08, 0x6C, 	0x09, 0x121, 	0x09, 0x1E8, 	0x0A, 0x1C1, 
-	0x0A, 0x11D, 	0x0A, 0x163, 	0x0A, 0x385, 	0x0A, 0x3DB, 	0x0A, 0x17D, 	0x0A, 0x106, 	0x0A, 0x397, 	0x0A, 0x24E, 
-	0x07, 0x2E, 	0x08, 0x98, 	0x0A, 0x33C, 	0x0A, 0x32E, 	0x0A, 0x1E9, 	0x09, 0xBF, 	0x0A, 0x3DF, 	0x0A, 0x1DD, 
-	0x0A, 0x32D, 	0x0A, 0x2ED, 	0x0A, 0x30B, 	0x0A, 0x107, 	0x0A, 0x2E8, 	0x0A, 0x3DE, 	0x0A, 0x125, 	0x0A, 0x1E8, 
-	0x09, 0xE9, 	0x0A, 0x1CD, 	0x0A, 0x1B5, 	0x09, 0x165, 	0x0A, 0x232, 	0x0A, 0x2E1, 	0x0B, 0x3AE, 	0x0B, 0x3C6, 
-	0x0B, 0x3E2, 	0x0A, 0x205, 	0x0A, 0x29A, 	0x0A, 0x248, 	0x0A, 0x2CD, 	0x0A, 0x23B, 	0x0B, 0x3C5, 	0x0A, 0x251, 
-	0x0A, 0x2E9, 	0x0A, 0x252, 	0x09, 0x1EA, 	0x0B, 0x3A0, 	0x0B, 0x391, 	0x0A, 0x23C, 	0x0B, 0x392, 	0x0B, 0x3D5, 
-	0x0A, 0x233, 	0x0A, 0x2CC, 	0x0B, 0x390, 	0x0A, 0x1BB, 	0x0B, 0x3A1, 	0x0B, 0x3C4, 	0x0A, 0x211, 	0x0A, 0x203, 
-	0x09, 0x12A, 	0x0A, 0x231, 	0x0B, 0x3E0, 	0x0A, 0x29B, 	0x0B, 0x3D7, 	0x0A, 0x202, 	0x0B, 0x3AD, 	0x0A, 0x213, 
-	0x0A, 0x253, 	0x0A, 0x32C, 	0x0A, 0x23D, 	0x0A, 0x23F, 	0x0A, 0x32F, 	0x0A, 0x11C, 	0x0A, 0x384, 	0x0A, 0x31C, 
-	0x0A, 0x17C, 	0x0A, 0x30A, 	0x0A, 0x2E0, 	0x0A, 0x276, 	0x0A, 0x250, 	0x0B, 0x3E3, 	0x0A, 0x396, 	0x0A, 0x18F, 
-	0x0A, 0x204, 	0x0A, 0x206, 	0x0A, 0x230, 	0x0A, 0x265, 	0x0A, 0x212, 	0x0A, 0x23E, 	0x0B, 0x3AC, 	0x0B, 0x393, 
-	0x0B, 0x3E1, 	0x0A, 0x1DE, 	0x0B, 0x3D6, 	0x0A, 0x31D, 	0x0B, 0x3E5, 	0x0B, 0x3E4, 	0x0A, 0x207, 	0x0B, 0x3C7, 
-	0x0A, 0x277, 	0x0B, 0x3D4, 	0x08, 0xC0,		0x0A, 0x162, 	0x0A, 0x3DA, 	0x0A, 0x124, 	0x0A, 0x1B4, 	0x0A, 0x264, 
-	0x0A, 0x33D, 	0x0A, 0x1D1, 	0x0A, 0x1AF, 	0x0A, 0x39E, 	0x0A, 0x24F, 	0x0B, 0x373, 	0x0A, 0x249, 	0x0B, 0x372, 
-	0x09, 0x167, 	0x0A, 0x210, 	0x0A, 0x23A, 	0x0A, 0x1B8, 	0x0B, 0x3AF, 	0x0A, 0x18E, 	0x0A, 0x2EC, 	0x07, 0x62, 
-	0x04, 0x0D
-};
-
-// Macro Definitions ( to avoid big-/little-endian problems )
-
-#define N2L(C, LL) LL  = ((unsigned int)(*((C)++))) << 24, LL |= ((unsigned int)(*((C)++))) << 16, LL |= ((unsigned int)(*((C)++))) << 8, LL |= ((unsigned int)(*((C)++)))
-#define L2N(LL, C) *((C)++) = (unsigned char)(((LL) >> 24) & 0xff), *((C)++) = (unsigned char)(((LL) >> 16) & 0xff), *((C)++) = (unsigned char)(((LL) >> 8) & 0xff), *((C)++) = (unsigned char)(((LL)) & 0xff)
-#define ROUND(LL, R, S, P) LL ^= P; LL ^= ((S[(R >> 24)]  + S[0x0100 + ((R >> 16) & 0xff)]) ^ S[0x0200 + ((R >> 8) & 0xff)]) + S[0x0300 + ((R) & 0xff)]
-
-// Class Variables Initialization
-
-//needed?
-//bool CCryptBlowfish::m_bTablesReady = false;
+bool BlowFish::tables_ready = false;
 
 // Constructor / Destructor
-
-CCryptBlowfish::CCryptBlowfish()
+BlowFish::BlowFish()
+{
+}
+BlowFish::~BlowFish()
 {
 }
 
-CCryptBlowfish::~CCryptBlowfish()
+// Public Member Functions
+
+void BlowFish::Init()
 {
+	if(!tables_ready)
+		InitTables();
+
+	table_index = CRYPT_GAMETABLE_START;
+	memcpy(game_seed, seed_table[0][table_index][0], CRYPT_GAMESEED_LENGTH);
+	stream_pos = 0;
+	block_pos = 0;
 }
 
-CCryptBlowfish::CCryptBlowfish(unsigned int masterKey1, unsigned int masterKey2)
+void BlowFish::Decrypt( unsigned char * in, unsigned char * out, int len )
 {
-	SetMasterKeys(masterKey1,masterKey2);
-}
-
-// Member Functions
-
-int CCryptBlowfish::Receive(void *buffer, int max_expected, SOCKET socket)
-{
-	int count = recv(socket, (char *) encrypted_data, max_expected, 0);
-	if (count > 0)
+	if(stream_pos + len > CRYPT_GAMETABLE_TRIGGER)
 	{
-		passert( count <= max_expected );
-		Decrypt( encrypted_data, buffer, count );
+		int len_rem = CRYPT_GAMETABLE_TRIGGER - stream_pos;
+
+		Decrypt(in, out, len_rem);
+
+		table_index = (table_index + CRYPT_GAMETABLE_STEP) % CRYPT_GAMETABLE_MODULO;
+		memcpy(game_seed, seed_table[1][table_index][0], CRYPT_GAMESEED_LENGTH);
+		stream_pos = 0;
+		block_pos = 0;
+
+		in += len_rem;
+		out += len_rem;
+		len -= len_rem;
 	}
-	return count;
-}
-
-void CCryptBlowfish::SetMasterKeys(unsigned int masterKey1, unsigned int masterKey2)
-{
-	m_masterKey[0] = masterKey1 & 0xFFFFFFFF;
-	m_masterKey[1] = masterKey2 & 0xFFFFFFFF;
-}
-
-void CCryptBlowfish::Decrypt(void *pvIn, void *pvOut, int len)
-{
-	unsigned char *pIn = (unsigned char *)pvIn;
-	unsigned char *pOut = (unsigned char *)pvOut;
-
-	if(m_type == CCryptBase::typeAuto)
+	for(int tempPos = 0; tempPos < len; tempPos++)
 	{
-		if(((*pIn ^ (unsigned char)m_loginKey[0])) == CRYPT_AUTO_VALUE) m_type = CCryptBase::typeLogin;
-		else m_type = CCryptBase::typeGame;
-	}
-
-	if(m_type == CCryptBase::typeLogin)
-	{
-		int tempPos;
-		for(tempPos = 0; tempPos < len; tempPos++)
+		if(!block_pos)
 		{
-			pOut[tempPos] = pIn[tempPos] ^ (unsigned char)m_loginKey[0];
+			unsigned int values[2];
 
-			unsigned int table[2];
-			table[0] = m_loginKey[0];
-			table[1] = m_loginKey[1];
+			unsigned char *pKey = game_seed;
+			N2L(pKey, values[0]);
+			N2L(pKey, values[1]);
 
-			m_loginKey[1] = (((((table[1] >> 1) | (table[0] << 31)) ^ m_masterKey[0]) >> 1) | (table[0] << 31)) ^ m_masterKey[0];
-			m_loginKey[0] = ((table[0] >> 1) | (table[1] << 31)) ^ m_masterKey[1];
-		}
-	}
-	else
-	{
-		if(m_gameStreamPos + len > CRYPT_GAMETABLE_TRIGGER)
-		{
-			int lenOld = CRYPT_GAMETABLE_TRIGGER - m_gameStreamPos;
+			RawDecrypt((unsigned int *)values, table_index);
 
-			Decrypt(pIn, pOut, lenOld);
-
-			m_gameTable = (m_gameTable + CRYPT_GAMETABLE_STEP) % CRYPT_GAMETABLE_MODULO;
-			memcpy(m_gameSeed, seed_table[1][m_gameTable][0], CRYPT_GAMESEED_LENGTH);
-			m_gameStreamPos = 0;
-			m_gameBlockPos = 0;
-
-			pIn += lenOld;
-			pOut += lenOld;
-			len -= lenOld;
+			pKey = game_seed;
+			L2N(values[0], pKey);
+			L2N(values[1], pKey);
 		}
 
-		int tempPos;
-		for(tempPos = 0; tempPos < len; tempPos++)
-		{
-			if(!m_gameBlockPos)
-			{
-				unsigned int values[2];
+		unsigned char c = *in++;
+		*out++ = game_seed[block_pos] ^ c;
+		game_seed[block_pos] = c;
 
-				unsigned char *pKey = m_gameSeed;
-				N2L(pKey, values[0]);
-				N2L(pKey, values[1]);
-
-				RawDecrypt((unsigned int *)values, m_gameTable);
-
-				pKey = m_gameSeed;
-				L2N(values[0], pKey);
-				L2N(values[1], pKey);
-			}
-
-			unsigned char c = *pIn++;
-			*pOut++ = m_gameSeed[m_gameBlockPos] ^ c;
-			m_gameSeed[m_gameBlockPos] = c;
-
-			m_gameBlockPos = (m_gameBlockPos + 1) & 0x07;
-		}
-
-		m_gameStreamPos += len;
+		block_pos = (block_pos + 1) & 0x07;
 	}
+
+	stream_pos += len;
 }
 
-void CCryptBlowfish::Init(void *pvSeed, int type)
+// Protected Member Functions
+
+void BlowFish::InitTables()
 {
-	unsigned char *pSeed = (unsigned char *)pvSeed;
-	N2L(pSeed, m_loginSeed);
-
-	m_loginKey[0] = (((~m_loginSeed) ^ 0x00001357) << 16) | ((m_loginSeed ^ 0xffffaaaa) & 0x0000ffff);
-	m_loginKey[1] = ((m_loginSeed ^ 0x43210000) >> 16) | (((~m_loginSeed) ^ 0xabcdffff) & 0xffff0000);
-
-	if(!m_bTablesReady) InitTables();
-
-	m_gameTable = CRYPT_GAMETABLE_START;
-	memcpy(m_gameSeed, seed_table[0][m_gameTable][0], CRYPT_GAMESEED_LENGTH);
-	m_gameStreamPos = 0;
-	m_gameBlockPos = 0;
-
-	m_type = type;
-}
-
-void CCryptBlowfish::InitTables()
-{
-	int tempKey;
-	for(tempKey = 0; tempKey < CRYPT_GAMEKEY_COUNT; tempKey++)
+	for(int tempKey = 0; tempKey < CRYPT_GAMEKEY_COUNT; tempKey++)
 	{
 		memcpy(p_table[tempKey], p_box, sizeof(p_box));
 		memcpy(s_table[tempKey], s_box, sizeof(s_box));
@@ -523,8 +363,7 @@ void CCryptBlowfish::InitTables()
 		unsigned char *pAct = key_table[tempKey];
 		unsigned char *pEnd = key_table[tempKey + 1];
 
-		int i;
-		for(i = 0; i < 18; i++)
+		for(int i = 0; i < 18; i++)
 		{
 			unsigned int mask = *pAct++;		
 			if(pAct >= pEnd) pAct = key_table[tempKey];
@@ -544,14 +383,14 @@ void CCryptBlowfish::InitTables()
 		unsigned int value[2];
 		value[0] = value[1] = 0;
 
-		for(i = 0; i < 18; i += 2)
+		for(int i = 0; i < 18; i += 2)
 		{
 			RawDecrypt(value, tempKey);
 			p_table[tempKey][i] = value[0];
 			p_table[tempKey][i+1] = value[1];
 		}
 
-		for(i = 0; i < 1024; i += 2)
+		for(int i = 0; i < 1024; i += 2)
 		{
 			RawDecrypt(value, tempKey);
 			s_table[tempKey][i] = value[0];
@@ -559,62 +398,13 @@ void CCryptBlowfish::InitTables()
 		}
 	}
 
-	m_bTablesReady = true;
+	tables_ready = true;
 }
 
-int CCryptBlowfish::Pack(void *pvIn, void *pvOut, int len)
+void BlowFish::RawDecrypt( unsigned int * values, int table )
 {
-	unsigned char *pIn = (unsigned char *)pvIn;
-	unsigned char *pOut = (unsigned char *)pvOut;
-
-	int actByte = 0;
-	int bitByte = 0;
-	int nrBits;
-	unsigned int value;
-
-	while(len--)
-	{
-		nrBits = bit_table[*pIn][0];
-		value = bit_table[*pIn++][1];
-
-		while(nrBits--)
-		{
-			pOut[actByte] = (pOut[actByte] << 1) | (unsigned char)((value >> nrBits) & 0x1);
-
-			bitByte = (bitByte + 1) & 0x07;
-			if(!bitByte) actByte++;
-		}
-	}
-
-	nrBits = bit_table[256][0];
-	value = bit_table[256][1];
-
-	while(nrBits--)
-	{
-		pOut[actByte] = (pOut[actByte] << 1) | (unsigned char)((value >> nrBits) & 0x1);
-
-		bitByte = (bitByte + 1) & 0x07;
-		if(!bitByte) actByte++;
-	}
-
-	if(bitByte)
-	{
-		while(bitByte < 8)
-		{
-			pOut[actByte] <<= 1;
-			bitByte++;
-		}
-
-		actByte++;
-	}
-
-	return actByte;
-}
-
-void CCryptBlowfish::RawDecrypt(unsigned int *pValues, int table)
-{
-	unsigned int left = pValues[0];
-	unsigned int right = pValues[1];
+	unsigned int left = values[0];
+	unsigned int right = values[1];
 
 	left ^= p_table[table][0];
 
@@ -624,6 +414,6 @@ void CCryptBlowfish::RawDecrypt(unsigned int *pValues, int table)
 	}
 	right ^= p_table[table][17];
 
-	pValues[1] = left;
-	pValues[0] = right;
+	values[1] = left;
+	values[0] = right;
 }
