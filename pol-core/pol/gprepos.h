b4c5d23797508f295a7280e29779a20a467b672a
turley <turley@polserver.com>
2010-01-06 07:46:53 +0000

    found no sense...commented out

--- pol-core/pol/gprepos.h
+++ pol-core/pol/gprepos.h
@@ -8,16 +8,16 @@ Notes
 
 */
 
-#ifndef GPREPOS_H
-#define GPREPOS_H
-
-#include <string>
-
-class GTerminalDef;
-class GFunctionDef;
-
-
-GTerminalDef* find_terminal( const std::string& name );
-GFunctionDef* find_function( const std::string& name );
-
-#endif
+//#ifndef GPREPOS_H
+//#define GPREPOS_H
+//
+//#include <string>
+//
+//class GTerminalDef;
+//class GFunctionDef;
+//
+//
+//GTerminalDef* find_terminal( const std::string& name );
+//GFunctionDef* find_function( const std::string& name );
+//
+//#endif
