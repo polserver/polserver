1a86a43063157c18ef14a2b9acfa70694e2f2543
turley <turleypol@users.noreply.github.com>
2010-01-15 12:28:41 +0000

    flipped writer

--- pol-core/clib/pkthelper.cpp
+++ pol-core/clib/pkthelper.cpp
@@ -47,3 +47,31 @@ void WritetoCharBuffer(char *buffer, s8 x, unsigned int *offset )
 {
 	buffer[(*offset)++] = x;
 }
+
+void WritetoCharBufferFlipped(char *buffer, u32 x, unsigned int *offset )
+{
+	buffer[(*offset)++] = (x >> CHAR_BIT*3) & UCHAR_MAX;
+	buffer[(*offset)++] = (x >> CHAR_BIT*2) & UCHAR_MAX;
+	buffer[(*offset)++] = (x >> CHAR_BIT) & UCHAR_MAX;
+	buffer[(*offset)++] = x & UCHAR_MAX;
+}
+
+void WritetoCharBufferFlipped(char *buffer, s32 x, unsigned int *offset )
+{
+	buffer[(*offset)++] = (x >> CHAR_BIT*3) & UCHAR_MAX;
+	buffer[(*offset)++] = (x >> CHAR_BIT*2) & UCHAR_MAX;
+	buffer[(*offset)++] = (x >> CHAR_BIT) & UCHAR_MAX;
+	buffer[(*offset)++] = x & UCHAR_MAX;
+}
+
+void WritetoCharBufferFlipped(char *buffer, u16 x, unsigned int *offset )
+{
+	buffer[(*offset)++] = (x >> CHAR_BIT) & UCHAR_MAX;
+	buffer[(*offset)++] = x & UCHAR_MAX;
+}
+
+void WritetoCharBufferFlipped(char *buffer, s16 x, unsigned int *offset )
+{
+	buffer[(*offset)++] = (x >> CHAR_BIT) & UCHAR_MAX;
+	buffer[(*offset)++] = x & UCHAR_MAX;
+}
