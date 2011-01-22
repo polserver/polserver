c99bd97e72946aa9563bd2ad20242d601a7c71d4
Turley <turley@polserver.com>
2011-01-22 15:25:39 +0000

    no exception will be thrown if region enter/leavescript doesnt exist

--- pol-core/clib/singleton.h
+++ pol-core/clib/singleton.h
@@ -21,19 +21,19 @@ class Singleton
 		{
 			if (!_instance)
 			{
-				singleton_mutex.lock(); //critical double check
+				_SingletonMutex.lock(); //critical double check
 				if (!_instance)
 				{
 					_instance = new T();
 					atexit(&Singleton<T>::_cleanup);
 				}
-				singleton_mutex.unlock();
+				_SingletonMutex.unlock();
 			}
 			return _instance;
 		}
 	private:
 		static T* _instance;
-		static Mutex singleton_mutex;
+		static Mutex _SingletonMutex;
 	protected:
 		Singleton() { }
 		~Singleton() { }
@@ -42,7 +42,7 @@ class Singleton
 		Singleton& operator=( Singleton& );
 };
 template <typename T> T* Singleton <T>::_instance = NULL;
-template <typename T> Mutex Singleton <T>::singleton_mutex;
+template <typename T> Mutex Singleton <T>::_SingletonMutex;
 
 #endif
 
