31885ee77d1c4b5ee7d4db2340b8dc34ce9a7480
turley <turley@polserver.com>
2011-02-16 10:15:04 +0000

    win2k fix for vs2010

--- lib/STLport5/build-msvc10-x64.bat
+++ lib/STLport5/build-msvc10-x64.bat
@@ -1,4 +1,6 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86_amd64
+set ProgRoot=%ProgramFiles%
+if not "%ProgramFiles(x86)%" == "" set ProgRoot=%ProgramFiles(x86)%
+call "%ProgRoot%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86_amd64
 call configure.bat msvc10x64
 call cd build\lib
 call nmake clean install
