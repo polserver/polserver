6a98a9ecac22f76385b4d2401742258da95cf154
AustinHeilman <AustinHeilman@gmail.com>
2011-09-01 03:23:28 +0000

    Quality can be edited with GetItemDescriptor()

--- pol-core/pol/item/equipmnt.cpp
+++ pol-core/pol/item/equipmnt.cpp
@@ -22,10 +22,13 @@ Notes
 Equipment::Equipment( const ItemDesc& itemdesc, UOBJ_CLASS uobj_class ) :
     Item( itemdesc, uobj_class ),
     eq_tmpl_(static_cast<const EquipDesc&>(find_itemdesc( objtype_ ))), // be sure to grab the permanent entry
-    quality_(1.0),
     hp_( eq_tmpl_.maxhp),
     maxhp_mod_(0)
 {
+	if ( itemdesc.quality )
+		quality_ = itemdesc.quality;
+	else
+		quality_ = 1.0;
 }
 
 Item* Equipment::clone() const
