42219dc3b4116469645780247fd2751a68ab69a5
AustinHeilman <AustinHeilman@11a62ffb-1e35-46b8-8505-d88fde283c85>
2011-09-01 03:23:28 +0000

    Quality can be edited with GetItemDescriptor()

--- trunk/pol-core/pol/item/itemdesc.h
+++ trunk/pol-core/pol/item/itemdesc.h
@@ -31,178 +31,179 @@ class ExportScript;
 
 struct ResourceComponent
 {
-    ResourceDef* rd;
-    unsigned amount;
+	ResourceDef* rd;
+	unsigned amount;
 
-    ResourceComponent( const std::string& rname, unsigned amount );
+	ResourceComponent( const std::string& rname, unsigned amount );
 };
 
 class ItemDesc
 {
 public:
-    enum Type { 
-        ITEMDESC, 
-        CONTAINERDESC, 
-        DOORDESC, 
-        WEAPONDESC, 
-        ARMORDESC, 
-        BOATDESC,
-        HOUSEDESC,
-        SPELLBOOKDESC,
-        SPELLSCROLLDESC,
-        MAPDESC
-    };
+	enum Type { 
+		ITEMDESC, 
+		CONTAINERDESC, 
+		DOORDESC, 
+		WEAPONDESC, 
+		ARMORDESC, 
+		BOATDESC,
+		HOUSEDESC,
+		SPELLBOOKDESC,
+		SPELLSCROLLDESC,
+		MAPDESC
+	};
 
-    static ItemDesc* create( ConfigElem& elem, const Package* pkg );
+	static ItemDesc* create( ConfigElem& elem, const Package* pkg );
 
-    ItemDesc( u32 objtype, ConfigElem& elem, Type type, const Package* pkg );
-    explicit ItemDesc( Type type );
-    virtual ~ItemDesc();
-    virtual void PopulateStruct( BStruct* descriptor ) const;
-    std::string objtype_description() const;
-    bool default_movable() const;
+	ItemDesc( u32 objtype, ConfigElem& elem, Type type, const Package* pkg );
+	explicit ItemDesc( Type type );
+	virtual ~ItemDesc();
+	virtual void PopulateStruct( BStruct* descriptor ) const;
+	std::string objtype_description() const;
+	bool default_movable() const;
 
-    Type type;
-    
-    const Package* pkg;
-    std::string objtypename;
+	Type type;
+	
+	const Package* pkg;
+	std::string objtypename;
 
-    u32 objtype;
-    u16 graphic;
-    u16 color;
-    //u16 weight;
-    u8 facing;
-    int weightmult;
-    int weightdiv;
-    std::string desc;
-    std::string tooltip;
-    ScriptDef walk_on_script;
-    ScriptDef on_use_script;
-    std::string equip_script;
-    std::string unequip_script;
-    ScriptDef control_script;
-    ScriptDef create_script;
-    ScriptDef destroy_script;
-    bool requires_attention;
-    bool lockable;
-    unsigned int vendor_sells_for;
-    unsigned int vendor_buys_for;
-    unsigned decay_time;
-    enum Movable { UNMOVABLE, MOVABLE, DEFAULT } movable;
-    unsigned short doubleclick_range;
+	u32 objtype;
+	u16 graphic;
+	u16 color;
+	//u16 weight;
+	u8 facing;
+	int weightmult;
+	int weightdiv;
+	std::string desc;
+	std::string tooltip;
+	ScriptDef walk_on_script;
+	ScriptDef on_use_script;
+	std::string equip_script;
+	std::string unequip_script;
+	ScriptDef control_script;
+	ScriptDef create_script;
+	ScriptDef destroy_script;
+	bool requires_attention;
+	bool lockable;
+	unsigned int vendor_sells_for;
+	unsigned int vendor_buys_for;
+	unsigned decay_time;
+	enum Movable { UNMOVABLE, MOVABLE, DEFAULT } movable;
+	unsigned short doubleclick_range;
 	bool use_requires_los; //DAVE 11/24
 	bool ghosts_can_use; //DAVE 11/24
-    bool can_use_while_paralyzed;
-    bool can_use_while_frozen;
-    bool newbie;
-    bool invisible;
-    bool decays_on_multis;
-    bool blocks_casting_if_in_hand;
-    unsigned short base_str_req;
+	bool can_use_while_paralyzed;
+	bool can_use_while_frozen;
+	bool newbie;
+	bool invisible;
+	bool decays_on_multis;
+	bool blocks_casting_if_in_hand;
+	unsigned short base_str_req;
 	unsigned short stack_limit;
+	double quality;
 
 	Dice resist_dice;
 
 	Resistances element_resist;
 	ElementDamages element_damage;
 
-    std::vector<ResourceComponent> resources;
+	std::vector<ResourceComponent> resources;
 
-    PropertyList props;
+	PropertyList props;
 	std::set<std::string> ignore_cprops;  //dave added 1/26/3
 
-    ExportScript* method_script;
+	ExportScript* method_script;
 
-    void unload_scripts();
+	void unload_scripts();
 };
 
 class ContainerDesc : public ItemDesc
 {
-    typedef ItemDesc base;
+	typedef ItemDesc base;
 public:
-    ContainerDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
-    virtual void PopulateStruct( BStruct* descriptor ) const;
+	ContainerDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
+	virtual void PopulateStruct( BStruct* descriptor ) const;
 
-    // string name;
-    // u16 objtype;
-    u16 gump;
-    u16 minx, maxx;
-    u16 miny, maxy;
+	// string name;
+	// u16 objtype;
+	u16 gump;
+	u16 minx, maxx;
+	u16 miny, maxy;
 
-    u16 max_weight;
-    u16 max_items;
+	u16 max_weight;
+	u16 max_items;
 
 	u8 max_slots;
 
-    ScriptDef can_insert_script;
-    ScriptDef on_insert_script;
-    ScriptDef can_remove_script;
-    ScriptDef on_remove_script;
+	ScriptDef can_insert_script;
+	ScriptDef on_insert_script;
+	ScriptDef can_remove_script;
+	ScriptDef on_remove_script;
 };
 
 class DoorDesc : public ItemDesc
 {
-    typedef ItemDesc base;
+	typedef ItemDesc base;
 public:
-    DoorDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
+	DoorDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
 	virtual void PopulateStruct( BStruct* descriptor ) const;
-    s16 xmod;
-    s16 ymod;
+	s16 xmod;
+	s16 ymod;
 };
 
 class SpellbookDesc : public ContainerDesc
 {
-    typedef ContainerDesc base;
+	typedef ContainerDesc base;
 public:
-    SpellbookDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
+	SpellbookDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
 	virtual void PopulateStruct( BStruct* descriptor ) const;
 
-    std::string spelltype;
+	std::string spelltype;
 };
 
 class SpellScrollDesc : public ItemDesc
 {
-    typedef ItemDesc base;
+	typedef ItemDesc base;
 public:
-    SpellScrollDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
+	SpellScrollDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
 	virtual void PopulateStruct( BStruct* descriptor ) const;
 
-    std::string spelltype;
+	std::string spelltype;
 };
 
 class MultiDesc : public ItemDesc
 {
-    typedef ItemDesc base;
+	typedef ItemDesc base;
 public:
-    MultiDesc( u32 objtype, ConfigElem& elem, Type type, const Package* pkg );
+	MultiDesc( u32 objtype, ConfigElem& elem, Type type, const Package* pkg );
 	virtual void PopulateStruct( BStruct* descriptor ) const;
-    u16 multiid;
+	u16 multiid;
 };
 
 class BoatDesc : public MultiDesc
 {
-    typedef MultiDesc base;
+	typedef MultiDesc base;
 public:
-    BoatDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
+	BoatDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
 	virtual void PopulateStruct( BStruct* descriptor ) const;
 };
 
 class HouseDesc : public MultiDesc
 {
-    typedef MultiDesc base;
+	typedef MultiDesc base;
 public:
-    HouseDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
+	HouseDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
 	virtual void PopulateStruct( BStruct* descriptor ) const;
 };
 
 class MapDesc : public ItemDesc
 {
-    typedef ItemDesc base;
+	typedef ItemDesc base;
 public:
-    MapDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
+	MapDesc( u32 objtype, ConfigElem& elem, const Package* pkg );
 	virtual void PopulateStruct( BStruct* descriptor ) const;
 
-    bool editable;
+	bool editable;
 };
 
 
@@ -220,7 +221,7 @@ extern bool has_walkon_script_[N_ITEM_DESC];
 
 inline bool has_walkon_script( u32 objtype )
 {
-    return has_walkon_script_[objtype];
+	return has_walkon_script_[objtype];
 }
 
 extern bool dont_save_itemtype[N_ITEM_DESC];
