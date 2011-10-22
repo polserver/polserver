deb8baf985ff4abc0352c9c992fc73eacae39a34
AustinHeilman <AustinHeilman@11a62ffb-1e35-46b8-8505-d88fde283c85>
2011-10-22 07:26:34 +0000

    Applied KnightRage's patches for errors found by Andenixa

--- trunk/pol-core/bscript/compctx.h
+++ trunk/pol-core/bscript/compctx.h
@@ -13,30 +13,30 @@ Notes
 class CompilerContext
 {
 public:
-    CompilerContext();
-    CompilerContext( const std::string& filename, int dbg_filenum, const char *s );
-    CompilerContext( const CompilerContext& );
-    CompilerContext& operator=(const CompilerContext&);
+	CompilerContext();
+	CompilerContext( const std::string& filename, int dbg_filenum, const char *s );
+	CompilerContext( const CompilerContext& );
+	CompilerContext& operator=(const CompilerContext&);
 
-    void printOn( std::ostream& os ) const;
-    void printOnShort( std::ostream& os ) const;
+	void printOn( std::ostream& os ) const;
+	void printOnShort( std::ostream& os ) const;
 
-    void skipws();
-    int skipcomments();
+	void skipws();
+	int skipcomments();
 
-    const char *s;
-    int line;
-    std::string filename;
+	const char *s;
+	int line;
+	std::string filename;
 
-    const char* s_begin;
+	const char* s_begin;
 
-    int dbg_filenum;
+	int dbg_filenum;
 };
 
 inline std::ostream& operator<<( std::ostream& os, const CompilerContext& ctx )
 {
-    ctx.printOn(os);
-    return os;
+	ctx.printOn(os);
+	return os;
 }
 
 #endif
