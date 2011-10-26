90dd7a39a801ca321ab631e8f9dd0aee5d2ba4a2
dsourc <tluume@gmail.com>
2011-10-26 19:36:04 +0000

    Added support for clients >= 7.0.16.0 char create packet 0xF8

--- pol-core/pol/network/client.h
+++ pol-core/pol/network/client.h
@@ -63,6 +63,7 @@ const u8 FLAG_GENDER = 0x01;
 const u8 FLAG_RACE   = 0x02;
 
 
+
 struct VersionDetailStruct
 {
 	int major;
@@ -79,6 +80,7 @@ const struct VersionDetailStruct CLIENT_VER_6017 ={6,0,1,7};
 const struct VersionDetailStruct CLIENT_VER_60142={6,0,14,2};
 const struct VersionDetailStruct CLIENT_VER_7000 ={7,0,0,0};
 const struct VersionDetailStruct CLIENT_VER_7090 ={7,0,9,0};
+const struct VersionDetailStruct CLIENT_VER_70160={7,0,16,0};
 
 enum ClientTypeFlag
 {
@@ -91,7 +93,8 @@ enum ClientTypeFlag
 	CLIENTTYPE_UOKR  = 0x40,
     CLIENTTYPE_7000  = 0x80, // 7.0.0.0  (Gargoyle race)
 	CLIENTTYPE_UOSA  = 0x100,
-	CLIENTTYPE_7090  = 0x200 // 7.0.9.0 (High Sea Adventures)
+	CLIENTTYPE_7090  = 0x200, // 7.0.9.0 (High Sea Adventures)
+	CLIENTTYPE_70160 = 0x400  // 7.0.16.0 (New Character Creation packet 0xF8)
 };
 
 typedef struct
