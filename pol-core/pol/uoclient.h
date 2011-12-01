2cee26deecb4c79c28c07ea02ca8bfc0fd59318c
muaddib_pol <muaddib_pol@11a62ffb-1e35-46b8-8505-d88fde283c85>
2011-12-01 14:47:41 +0000

    + UoClientGeneral.maxskills changed to unsigned short. + Added vcvars for VS2010.

--- pol-core/pol/uoclient.h
+++ pol-core/pol/uoclient.h
@@ -2,7 +2,7 @@
 History
 =======
 2010/02/03 Turley:    MethodScript support for mobiles
-
+2011/12/01 MuadDib:   Changed max_skills to unsigned short. 0xFF max in packets anyway. Probably never need to support over 255 skills anyway.
 
 Notes
 =======
@@ -39,7 +39,7 @@ public:
     Mapping hits;
     Mapping stamina;
     Mapping mana;
-	unsigned int maxskills; //dave changed 3/15/03, support configurable max skillid
+	unsigned short maxskills; //dave changed 3/15/03, support configurable max skillid
 	ExportScript* method_script;
 };
 
