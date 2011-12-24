dcbb0b9a3e924c003ba9f8907eae8a25b85091a4
turley <turley@polserver.com>
2011-12-24 09:46:30 +0000

    locale setting during startup makefile changes for runecl 64bit polversion string added

--- pol-core/plib/polver.cpp
+++ pol-core/plib/polver.cpp
@@ -38,19 +38,24 @@ unsigned int polver = 99;
 #define PROFILE_TAG ""
 #endif
 
+#if defined _WIN64 || defined _LP64
+#define POL_64 " - 64bit"
+#else
+#define POL_64 ""
+#endif
 
 #ifdef POL_BUILDTAG
-  const char polbuildtag[] = POL_BUILDTAG""PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG;
+  const char polbuildtag[] = POL_BUILDTAG""PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG""POL_64;
 #elif defined(_MSC_VER) && (_MSC_VER <= 1200)
-  const char polbuildtag[] = "VC6"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG;
+  const char polbuildtag[] = "VC6"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG""POL_64;
 #elif defined(_MSC_VER) && (_MSC_VER <= 1300)
-  const char polbuildtag[] = "VS.NET"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG;
+  const char polbuildtag[] = "VS.NET"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG""POL_64;
 #elif defined(_MSC_VER) && (_MSC_VER <= 1310)
-  const char polbuildtag[] = "VS.NET 2003"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG;
+  const char polbuildtag[] = "VS.NET 2003"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG""POL_64;
 #elif defined(_MSC_VER) && (_MSC_VER <= 1400)
-  const char polbuildtag[] = "VS.NET 2005"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG;
+  const char polbuildtag[] = "VS.NET 2005"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG""POL_64;
 #elif defined(_MSC_VER) && (_MSC_VER <= 1500)
-  const char polbuildtag[] = "VS.NET 2008"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG;
+  const char polbuildtag[] = "VS.NET 2008"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG""POL_64;
 #elif defined(_MSC_VER) && (_MSC_VER <= 1600)
-  const char polbuildtag[] = "VS.NET 2010"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG;
+  const char polbuildtag[] = "VS.NET 2010"PERGON_TAG""MEMORYLEAK_TAG""PROFILE_TAG""POL_64;
 #endif
