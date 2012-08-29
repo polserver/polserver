6c3cfcaa906aa4815d90d2842ad7436737ca9d6a
turley <turley@11a62ffb-1e35-46b8-8505-d88fde283c85>
2012-08-29 15:27:06 +0000

    Added batch files for building boost under windows

---
+++ trunk/lib/boost_1_51_0/buildboostmsvc9x86.bat
@@ -0,0 +1,14 @@
+set ProgRoot=%ProgramFiles%
+if not "%ProgramFiles(x86)%" == "" set ProgRoot=%ProgramFiles(x86)%
+call "%ProgRoot%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+
+call bootstrap
+
+ECHO using stlport : 5.2 : ../STLport5/stlport : ../STLport5/lib ; >> project-config.jam
+ECHO. >> project-config.jam
+
+bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-9.0 stdlib=stlport link=static runtime-link=static address-model=32 variant=release --abbreviate-paths --stagedir=lib\x86 --build-dir=build\x86 --build-type=complete stage
+bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-9.0 stdlib=stlport link=static runtime-link=static address-model=32 variant=debug define=_STLP_DEBUG --abbreviate-paths --stagedir=lib\x86 --build-dir=build\x86 --build-type=complete stage
+
+
+
