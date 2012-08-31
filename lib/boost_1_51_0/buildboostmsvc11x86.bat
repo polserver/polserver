76bcc9eaecc98d5ae8629b1eb9669b60a0fd87d9
turley <turley@polserver.com>
2012-08-31 09:23:04 +0000

    boost: build only multi threaded libs btw its save to delete the complete folder "build" after successful compilation

--- lib/boost_1_51_0/buildboostmsvc11x86.bat
+++ lib/boost_1_51_0/buildboostmsvc11x86.bat
@@ -4,5 +4,5 @@ call "%ProgRoot%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 
 call bootstrap
 
-bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-11.0 link=static runtime-link=static address-model=32 variant=release --abbreviate-paths --stagedir=lib\x86 --build-dir=build\x86 --build-type=complete stage
-bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-11.0 link=static runtime-link=static address-model=32 variant=debug define=_STLP_DEBUG --abbreviate-paths --stagedir=lib\x86 --build-dir=build\x86 --build-type=complete stage
+bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-11.0 link=static runtime-link=static address-model=32 variant=release --abbreviate-paths --stagedir=lib\x86 --build-dir=build\x86 stage
+bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-11.0 link=static runtime-link=static address-model=32 variant=debug --abbreviate-paths --stagedir=lib\x86 --build-dir=build\x86 stage
