63a828fc2db7b128c2aa1a0a94cb202a05440e04
turley <turley@11a62ffb-1e35-46b8-8505-d88fde283c85>
2013-01-05 08:40:11 +0000

    Happy new year

--- trunk/pol-core/pol/pol.cpp
+++ trunk/pol-core/pol/pol.cpp
@@ -1957,7 +1957,7 @@ int xmain_inner( int argc, char *argv[] )
 
 	cout << progverstr << " (" << polbuildtag << ")" << endl;
 	cout << "compiled on " << compiledate << " " << compiletime << endl;
-	cout << "Copyright (C) 1993-2012 Eric N. Swanson" << endl;
+	cout << "Copyright (C) 1993-2013 Eric N. Swanson" << endl;
 	cout << endl;
 
 	checkpoint( "opening logfiles" );
