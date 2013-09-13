b005c391ac54966ac22474d601b6ed066338ddcc
turley <turley@polserver.com>
2013-09-13 17:00:09 +0000

    Removal of old stlports

--- lib/STLport5/build-msvc11-x64.bat
+++
@@ -1,6 +0,0 @@
-set ProgRoot=%ProgramFiles%
-if not "%ProgramFiles(x86)%" == "" set ProgRoot=%ProgramFiles(x86)%
-call "%ProgRoot%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
-call configure.bat msvc11x64
-call cd build\lib
-call nmake clean install
