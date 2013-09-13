7465e5cf9eb6d6cebeaa774251b9799d053da8ff
turley <turley@11a62ffb-1e35-46b8-8505-d88fde283c85>
2013-09-13 17:00:09 +0000

    Removal of old stlports

--- trunk/lib/STLport5/build-msvc9-x64.bat
+++
@@ -1,6 +0,0 @@
-set ProgRoot=%ProgramFiles%
-if not "%ProgramFiles(x86)%" == "" set ProgRoot=%ProgramFiles(x86)%
-call "%ProgRoot%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86_amd64
-call configure.bat msvc9x64
-call cd build\lib
-call nmake clean install
