7dbba86765f7b48f33ad193231d1f844640ed186
turley <turley@polserver.com>
2013-12-21 09:49:02 +0000

    updated boost to 1.55

--- lib/prepare_boost.bat
+++ lib/prepare_boost.bat
@@ -3,5 +3,5 @@
 echo This script will extract boost to this folder.
 pause
 
-..\pol-core\dist\7za.exe x -so boost_1_54_0.tar.bz2 | ..\pol-core\dist\7za.exe x -si -ttar
+..\pol-core\dist\7za.exe x -so boost_1_55_0.tar.bz2 | ..\pol-core\dist\7za.exe x -si -ttar
 
