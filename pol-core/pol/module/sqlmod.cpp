dc2f993d091b4bc633ada2839e5e15ba7f91cd54
turley <turley@polserver.com>
2014-01-20 19:14:20 +0000

    wait for loggingfacility on server crash made the sql module at least a bit more scriptfriendly: its now save to pass sql objects as parameters, result objects get automatically freed. Thus also removed the mysql_free_result function. mysql_close is also now senseless, but kept for the guys who want to close the connection direct. All I could do in half an hour sorry.. Never ever pass a C API directly into a scripting language! And now the complete class: "Never ever ..."

--- pol-core/pol/module/sqlmod.cpp
+++ pol-core/pol/module/sqlmod.cpp
@@ -25,7 +25,6 @@ namespace Pol {
       { "MySQL_Connect", &SQLExecutorModule::mf_ConnectToDB },
       { "MySQL_Query", &SQLExecutorModule::mf_Query },
       { "MySQL_Close", &SQLExecutorModule::mf_Close },
-      { "MySQL_Free_Result", &SQLExecutorModule::mf_FreeResult },
       { "MySQL_Num_Fields", &SQLExecutorModule::mf_NumFields },
       { "MySQL_Fetch_Row", &SQLExecutorModule::mf_FetchRow },
       { "MySQL_Affected_Rows", &SQLExecutorModule::mf_AffectedRows },
@@ -42,14 +41,15 @@ namespace Pol {
 
 	Bscript::BObjectImp* SQLExecutorModule::mf_ConnectToDB()
 	{
+      const String *host = getStringParam( 0 );
+      const String *username = getStringParam( 1 );
+      const String *password = getStringParam( 2 );
+      if ( !host || !username || !password )
+      {
+        return new BError( "Invalid parameters" );
+      }
 
-	  const String *host, *username, *password;
-	  if ( !( host = getStringParam( 0 ) ) || !( username = getStringParam( 1 ) ) || !( password = getStringParam( 2 ) ) )
-	  {
-		return new BError( "Invalid parameters" );
-	  }
-
-      Core::BSQLConnection *sql = new Core::BSQLConnection( );
+      auto sql = std::unique_ptr<Core::BSQLConnection>( new Core::BSQLConnection() );
 	  if ( sql->getLastErrNo() )
 	  {
 		return new BError( "Insufficient memory" );
@@ -58,17 +58,16 @@ namespace Pol {
 	  {
 		return new BError( sql->getLastError() );
 	  }
-	  return sql;
-	  //return new BLong(1);
+	  return sql.release();
 	}
 	Bscript::BObjectImp* SQLExecutorModule::mf_SelectDb()
 	{
-      Core::BSQLConnection *sql;
-	  const String *db;
-      if ( !( sql = EXPLICIT_CAST( Core::BSQLConnection*, Bscript::BObjectImp* )( getParamImp( 0, Bscript::BObjectImp::OTSQLConnection ) ) ) || !( db = getStringParam( 1 ) ) )
-	  {
-		return new BError( "Invalid parameters" );
-	  }
+      Core::BSQLConnection *sql = static_cast<Core::BSQLConnection*>( getParamImp( 0, Bscript::BObjectImp::OTSQLConnection ) );
+      const String *db = getStringParam( 1 );
+      if ( !sql || !db )
+      {
+        return new BError( "Invalid parameters" );
+      }
 	  if ( !sql->select_db( *db ) )
 		return new BError( sql->getLastError() );
 	  return new BLong( 1 );
@@ -76,9 +75,9 @@ namespace Pol {
 
 	Bscript::BObjectImp* SQLExecutorModule::mf_Query()
 	{
-      Core::BSQLConnection *sql;
-	  const String *query;
-      if ( !( sql = EXPLICIT_CAST( Core::BSQLConnection*, Bscript::BObjectImp* )( getParamImp( 0, Bscript::BObjectImp::OTSQLConnection ) ) ) || !( query = getStringParam( 1 ) ) )
+      Core::BSQLConnection *sql = static_cast<Core::BSQLConnection*>( getParamImp( 0, Bscript::BObjectImp::OTSQLConnection ) );;
+      const String *query = getStringParam( 1 );
+      if ( !sql || !query )
 	  {
 		return new BError( "Invalid parameters" );
 	  }
@@ -88,13 +87,12 @@ namespace Pol {
 	  }
 
 	  return sql->getResultSet();
-	  //return new BLong(0);
 	}
 
 	Bscript::BObjectImp* SQLExecutorModule::mf_NumFields()
 	{
-      Core::BSQLResultSet *result;
-      if ( !( result = EXPLICIT_CAST( Core::BSQLResultSet*, Bscript::BObjectImp* )( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) ) ) )
+      Core::BSQLResultSet *result = static_cast<Core::BSQLResultSet*>( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ));
+      if ( !result )
 	  {
 		return new BError( "Invalid parameters" );
 	  }
@@ -103,12 +101,10 @@ namespace Pol {
 
 	Bscript::BObjectImp* SQLExecutorModule::mf_FieldName()
 	{
-      Core::BSQLResultSet *result;
-	  int index;
-	  //BLong& lng = (BLong &) right;
-	  //unsigned index = (unsigned) lng.value();
+      Core::BSQLResultSet *result = static_cast<Core::BSQLResultSet*>( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) );
+      int index = getParam( 1, index );
 
-      if ( !( result = EXPLICIT_CAST( Core::BSQLResultSet*, Bscript::BObjectImp* )( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) ) ) || !getParam( 1, index ) )
+      if ( !result || !index )
 	  {
 		return new BError( "Invalid parameters" );
 	  }
@@ -120,8 +116,8 @@ namespace Pol {
 
 	Bscript::BObjectImp* SQLExecutorModule::mf_AffectedRows()
 	{
-      Core::BSQLResultSet *result;
-      if ( !( result = EXPLICIT_CAST( Core::BSQLResultSet*, Bscript::BObjectImp* )( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) ) ) )
+      Core::BSQLResultSet *result = static_cast<Core::BSQLResultSet*>( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) );;
+      if ( !result )
 	  {
 		return new BError( "Invalid parameters" );
 	  }
@@ -130,8 +126,8 @@ namespace Pol {
 
 	Bscript::BObjectImp* SQLExecutorModule::mf_NumRows()
 	{
-      Core::BSQLResultSet *result;
-      if ( !( result = EXPLICIT_CAST( Core::BSQLResultSet*, Bscript::BObjectImp* )( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) ) ) )
+      Core::BSQLResultSet *result = static_cast<Core::BSQLResultSet*>( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) );;
+      if ( !result )
 	  {
 		return new BError( "Invalid parameters" );
 	  }
@@ -140,27 +136,17 @@ namespace Pol {
 
 	Bscript::BObjectImp* SQLExecutorModule::mf_Close()
 	{
-      Core::BSQLConnection *sql;
-      if ( !( sql = EXPLICIT_CAST( Core::BSQLConnection*, Bscript::BObjectImp* )( getParamImp( 0, Bscript::BObjectImp::OTSQLConnection ) ) ) )
-		return new BError( "Invalid parameters" );
+      Core::BSQLConnection *sql = static_cast<Core::BSQLConnection *>( getParamImp( 0, Bscript::BObjectImp::OTSQLConnection ) );
+      if ( !sql )
+        return new BError( "Invalid parameters" );
 	  sql->close();
-	  return new BLong( 1 );
-	}
-	Bscript::BObjectImp* SQLExecutorModule::mf_FreeResult()
-	{
-      Core::BSQLResultSet *result;
-      if ( !( result = EXPLICIT_CAST( Core::BSQLResultSet*, Bscript::BObjectImp* )( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) ) ) )
-	  {
-		return new BError( "Invalid parameters" );
-	  }
-	  result->free();
 	  return new BLong( 1 );
 	}
 
 	Bscript::BObjectImp* SQLExecutorModule::mf_FetchRow()
 	{
-      Core::BSQLResultSet *result;
-      if ( !( result = EXPLICIT_CAST( Core::BSQLResultSet*, Bscript::BObjectImp* )( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) ) ) )
+      Core::BSQLResultSet *result = static_cast<Core::BSQLResultSet*>( getParamImp( 0, Bscript::BObjectImp::OTSQLResultSet ) );
+      if ( !result )
 	  {
 		return new BError( "Invalid parameters" );
 	  }
@@ -178,7 +164,6 @@ namespace Pol {
 	  MF_NO_MYSQL( mf_AffectedRows )
 	  MF_NO_MYSQL( mf_NumRows )
 	  MF_NO_MYSQL( mf_Close )
-	  MF_NO_MYSQL( mf_FreeResult )
 	  MF_NO_MYSQL( mf_FetchRow )
 #endif
   }
