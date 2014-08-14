f7888ad8c43e7da91afab09721a18982feddfce9
turley <turley@polserver.com>
2014-08-14 19:54:01 +0200

    changed to python2.7

--- testsuite/escript/performtests.py
+++ testsuite/escript/performtests.py
@@ -11,12 +11,12 @@ class Compare:
       l1=f1.readlines()
       l2=f2.readlines()
       if len(l1) != len(l2):
-        print('line count differs')
+        print 'line count differs'
         return False
       for c1,c2 in zip(l1,l2):
         if c1!=c2:
-          print('"',c1,'"')
-          print('"',c2,'"')
+          print '"',c1,'"'
+          print '"',c2,'"'
           return False
       return True
 
@@ -60,19 +60,19 @@ class StdTests:
   def __call__(self,compiler,runecl):
     all_passed=True
     for f in self.files:
-      print('Testing',f)
+      print 'Testing',f
       if compiler(f) is None:
         all_passed=False
-        print('failed to compile')
+        print 'failed to compile'
         continue
 
       if runecl(f) is None:
         all_passed=False
-        print('failed to execute')
+        print 'failed to execute'
         continue
 
       if not Compare.outputcompare(f):
-        print('output differs')
+        print 'output differs'
         all_passed=False
     return all_passed
 
