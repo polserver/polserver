cc092ad827afca298eda5a47301180dd3b20bac1
frozenblit <rozenblit@gmail.com>
2014-09-01 00:15:31 +0200

    Added a ClientFeatures class
    Testing some ideas for a new class to take care of knowing which
    features are enabled based on the client version and acct expansion.

--- pol-core/plib/uoexpansion.cpp
+++ pol-core/plib/uoexpansion.cpp
@@ -1,1 +1,25 @@
 #include "uoexpansion.h"
+
+namespace Pol {
+    namespace Plib {
+
+        char *ExpansionNames[numExpansions] = {
+            "T2A", "LBR", "AOS",
+            "SE", "ML", "KR",
+            "SA", "HSA"
+        };
+        B9Feature ExpansionFlags[numExpansions] = {
+            B9Feature::T2A, B9Feature::LBR, B9Feature::AOS,
+            B9Feature::SE, B9Feature::ML, B9Feature::KRFaces,
+            B9Feature::SA, B9Feature::HSA
+        };
+
+        const char* getExpansionName(ExpansionVersion x) {
+            if (x > ExpansionVersion::LastVersion)
+                return "";
+
+            return ExpansionNames[static_cast<u8>(x)];
+        }
+
+    }
+}
