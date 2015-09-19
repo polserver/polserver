6f87494410879244b621185d8e506230ebde19f2
turley <michael.volz81@googlemail.com>
2015-09-19 10:56:31 +0200

    adapted unicode

--- pol-core/pol/unicode.h
+++ pol-core/pol/unicode.h
@@ -8,8 +8,8 @@ Notes
 
 */
 
-#ifndef CLIB_UNICODE_H
-#define CLIB_UNICODE_H
+#ifndef POL_UNICODE_H
+#define POL_UNICODE_H
 
 #include <string>
 
@@ -18,14 +18,8 @@ Notes
 #include "../bscript/bobject.h"
 #endif
 
-#include "../pol/utype.h"
 namespace Pol {
-  namespace Clib {
-	//std::string toascii( const std::wstring& wstr );
-	//std::wstring tounicode( const std::string& str );
-
-	void unicode_copy_string( wchar_t* dst, size_t dstsize, const char* src );
-
+  namespace Core {
 	bool convertArrayToUC( Bscript::ObjArray*& in_text, u16* out_wtext,
 						   size_t textlen, bool ConvToBE = false, bool nullterm = true );
 	bool convertUCtoArray( const u16* in_wtext, Bscript::ObjArray*& out_text,
