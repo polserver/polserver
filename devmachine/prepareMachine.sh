ebb7ef94e1491bcc999e1672406f3d8d2d2e44c1
turley <michael.volz81@googlemail.com>
2015-09-19 10:01:25 +0200

    updated vagrant vm (Ubuntu 14, gcc5 clang3.8 cmake)

--- devmachine/prepareMachine.sh
+++ devmachine/prepareMachine.sh
@@ -36,6 +36,14 @@ echo ---------------------------------------------------------------------------
 echo
 echo sudo apt-get install mysql-client libmysqlclient-dev -y
 
+
+echo
+echo -------------------------------------------------------------------------------
+echo OpenSSL
+echo -------------------------------------------------------------------------------
+echo
+echo sudo apt-get install libssl-dev -y
+
 echo
 echo -------------------------------------------------------------------------------
 echo  add-apt-repository
@@ -45,7 +53,7 @@ sudo apt-get install python-software-properties -y
 
 echo
 echo -------------------------------------------------------------------------------
-echo add gcc 4.8 repro
+echo add gcc repro
 echo -------------------------------------------------------------------------------
 echo
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
@@ -55,8 +63,15 @@ echo ---------------------------------------------------------------------------
 echo add llvm repro
 echo -------------------------------------------------------------------------------
 echo
-sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
+sudo add-apt-repository 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main'
 sudo wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+
+echo
+echo -------------------------------------------------------------------------------
+echo add cmake repro
+echo -------------------------------------------------------------------------------
+echo
+sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
 
 echo
 echo -------------------------------------------------------------------------------
@@ -70,19 +85,23 @@ echo ---------------------------------------------------------------------------
 echo install clang
 echo -------------------------------------------------------------------------------
 echo
-sudo apt-get install clang-3.7 clang-3.7-doc libclang-common-3.7-dev libclang-3.7-dev libclang1-3.7 libllvm-3.7-ocaml-dev libllvm3.7 lldb-3.7 llvm-3.7 llvm-3.7-dev llvm-3.7-doc llvm-3.7-examples llvm-3.7-runtime clang-modernize-3.7 clang-format-3.7 -y
-# libclang1-3.4-dbg
-# libllvm3.4-dbg
+sudo apt-get install clang-3.8 clang-3.8-doc libclang-common-3.8-dev libclang-3.8-dev libclang1-3.8 libllvm-3.8-ocaml-dev libllvm3.8 lldb-3.8 llvm-3.8 llvm-3.8-dev llvm-3.8-doc llvm-3.8-examples llvm-3.8-runtime clang-modernize-3.8 clang-format-3.8 -y
 
 echo
 echo -------------------------------------------------------------------------------
-echo install gcc 4.8
+echo install gcc 5
 echo -------------------------------------------------------------------------------
 echo
-echo sudo apt-get install g++-4.8 cpp-4.8 gcc-4.8 libcloog-isl4 libisl10 gcc-4.8-locales g++-4.8-multilib gcc-4.8-doc libstdc++6-4.8-dbg gcc-4.8-multilib libmudflap0-4.8-dev libgomp1-dbg libitm1-dbg libatomic1-dbg libasan0-dbg libtsan0-dbg libquadmath0-dbg libmudflap0-dbg binutils-gold -y
+sudo apt-get install g++-5 cpp-5 gcc-5  gcc-5-locales g++-5-multilib gcc-5-doc libstdc++6-5-dbg gcc-5-multilib libcloog-isl4 libisl10 libgomp1-dbg libitm1-dbg libatomic1-dbg libasan0-dbg libtsan0-dbg libquadmath0-dbg binutils -y
 
+echo
+echo -------------------------------------------------------------------------------
+echo install cmake
+echo -------------------------------------------------------------------------------
+echo
+sudo apt-get install cmake -y
 
-
+sudo apt-get upgrade
 
 # Check out the code (will take a while because of boost)
 # svn checkout svn://svn.code.sf.net/p/polserver/code/trunk polserver
@@ -91,3 +110,4 @@ echo sudo apt-get install g++-4.8 cpp-4.8 gcc-4.8 libcloog-isl4 libisl10 gcc-4.8
 if [ ! -d "/home/vagrant/polserver" ]; then
 	ln -s /vagrant /home/vagrant/polserver
 fi
+
