ce2fd726c1716b805fb936c4eafbc6c21fa7dd1e
turley <michael.volz81@googlemail.com>
2015-12-25 12:24:07 +0100

    Added options to force gcc/clang or prune

--- pol-core/bin-build/build_linux.sh
+++ pol-core/bin-build/build_linux.sh
@@ -14,6 +14,40 @@ echo "CPU cores        : $CPU_CORES"
 echo "Memory size (GB) : $PHYS_MEM_GB"
 echo "CMAKE version    : $CMAKE_VERSION"
 
+case "$1" in
+	-g|-gcc5)
+		export CXX="g++-5" CC="gcc-5";
+		echo "FORCING GCC-5"
+		make clean
+		rm -r -f pol-core
+		rm -r -f lib
+		rm -r -f CMakeFiles
+		rm CMakeCache.txt
+		rm Makefile
+		;;
+	-c|-clang37)
+		export CXX="clang++-3.7" CC="clang-3.7";
+		echo "FORCING CLANG-3.7"
+		make clean
+		rm -r -f pol-core
+		rm -r -f lib
+		rm -r -f CMakeFiles
+		rm CMakeCache.txt
+		rm Makefile
+		;;
+	-p|-prune)
+		make clean
+		rm -r -f pol-core
+		rm -r -f lib
+		rm -r -f CMakeFiles
+		rm CMakeCache.txt
+		rm Makefile
+		exit
+		;;
+	*)
+		;;
+esac
+
 echo "###################################################################################"
 echo "########################### Prepraring CMake  #####################################"
 echo "###################################################################################"
