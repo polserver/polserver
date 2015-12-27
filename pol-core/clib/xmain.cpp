0a1748835f412d59a9e7a9167e73e177733349ca
Thomas Volkert <thomas@homer-conferencing.com>
2015-12-27 20:36:43 +0100

    Removed obsolete code fragment, some more code cleanup

--- pol-core/clib/xmain.cpp
+++ pol-core/clib/xmain.cpp
@@ -9,15 +9,6 @@ Notes
 
 */
 
-#include "xmain.h"
-
-#include "Debugging/ExceptionParser.h"
-#include "Program/ProgramConfig.h"
-
-#include "clib.h"
-#include "logfacility.h"
-#include "strexcpt.h"
-
 #if defined(WINDOWS)
 #include "Header_Windows.h"
 #pragma comment(lib, "psapi.lib") // 32bit is a bit dumb..
@@ -26,86 +17,11 @@ Notes
 #   include <sys/resource.h>
 #endif
 #include <stdexcept>
-#include <string>
-
-#if 0 //TODO: remove the following code fragment after the reworking after CMake introduction is finished
-int main( int argc, char *argv[] )
-{
-	using namespace Pol;
-	Clib::Logging::LogFacility logger;
-	Clib::Logging::initLogging( &logger );
-	Clib::ExceptionParser::initGlobalExceptionCatching();
-
-	setlocale(LC_TIME,"");
-    int exitcode = 0;
-
-    try 
-    {
-	// FIXME: 2008 Upgrades needed here? Make sure this still valid on 2008
-#if defined(_WIN32) && defined(_DEBUG) && _MSC_VER >= 1300
-        // on VS.NET, disable obnoxious heap checking
-        int flags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
-        flags &= 0x0000FFFF; // set heap check frequency to 0
-        _CrtSetDbgFlag( flags );
-#endif
-
-
-		/**
-		 * determine and store the executable name
-		 */
-	    std::string exe_path = argv[0];
-		#ifdef _WIN32
-			char module_path[_MAX_PATH];
-			if (GetModuleFileName( NULL, module_path, sizeof module_path))
-			  exe_path = module_path;
-		#endif
-        PROG_CONFIG::configureProgramEnvironment(exe_path);
-
-        exitcode = xmain( argc, argv );
-    }
-    catch( const char *msg )
-    {
-        ERROR_PRINT << "Execution aborted due to: " << msg << "\n";
-        exitcode = 1;
-    }
-    catch( std::string& str )
-    {
-      ERROR_PRINT << "Execution aborted due to: " << str << "\n";
-        exitcode = 1;
-    }       // egcs has some trouble realizing 'exception' should catch
-    catch (std::runtime_error& re)   // runtime_errors, so...
-    {
-      ERROR_PRINT << "Execution aborted due to: " << re.what( ) << "\n";
-        exitcode = 1;
-    }
-    catch (std::exception& ex)
-    {
-      ERROR_PRINT << "Execution aborted due to: " << ex.what( ) << "\n";
-        exitcode = 1;
-    }
-    catch( int xn )
-    {
-        // Something that throws an integer is responsible for printing
-        // its own error message.  
-        // "throw 3" is meant as an alternative to exit(3).
-        exitcode = xn;
-    }
-#ifndef _WIN32
-    catch( ... )
-    {
-      ERROR_PRINT << "Execution aborted due to generic exception." << "\n";
-        exitcode = 2;
-    }
-#endif    
-	Clib::Logging::global_logger->wait_for_empty_queue();
-
-    return exitcode;
-}
-#endif
 
 namespace Pol {
   namespace Clib  {
 
+    //TODO: create a system.cpp/h and put the following function with some other features in a separate static class "system"
     size_t getCurrentMemoryUsage( )
     {
 #if defined(_WIN32)
