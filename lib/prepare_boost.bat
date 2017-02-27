0f66818b9e87c82c775afe40e88e103500bd8d1e
Fernando Rozenblit <rozenblit@gmail.com>
2017-02-27 23:53:15 +0100

    Update boost to version 1.63

--- lib/prepare_boost.bat
+++ lib/prepare_boost.bat
@@ -1,10 +1,10 @@
 @echo off
 
 
-if exist boost_1_55_0\boost goto dontextract
+if exist boost_1_63_0\boost goto dontextract
 
 
-..\pol-core\dist\7za.exe x -so boost_1_55_0.tar.bz2 | ..\pol-core\dist\7za.exe x -si -ttar
+..\pol-core\dist\7za.exe x -so boost_1_63_0.tar.bz2 | ..\pol-core\dist\7za.exe x -si -ttar
 
 :dontextract
 echo Nothing to do. Files already extracted.
