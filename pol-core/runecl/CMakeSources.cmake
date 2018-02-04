8a72f084450ee34872c641498b4d40fb3f044373
turleypol <turley@polserver.com>
2018-02-04 20:07:10 +0100

    seperated source definition in extra files and explixitly named all headers (anyway better and needed for IDEs)

---
+++ pol-core/runecl/CMakeSources.cmake
@@ -0,0 +1,18 @@
+set (runecl_sources  # sorted !
+  ../pol/binaryfilescrobj.cpp 
+  ../pol/cfgrepos.cpp 
+  ../pol/dice.cpp
+  ../pol/globals/ucfg.cpp
+  ../pol/module/basiciomod.cpp 
+  ../pol/module/basicmod.cpp 
+  ../pol/module/cfgmod.cpp 
+  ../pol/module/datastore.cpp
+  ../pol/module/filemod.cpp 
+  ../pol/module/mathmod.cpp
+  ../pol/module/utilmod.cpp 
+  ../pol/proplist.cpp
+  ../pol/xmlfilescrobj.cpp
+  RunEclMain.cpp
+  RunEclMain.h
+  StdAfx.h
+)
---
+++ pol-core/uoconvert/CMakeSources.cmake
@@ -0,0 +1,9 @@
+set (uoconvert_sources  # sorted !
+  ../pol/polfile1.cpp
+  StdAfx.h
+  UoConvertMain.cpp
+  UoConvertMain.h
+  resource.h
+  uoconvert.ico
+  uoconvert.rc
+)
