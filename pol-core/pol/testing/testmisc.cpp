e761ed096d92e063157dac0ca8847d54430b04ff
turleypol <turley@polserver.com>
2018-06-02 22:37:14 +0200

    added small pkt test for upcomming changes

--- pol-core/pol/testing/testmisc.cpp
+++ pol-core/pol/testing/testmisc.cpp
@@ -3,6 +3,8 @@
  * @par History
  */
 
+#include <algorithm>
+#include <array>
 #include <string>
 
 #include "../../clib/logfacility.h"
@@ -10,6 +12,7 @@
 #include "../../plib/maptile.h"
 #include "../dynproperties.h"
 #include "../globals/uvars.h"
+#include "../network/packethelper.h"
 #include "../realms/realm.h"
 
 namespace Pol
@@ -47,6 +50,91 @@ void dynprops_test()
   h.itemname( "" );
   INFO_PRINT << "name " << h.itemname() << " " << h.has_itemname() << "\n";
   INFO_PRINT << "size " << h.estimateSizeDynProps() << "\n";
+}
+
+void packet_test()
+{
+  using namespace Network;
+  using namespace Network::PktHelper;
+  auto debug = []( const PacketOut<PktOut_2F>& p ) {
+    fmt::Writer w;
+    for ( const u8& c : p->buffer )
+    {
+      w << fmt::hex( c ) << " ";
+    }
+    INFO_PRINT << w.str() << "\n";
+  };
+  auto test = []( const PacketOut<PktOut_2F>& p, const std::array<s8, 10>& a ) {
+
+    if ( std::equal( std::begin( p->buffer ), std::end( p->buffer ), std::begin( a ) ) )
+      INFO_PRINT << "success\n";
+    else
+    {
+      INFO_PRINT << "failed\n";
+
+      fmt::Writer w;
+      for ( const u8& c : a )
+      {
+        w << fmt::hex( c ) << " ";
+      }
+      INFO_PRINT << w.str() << "\n";
+    }
+  };
+  {
+    PacketOut<PktOut_2F> p;  // size 10
+    p->Write<s8>( 0x12 );
+    p->Write<u8>( 0x21u );
+    p->WriteFlipped<s8>( 0x12 );
+    p->WriteFlipped<u8>( 0x21u );
+    debug( p );
+    std::array<s8, 10> a{0x2f, 0x12, 0x21, 0x12, 0x21, 0, 0, 0, 0, 0};
+    test( p, a );
+  }
+  {
+    PacketOut<PktOut_2F> p;  // size 10
+    p->Write<s16>( 0x1234 );
+    p->Write<u16>( 0x4321u );
+    p->WriteFlipped<s16>( 0x1234 );
+    p->WriteFlipped<u16>( 0x4321u );
+    debug( p );
+    std::array<s8, 10> a{0x2f, 0x34, 0x12, 0x21, 0x43, 0x12, 0x34, 0x43, 0x21, 0};
+    test( p, a );
+  }
+  {
+    PacketOut<PktOut_2F> p;  // size 10
+    p->Write<s32>( 0x12344321 );
+    p->Write<u32>( 0x12344321u );
+    debug( p );
+    std::array<s8, 10> a{0x2f, 0x21, 0x43, 0x34, 0x12, 0x21, 0x43, 0x34, 0x12, 0};
+    test( p, a );
+  }
+  {
+    PacketOut<PktOut_2F> p;  // size 10
+    p->WriteFlipped<s32>( 0x12344321 );
+    p->WriteFlipped<u32>( 0x12344321u );
+    debug( p );
+    std::array<s8, 10> a{0x2f, 0x12, 0x34, 0x43, 0x21, 0x12, 0x34, 0x43, 0x21, 0};
+    test( p, a );
+  }
+  {
+    PacketOut<PktOut_2F> p;  // size 10
+    std::string s( "1234" );
+    p->Write( s.c_str(), 4, false );
+    u8 b[] = {0x12, 0x34, 0x43, 0x21};
+    p->Write( b, 4 );
+    debug( p );
+    std::array<s8, 10> a{0x2f, 0x31, 0x32, 0x33, 0x34, 0x12, 0x34, 0x43, 0x21, 0};
+    test( p, a );
+  }
+  {
+    PacketOut<PktOut_2F> p;  // size 10
+    u16 b[] = {0x12, 0x34};
+    p->Write( &b[0], 2, false );
+    p->WriteFlipped( &b[0], 2, false );
+    debug( p );
+    std::array<s8, 10> a{0x2f, 0x12, 0, 0x34, 0, 0, 0x12, 0, 0x34, 0};
+    test( p, a );
+  }
 }
 
 }  // namespace Testing
