dafb1d7b9edb5dff3fe6d67694bf267dcfc1bb3a
turleypol <turley@polserver.com>
2018-12-02 19:39:57 +0100

    updated flyweight memory report (used for memory saving of strings) enable via ENABLE_FLYWEIGHT_REPORT cmake option

--- pol-core/clib/boostutils.cpp
+++ pol-core/clib/boostutils.cpp
@@ -4,8 +4,39 @@ namespace Pol
 {
 namespace boost_utils
 {
-#ifdef DEBUG_FLYWEIGHT
-std::vector<bucket_query*> debug_flyweight_queries;
+flyweight_initializers::~flyweight_initializers()
+{
+#ifdef ENABLE_FLYWEIGHT_REPORT
+  if ( Query::debug_flyweight_queries != nullptr )
+    delete Query::debug_flyweight_queries;
+  Query::debug_flyweight_queries = nullptr;
 #endif
 }
+
+#ifdef ENABLE_FLYWEIGHT_REPORT
+std::vector<bucket_query*>* Query::debug_flyweight_queries = nullptr;
+
+bucket_query::bucket_query()
+{
+  Query::add( this );
 }
+
+void Query::add( bucket_query* b )
+{
+  if ( debug_flyweight_queries == nullptr )
+    debug_flyweight_queries = new std::vector<bucket_query*>;
+  debug_flyweight_queries->push_back( b );
+}
+
+std::vector<std::pair<size_t, size_t>> Query::getCountAndSize()
+{
+  std::vector<std::pair<size_t, size_t>> res;
+  for ( const auto& factory : *debug_flyweight_queries )
+  {
+    res.emplace_back( factory->bucket_count(), factory->estimateSize() );
+  }
+  return res;
+}
+#endif
+}  // namespace boost_utils
+}  // namespace Pol
