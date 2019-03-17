3476ac38b83c4c1362608cedca63f98d606f0d73
Kevin Eady <kevin.c.eady@gmail.com>
2019-03-17 20:08:59 +0100

    fix cmake

--- cmake/node.cmake
+++ cmake/node.cmake
@@ -19,7 +19,6 @@ set (NODE_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/../lib/${NODE_PATHNAME}")
 set (NODE_LIB "${NODE_SOURCE_DIR}/${NODE_LIBRARY_DIR}/${NODE_LIBRARY_NAME}")
 set (NODE_DLL "${NODE_SOURCE_DIR}/${NODE_LIBRARY_DIR}/${NODE_DLL_NAME}")
 
-if(NOT EXISTS ${NODE_LIB})
   set(NODE_URL "${NODE_URL_BASEPATH}${NODE_PATHNAME}.zip")
   message("    ** extracting nodejs from ${NODE_URL} => ${NODE_SOURCE_DIR}")
     ExternalProject_Add(nodejs
@@ -32,19 +31,11 @@ if(NOT EXISTS ${NODE_LIB})
       CONFIGURE_COMMAND ""
       BUILD_COMMAND ""
       
-      LOG_DOWNLOAD 1
+      #LOG_DOWNLOAD 1
       BUILD_IN_SOURCE 1
-      LOG_CONFIGURE 1
-      LOG_BUILD 1
-      LOG_INSTALL 1
+      BUILD_BYPRODUCTS ${NODE_LIB}
+      #LOG_CONFIGURE 1
+      #LOG_BUILD 1
+      #LOG_INSTALL 1
     )
     set_target_properties (nodejs PROPERTIES FOLDER 3rdParty)
-
-  # ExternalProject_Get_Property(nodejs SOURCE_DIR)
-# message(STATUS "Source directory of node ${SOURCE_DIR}")
-
-
-else()
-  message("Node already extracted")
-endif()
-
