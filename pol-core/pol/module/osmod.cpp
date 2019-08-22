a31f1d509b9816d3cb0a96d9f5f78f06a202326a
Jonathan Morland-Barrett <jmorland.barrett@gmail.com>
2019-08-22 14:12:01 +0800

    Added error for if NaN sent as PID

--- pol-core/pol/module/osmod.cpp
+++ pol-core/pol/module/osmod.cpp
@@ -152,7 +152,9 @@ BObjectImp* OSExecutorModule::mf_debugger()
 BObjectImp* OSExecutorModule::getprocess()
 {
   unsigned int pid;
-  getParam(0, pid);
+  if (!getParam(0, pid))
+      return new BError("Invalid parameter type");
+
   if ( pid == 0 )
   {
     pid = pid_;
