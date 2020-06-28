c9c860f78d34bdd2c1e51bce2b1c411b1e4c1e72
Eric Swanson <eric.the.unicorn@gmail.com>
2020-06-27 20:46:49 -0700

    escript_grammar.cmake from Turley

---
+++ cmake/escript_grammar.cmake
@@ -0,0 +1,45 @@
+find_package(Java QUIET COMPONENTS Runtime)
+
+if(NOT ANTLR_EXECUTABLE)
+  find_program(ANTLR_EXECUTABLE
+    NAMES antlr.jar antlr4.jar antlr-4.jar antlr-4.8-complete.jar
+  )
+endif()
+if(Java_JAVA_EXECUTABLE AND ANTLR_EXECUTABLE)
+  message("adding target 'escriptgrammar'")
+  add_custom_target(escriptgrammar
+    COMMAND ${Java_JAVA_EXECUTABLE} -jar ${ANTLR_EXECUTABLE}
+      -Dlanguage=Cpp -visitor
+      ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptParser.g4
+      -o ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar
+    COMMAND ${Java_JAVA_EXECUTABLE} -jar ${ANTLR_EXECUTABLE}
+      -Dlanguage=Cpp
+      ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptLexer.g4
+      -o ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar
+    COMMENT "Generating grammar files"
+  )
+endif()
+
+#fake target for vstudio gui
+add_custom_target(escriptfiles
+  WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+  VERBATIM
+  SOURCES 
+    ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptParser.g4
+    ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptParser.h
+    ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptParser.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptLexer.g4
+    ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptLexer.h
+    ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptLexer.cpp
+)
+source_group(escriptgrammar FILES
+  ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptParser.g4
+  ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptParser.h
+  ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptParser.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptLexer.g4
+  ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptLexer.h
+  ${CMAKE_CURRENT_LIST_DIR}/../lib/EScriptGrammar/EscriptLexer.cpp
+)
+set_target_properties(escriptfiles PROPERTIES EXCLUDE_FROM_ALL TRUE)
+set_target_properties(escriptfiles PROPERTIES EXCLUDE_FROM_DEFAULT_BUILD TRUE)
+set_target_properties(escriptfiles PROPERTIES FOLDER Grammar)
