bd69c7cda366ca1e876531f0cf2df67a5acc2da5
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:10 -0700

    SimpleStatementBuilder, CompoundStatementBuilder - handle var statements

--- pol-core/bscript/compiler/astbuilder/SimpleStatementBuilder.h
+++ pol-core/bscript/compiler/astbuilder/SimpleStatementBuilder.h
@@ -12,8 +12,18 @@ class SimpleStatementBuilder : public ExpressionBuilder
 public:
   SimpleStatementBuilder( const SourceFileIdentifier&, BuilderWorkspace& );
 
+  void add_var_statements( EscriptGrammar::EscriptParser::VarStatementContext*,
+                           std::vector<std::unique_ptr<Statement>>& );
+  void add_var_statements( EscriptGrammar::EscriptParser::UnambiguousVarStatementContext*,
+                           std::vector<std::unique_ptr<Statement>>& );
+
   static std::unique_ptr<Statement> consume_statement_result(
       std::unique_ptr<Statement> statement );
+
+  std::unique_ptr<Expression> variable_initializer(
+      EscriptGrammar::EscriptParser::VariableDeclarationInitializerContext* );
+  std::unique_ptr<Expression> variable_initializer(
+      EscriptGrammar::EscriptParser::UnambiguousVariableDeclarationInitializerContext* );
 
 };
 
