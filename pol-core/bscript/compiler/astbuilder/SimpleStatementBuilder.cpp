c210db7804bbb12df73646c820f93d4d0cc65943
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:20 -0700

    Remainder astbuilder SimpleStatementBuilder

--- pol-core/bscript/compiler/astbuilder/SimpleStatementBuilder.cpp
+++ pol-core/bscript/compiler/astbuilder/SimpleStatementBuilder.cpp
@@ -1,12 +1,21 @@
 #include "SimpleStatementBuilder.h"
 
 #include "compiler/Report.h"
-#include "compiler/astbuilder/BuilderWorkspace.h"
+#include "compiler/ast/BinaryOperator.h"
+#include "compiler/ast/BreakStatement.h"
+#include "compiler/ast/ConstDeclaration.h"
+#include "compiler/ast/ContinueStatement.h"
+#include "compiler/ast/DebugStatementMarker.h"
+#include "compiler/ast/EnumDeclaration.h"
 #include "compiler/ast/Expression.h"
+#include "compiler/ast/Identifier.h"
+#include "compiler/ast/IntegerValue.h"
+#include "compiler/ast/ReturnStatement.h"
 #include "compiler/ast/StringValue.h"
-#include "compiler/astbuilder/BuilderWorkspace.h"
 #include "compiler/ast/ValueConsumer.h"
 #include "compiler/ast/VarStatement.h"
+#include "compiler/astbuilder/BuilderWorkspace.h"
+#include "compiler/model/CompilerWorkspace.h"
 
 using EscriptGrammar::EscriptParser;
 
@@ -20,6 +29,13 @@ SimpleStatementBuilder::SimpleStatementBuilder( const SourceFileIdentifier& sour
                                     BuilderWorkspace& workspace )
   : ExpressionBuilder( source_file_identifier, workspace )
 {
+}
+
+void SimpleStatementBuilder::add_intrusive_debug_marker(
+    antlr4::ParserRuleContext* ctx, std::vector<std::unique_ptr<Statement>>& statements )
+{
+  statements.push_back( std::make_unique<DebugStatementMarker>(
+      location_for( *ctx ), ctx->getText(), ctx->start->getStartIndex() ) );
 }
 
 void SimpleStatementBuilder::add_var_statements( EscriptParser::VarStatementContext* ctx,
@@ -90,10 +106,143 @@ void SimpleStatementBuilder::add_var_statements( EscriptParser::UnambiguousVarSt
   }
 }
 
+std::unique_ptr<BreakStatement> SimpleStatementBuilder::break_statement(
+    EscriptParser::BreakStatementContext* ctx )
+{
+  auto source_location = location_for( *ctx );
+  std::string label = ctx->IDENTIFIER() ? text( ctx->IDENTIFIER() ) : "";
+
+  return std::make_unique<BreakStatement>( source_location, std::move( label ) );
+}
+
+std::unique_ptr<ConstDeclaration> SimpleStatementBuilder::const_declaration(
+    EscriptParser::ConstStatementContext* ctx )
+{
+  auto variable_declaration = ctx->variableDeclaration();
+  auto identifier = text( variable_declaration->IDENTIFIER() );
+  auto expression_context = variable_declaration->variableDeclarationInitializer()->expression();
+  auto value = expression( expression_context );
+
+  return std::make_unique<ConstDeclaration>( location_for( *ctx ), std::move( identifier ),
+                                               std::move( value ) );
+}
+
+std::unique_ptr<ConstDeclaration> SimpleStatementBuilder::const_declaration(
+    EscriptParser::UnambiguousConstStatementContext* ctx )
+{
+  auto variable_declaration = ctx->unambiguousVariableDeclaration();
+  auto identifier = text( variable_declaration->IDENTIFIER() );
+  auto expression_context =
+      variable_declaration->unambiguousVariableDeclarationInitializer()->unambiguousExpression();
+  auto value = expression( expression_context );
+
+  return std::make_unique<ConstDeclaration>( location_for( *ctx ), std::move( identifier ),
+                                               std::move( value ) );
+}
+
 std::unique_ptr<Statement> SimpleStatementBuilder::consume_statement_result(
     std::unique_ptr<Statement> statement )
 {
   return std::make_unique<ValueConsumer>( statement->source_location, std::move( statement ) );
+}
+
+std::unique_ptr<ContinueStatement> SimpleStatementBuilder::continue_statement(
+    EscriptParser::ContinueStatementContext* ctx )
+{
+  auto source_location = location_for( *ctx );
+  std::string label = ctx->IDENTIFIER() ? text( ctx->IDENTIFIER() ) : "";
+
+  return std::make_unique<ContinueStatement>( source_location, std::move( label ) );
+}
+
+std::unique_ptr<EnumDeclaration> SimpleStatementBuilder::enum_declaration(
+    EscriptParser::EnumStatementContext* ctx )
+{
+  std::vector<std::string> names;
+  std::vector<std::unique_ptr<Expression>> expressions;
+  if ( auto enum_list = ctx->enumList() )
+  {
+    std::string last_identifier;
+    for ( auto entry : enum_list->enumListEntry() )
+    {
+      auto source_location = location_for( *entry );
+      std::string identifier = text( entry->IDENTIFIER() );
+      std::unique_ptr<Expression> value;
+      if ( auto expr_ctx = entry->expression() )
+      {
+        value = expression( expr_ctx );
+      }
+      else if ( !last_identifier.empty() )
+      {
+        // The optimizer runs later, so we don't necessarily know the value of
+        // the previous enum value.  The optimizer will sort it out.
+        auto lhs = std::make_unique<Identifier>( source_location, last_identifier );
+        auto one = std::make_unique<IntegerValue>( source_location, 1 );
+        value = std::make_unique<BinaryOperator>( source_location, std::move( lhs ), "+", TOK_ADD,
+                                                    std::move( one ) );
+      }
+      else
+      {
+        value = std::make_unique<IntegerValue>( source_location, 0 );
+      }
+      bool allow_overwrite = true;
+      auto constant = std::make_unique<ConstDeclaration>( location_for( *entry ), identifier,
+                                                            std::move( value ), allow_overwrite );
+      workspace.compiler_workspace.const_declarations.push_back( std::move( constant ) );
+
+      last_identifier = identifier;
+    }
+  }
+
+  auto source_location = location_for( *ctx );
+  std::string identifier = text( ctx->IDENTIFIER() );
+  return std::make_unique<EnumDeclaration>( source_location, std::move( identifier ),
+                                              std::move( names ), std::move( expressions ) );
+}
+
+std::unique_ptr<EnumDeclaration> SimpleStatementBuilder::enum_declaration(
+    EscriptParser::UnambiguousEnumStatementContext* ctx )
+{
+  std::vector<std::string> names;
+  std::vector<std::unique_ptr<Expression>> expressions;
+  if ( auto enum_list = ctx->unambiguousEnumList() )
+  {
+    std::string last_identifier;
+    for ( auto entry : enum_list->unambiguousEnumListEntry() )
+    {
+      auto source_location = location_for( *entry );
+      std::string identifier = text( entry->IDENTIFIER() );
+      std::unique_ptr<Expression> value;
+      if ( auto expr_ctx = entry->unambiguousExpression() )
+      {
+        value = expression( expr_ctx );
+      }
+      else if ( !last_identifier.empty() )
+      {
+        // The optimizer runs later, so we don't necessarily know the value of
+        // the previous enum value.  The optimizer will sort it out.
+        auto lhs = std::make_unique<Identifier>( source_location, last_identifier );
+        auto one = std::make_unique<IntegerValue>( source_location, 1 );
+        value = std::make_unique<BinaryOperator>( source_location, std::move( lhs ), "+", TOK_ADD,
+                                                    std::move( one ) );
+      }
+      else
+      {
+        value = std::make_unique<IntegerValue>( source_location, 0 );
+      }
+      bool allow_overwrite = true;
+      auto constant = std::make_unique<ConstDeclaration>( location_for( *entry ), identifier,
+                                                            std::move( value ), allow_overwrite );
+      workspace.compiler_workspace.const_declarations.push_back( std::move( constant ) );
+
+      last_identifier = identifier;
+    }
+  }
+
+  auto source_location = location_for( *ctx );
+  std::string identifier = text( ctx->IDENTIFIER() );
+  return std::make_unique<EnumDeclaration>( source_location, std::move( identifier ), names,
+                                              std::move( expressions ) );
 }
 
 std::unique_ptr<Expression> SimpleStatementBuilder::variable_initializer(
@@ -112,6 +261,34 @@ std::unique_ptr<Expression> SimpleStatementBuilder::variable_initializer(
     return expression( expr );
   else
     return std::unique_ptr<Expression>( new StringValue( location_for( *ctx ), "" ) );
+}
+
+std::unique_ptr<ReturnStatement> SimpleStatementBuilder::return_statement(
+    EscriptParser::ReturnStatementContext* ctx )
+{
+  auto source_location = location_for( *ctx );
+
+  std::unique_ptr<Expression> result;
+  if ( auto expression_ctx = ctx->expression() )
+    result = expression( expression_ctx );
+  else
+    result = std::unique_ptr<Expression>( new StringValue( source_location, "" ) );
+
+  return std::make_unique<ReturnStatement>( source_location, std::move( result ) );
+}
+
+std::unique_ptr<ReturnStatement> SimpleStatementBuilder::return_statement(
+    EscriptParser::UnambiguousReturnStatementContext* ctx )
+{
+  auto source_location = location_for( *ctx );
+
+  std::unique_ptr<Expression> result;
+  if ( auto expression_ctx = ctx->unambiguousExpression() )
+    result = expression( expression_ctx );
+  else
+    result = std::make_unique<StringValue>( source_location, "" );
+
+  return std::make_unique<ReturnStatement>( source_location, std::move( result ) );
 }
 
 }  // namespace Pol::Bscript::Compiler
--- pol-core/bscript/compiler/astbuilder/SimpleStatementBuilder.h
+++ pol-core/bscript/compiler/astbuilder/SimpleStatementBuilder.h
@@ -6,25 +6,53 @@
 namespace Pol::Bscript::Compiler
 {
 class Statement;
+class BreakStatement;
+class ConstDeclaration;
+class ContinueStatement;
+class EnumDeclaration;
+class ReturnStatement;
 
 class SimpleStatementBuilder : public ExpressionBuilder
 {
 public:
   SimpleStatementBuilder( const SourceFileIdentifier&, BuilderWorkspace& );
 
+  void add_intrusive_debug_marker( antlr4::ParserRuleContext*,
+                                   std::vector<std::unique_ptr<Statement>>& );
+
   void add_var_statements( EscriptGrammar::EscriptParser::VarStatementContext*,
                            std::vector<std::unique_ptr<Statement>>& );
   void add_var_statements( EscriptGrammar::EscriptParser::UnambiguousVarStatementContext*,
                            std::vector<std::unique_ptr<Statement>>& );
 
+  std::unique_ptr<BreakStatement> break_statement(
+      EscriptGrammar::EscriptParser::BreakStatementContext* );
+
+  std::unique_ptr<ConstDeclaration> const_declaration(
+      EscriptGrammar::EscriptParser::ConstStatementContext* );
+  std::unique_ptr<ConstDeclaration> const_declaration(
+      EscriptGrammar::EscriptParser::UnambiguousConstStatementContext* );
+
   static std::unique_ptr<Statement> consume_statement_result(
       std::unique_ptr<Statement> statement );
+
+  std::unique_ptr<ContinueStatement> continue_statement(
+      EscriptGrammar::EscriptParser::ContinueStatementContext* ctx );
+
+  std::unique_ptr<EnumDeclaration> enum_declaration(
+      EscriptGrammar::EscriptParser::EnumStatementContext* );
+  std::unique_ptr<EnumDeclaration> enum_declaration(
+      EscriptGrammar::EscriptParser::UnambiguousEnumStatementContext* );
 
   std::unique_ptr<Expression> variable_initializer(
       EscriptGrammar::EscriptParser::VariableDeclarationInitializerContext* );
   std::unique_ptr<Expression> variable_initializer(
       EscriptGrammar::EscriptParser::UnambiguousVariableDeclarationInitializerContext* );
 
+  std::unique_ptr<ReturnStatement> return_statement(
+      EscriptGrammar::EscriptParser::ReturnStatementContext* );
+  std::unique_ptr<ReturnStatement> return_statement(
+      EscriptGrammar::EscriptParser::UnambiguousReturnStatementContext* );
 };
 
 }  // namespace Pol::Bscript::Compiler
