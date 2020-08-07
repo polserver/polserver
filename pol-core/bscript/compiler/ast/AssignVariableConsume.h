b1083a48b1c05ca49275b7742365439f5a694ad4
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:13 -0700

    Add ast/ AssignVariableConsume

---
+++ pol-core/bscript/compiler/ast/AssignVariableConsume.h
@@ -0,0 +1,25 @@
+#ifndef POLSERVER_ASSIGNVARIABLECONSUME_H
+#define POLSERVER_ASSIGNVARIABLECONSUME_H
+
+#include "Statement.h"
+
+namespace Pol::Bscript::Compiler
+{
+class Identifier;
+
+class AssignVariableConsume : public Statement
+{
+public:
+  AssignVariableConsume( const SourceLocation&, std::unique_ptr<Identifier> identifier,
+                         std::unique_ptr<Node> rhs );
+
+  void accept( NodeVisitor& visitor ) override;
+  void describe_to( fmt::Writer& ) const override;
+
+  Identifier& identifier();
+  Node& rhs();
+};
+
+}  // namespace Pol::Bscript::Compiler
+
+#endif  // POLSERVER_ASSIGNVARIABLECONSUME_H
