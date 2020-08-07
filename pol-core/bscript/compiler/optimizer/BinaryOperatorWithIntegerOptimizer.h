6d864dbfab731d529bf3d853ca93971324c94147
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:13 -0700

    Add optimizer/ BinaryOperatorWithIntegerOptimizer

---
+++ pol-core/bscript/compiler/optimizer/BinaryOperatorWithIntegerOptimizer.h
@@ -0,0 +1,32 @@
+#ifndef POLSERVER_BINARYOPERATORWITHINTEGEROPTIMIZER_H
+#define POLSERVER_BINARYOPERATORWITHINTEGEROPTIMIZER_H
+
+#include "compiler/ast/NodeVisitor.h"
+
+#include <memory>
+
+namespace Pol::Bscript::Compiler
+{
+class BinaryOperator;
+class Expression;
+class IntegerValue;
+class Report;
+
+class BinaryOperatorWithIntegerOptimizer : public NodeVisitor
+{
+public:
+  const IntegerValue& lhs;
+  const BinaryOperator& op;
+  Report& report;
+
+  std::unique_ptr<Expression> optimized_result;
+
+  BinaryOperatorWithIntegerOptimizer( IntegerValue& lhs, BinaryOperator&, Report& );
+
+  void visit_children( Node& parent ) override;
+  void visit_integer_value( IntegerValue& rhs ) override;
+};
+
+}  // namespace Pol::Bscript::Compiler
+
+#endif  // POLSERVER_BINARYOPERATORWITHINTEGEROPTIMIZER_H
