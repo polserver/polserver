7b8ab9fa1073c7910359cf27a69fa3dd4c8f0355
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:20 -0700

    Remainder FunctionResolver

--- pol-core/bscript/compiler/astbuilder/FunctionResolver.cpp
+++ pol-core/bscript/compiler/astbuilder/FunctionResolver.cpp
@@ -1,9 +1,12 @@
 #include "FunctionResolver.h"
 
+#include "AvailableUserFunction.h"
+#include "SourceFileProcessor.h"
 #include "compiler/Profile.h"
 #include "compiler/Report.h"
 #include "compiler/ast/Function.h"
 #include "compiler/ast/ModuleFunctionDeclaration.h"
+#include "compiler/ast/UserFunction.h"
 #include "compiler/file/SourceLocation.h"
 #include "compiler/model/FunctionLink.h"
 
@@ -18,6 +21,22 @@ const Function* FunctionResolver::find( const std::string& scoped_name )
     return ( *itr ).second;
   else
     return nullptr;
+}
+
+void FunctionResolver::register_available_user_function(
+    const SourceLocation& source_location,
+    EscriptGrammar::EscriptParser::FunctionDeclarationContext* ctx )
+{
+  register_available_user_function_parse_tree( source_location, ctx, ctx->IDENTIFIER(),
+                                               ctx->EXPORTED() );
+}
+
+void FunctionResolver::register_available_user_function(
+    const SourceLocation& source_location,
+    EscriptGrammar::EscriptParser::UnambiguousFunctionDeclarationContext* ctx )
+{
+  register_available_user_function_parse_tree( source_location, ctx, ctx->IDENTIFIER(),
+                                               ctx->EXPORTED() );
 }
 
 void FunctionResolver::register_function_link( const std::string& name,
@@ -39,6 +58,78 @@ void FunctionResolver::register_module_function( ModuleFunctionDeclaration* mf )
   resolved_functions_by_name[mf->name] = mf;
   auto scoped_name = mf->module_name + "::" + mf->name;
   resolved_functions_by_name[scoped_name] = mf;
+}
+
+void FunctionResolver::register_user_function( UserFunction* uf )
+{
+  resolved_functions_by_name[uf->name] = uf;
+}
+
+bool FunctionResolver::resolve( std::vector<AvailableUserFunction>& to_build_ast )
+{
+  for ( auto unresolved_itr = unresolved_function_links_by_name.begin();
+        unresolved_itr != unresolved_function_links_by_name.end(); )
+  {
+    const std::string& name = ( *unresolved_itr ).first;
+    const std::vector<std::shared_ptr<FunctionLink>>& function_links = ( *unresolved_itr ).second;
+
+    auto previously_resolved_itr = resolved_functions_by_name.find( name );
+    if ( previously_resolved_itr != resolved_functions_by_name.end() )
+    {
+      Function* resolved_function = ( *previously_resolved_itr ).second;
+      for ( auto& function_link : function_links )
+      {
+        function_link->link_to( resolved_function );
+      }
+
+      unresolved_itr = unresolved_function_links_by_name.erase( unresolved_itr );
+    }
+    else
+    {
+      auto available_itr = available_user_function_parse_trees.find( name );
+      if ( available_itr != available_user_function_parse_trees.end() )
+      {
+        to_build_ast.push_back( ( *available_itr ).second );
+        available_user_function_parse_trees.erase( available_itr );
+        ++unresolved_itr;
+      }
+      else
+      {
+        // complain, super complain
+        const std::shared_ptr<FunctionLink>& function_link = ( function_links )[0];
+        report.error( function_link->source_location, "User Function '", name, "' not found.\n" );
+        unresolved_itr = unresolved_function_links_by_name.erase( unresolved_itr );
+      }
+    }
+  }
+
+  return !to_build_ast.empty();
+}
+
+void FunctionResolver::register_available_user_function_parse_tree(
+    const SourceLocation& source_location, antlr4::ParserRuleContext* ctx,
+    antlr4::tree::TerminalNode* identifier, antlr4::tree::TerminalNode* exported )
+{
+  std::string name = identifier->getSymbol()->getText();
+  auto itr = available_user_function_parse_trees.find( name );
+  if ( itr != available_user_function_parse_trees.end() )
+  {
+    AvailableUserFunction& previous = ( *itr ).second;
+
+    report.error( source_location, "Function '", name, "' defined more than once.\n",
+                  "  Previous declaration: ", previous.source_location, "\n" );
+  }
+
+  auto auf = AvailableUserFunction{ source_location, ctx };
+  available_user_function_parse_trees.insert( { name, auf } );
+
+  if ( exported )
+  {
+    auto function_name = identifier->getSymbol()->getText();
+
+    // just make sure there is an entry, so that we build an AST for it
+    unresolved_function_links_by_name[function_name];
+  }
 }
 
 }  // namespace Pol::Bscript::Compiler
--- pol-core/bscript/compiler/astbuilder/FunctionResolver.h
+++ pol-core/bscript/compiler/astbuilder/FunctionResolver.h
@@ -12,10 +12,12 @@
 
 namespace Pol::Bscript::Compiler
 {
+struct AvailableUserFunction;
 class Function;
 class FunctionLink;
 class ModuleFunctionDeclaration;
 class Report;
+class UserFunction;
 
 class FunctionResolver
 {
@@ -24,11 +26,26 @@ public:
 
   const Function* find( const std::string& scoped_name );
 
+  void register_available_user_function(
+      const SourceLocation&, EscriptGrammar::EscriptParser::FunctionDeclarationContext* );
+  void register_available_user_function(
+      const SourceLocation&,
+      EscriptGrammar::EscriptParser::UnambiguousFunctionDeclarationContext* );
+
   void register_function_link( const std::string& name,
                                std::shared_ptr<FunctionLink> function_link );
   void register_module_function( ModuleFunctionDeclaration* );
+  void register_user_function( UserFunction* );
+
+  bool resolve( std::vector<AvailableUserFunction>& user_functions_to_build );
+
 
 private:
+
+  void register_available_user_function_parse_tree( const SourceLocation&,
+                                                    antlr4::ParserRuleContext*,
+                                                    antlr4::tree::TerminalNode* identifier,
+                                                    antlr4::tree::TerminalNode* exported );
 
   Report& report;
 
@@ -39,11 +56,14 @@ private:
   // using AmbiguousUserFunctionMap =
   //    ci_map<EscriptGrammar::EscriptParser::FunctionDeclarationContext*>;
 
+  using UserFunctionMap = std::map<std::string, AvailableUserFunction, Clib::ci_cmp_pred>;
+
   using FunctionMap = std::map<std::string, Function*, Clib::ci_cmp_pred>;
 
   using FunctionReferenceMap =
       std::map<std::string, std::vector<std::shared_ptr<FunctionLink>>, Clib::ci_cmp_pred>;
 
+  UserFunctionMap available_user_function_parse_trees;
   FunctionMap resolved_functions_by_name;
   FunctionReferenceMap unresolved_function_links_by_name;
 };
