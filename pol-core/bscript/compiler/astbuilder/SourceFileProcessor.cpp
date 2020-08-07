8bba54ef62ccdd6ed59a6e326b647ef7d9993007
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:20 -0700

    Remainder SourceFileProcessor

--- pol-core/bscript/compiler/astbuilder/SourceFileProcessor.cpp
+++ pol-core/bscript/compiler/astbuilder/SourceFileProcessor.cpp
@@ -3,13 +3,18 @@
 #include "../clib/fileutil.h"
 #include "../clib/logfacility.h"
 #include "../clib/timer.h"
+#include "../plib/pkg.h"
 
+#include "AvailableUserFunction.h"
 #include "BuilderWorkspace.h"
 #include "ModuleProcessor.h"
 #include "compiler/Profile.h"
 #include "compiler/Report.h"
 #include "compiler/ast/Statement.h"
 #include "compiler/ast/TopLevelStatements.h"
+#include "compiler/ast/ConstDeclaration.h"
+#include "compiler/ast/Program.h"
+#include "compiler/ast/UserFunction.h"
 #include "compiler/file/SourceFile.h"
 #include "compiler/file/SourceFileCache.h"
 #include "compiler/file/SourceFileIdentifier.h"
@@ -70,7 +75,7 @@ void SourceFileProcessor::use_module( const std::string& module_name,
   profile.parse_em_count++;
 
   Pol::Tools::HighPerfTimer visit_module_unit_timer;
-  module_unit_context->accept( this );
+  module_unit_context->accept( &module_processor );
   long long ast_elapsed = visit_module_unit_timer.ellapsed().count();
   profile.ast_em_micros.fetch_add( ast_elapsed );
 
@@ -99,10 +104,200 @@ void SourceFileProcessor::process_source( SourceFile& sf )
   }
 }
 
+void SourceFileProcessor::process_include( long long* us_counted, SourceFile& sf )
+{
+  Pol::Tools::HighPerfTimer timer0;
+  auto compilation_unit = sf.get_compilation_unit( report, source_file_identifier );
+  // INFO_PRINT << "compilation unit:\n" << compilationUnit->toStringTree( true ) << "\n";
+  long long parse_us_counted = timer0.ellapsed().count();
+  profile.parse_inc_us.fetch_add( parse_us_counted );
+  profile.parse_inc_count++;
+
+  *us_counted += parse_us_counted;
+
+  if ( report.error_count() == 0 )
+  {
+    Pol::Tools::HighPerfTimer timer;
+    compilation_unit->accept( this );
+    long long ast_us_elapsed = timer.ellapsed().count();
+    profile.ast_inc_us.fetch_add( ast_us_elapsed );
+    *us_counted += ast_us_elapsed;
+  }
+}
+
+void SourceFileProcessor::handle_include_declaration( EscriptParser::IncludeDeclarationContext* ctx,
+                                                      long long* us_counted )
+{
+  auto source_location = location_for( *ctx );
+
+  std::string include_name;
+  if ( auto string_literal = ctx->stringIdentifier()->STRING_LITERAL() )
+    include_name = tree_builder.unquote( string_literal );
+  else if ( auto identifier = ctx->stringIdentifier()->IDENTIFIER() )
+    include_name = identifier->getSymbol()->getText();
+  else
+    source_location.internal_error(
+        "Unable to include module: expected a string literal or identifier.\n" );
+
+  boost::optional<std::string> maybe_canonical_include_pathname =
+      locate_include_file( source_location, include_name );
+
+  if ( !maybe_canonical_include_pathname )
+  {
+    report.error( source_location,
+                  "Unable to include file '" + include_name + "': failed to locate.\n" );
+    return;
+  }
+
+  std::string canonical_include_pathname = maybe_canonical_include_pathname.value();
+
+  if ( workspace.source_files.count( canonical_include_pathname ) == 0 )
+  {
+    auto ident = std::make_unique<SourceFileIdentifier>(
+        workspace.compiler_workspace.referenced_source_file_identifiers.size(),
+        canonical_include_pathname );
+    auto sf = workspace.inc_cache.load( *ident, report );
+    if ( !sf )
+    {
+      report.error( source_location, "Unable to include file '", canonical_include_pathname,
+                    "': failed to load.\n" );
+      return;
+    }
+
+    SourceFileProcessor include_processor( *ident, workspace, false );
+    workspace.compiler_workspace.referenced_source_file_identifiers.push_back( std::move( ident ) );
+    workspace.source_files[sf->pathname] = sf;
+
+    include_processor.process_include( us_counted, *sf );
+  }
+}
+
+boost::optional<std::string> SourceFileProcessor::locate_include_file(
+    const SourceLocation& source_location, const std::string& include_name )
+{
+  std::string filename_part = include_name + ".inc";
+
+  // ERROR_PRINT << "handle_include_declaration modulename=" << include_name << "\n";
+
+  std::string current_file_path =
+      Pol::Bscript::Legacy::getpathof( source_location.source_file_identifier->pathname );
+  std::string filename_full = current_file_path + filename_part;
+
+  if ( filename_part[0] == ':' )
+  {
+    const Plib::Package* pkg = nullptr;
+    std::string path;
+    if ( Plib::pkgdef_split( filename_part, nullptr, &pkg, &path ) )
+    {
+      if ( pkg != nullptr )
+      {
+        filename_full = pkg->dir() + path;
+        std::string try_filename_full = pkg->dir() + "include/" + path;
+
+        if ( compilercfg.VerbosityLevel >= 10 )
+          INFO_PRINT << "Searching for " << filename_full << "\n";
+
+        if ( !Clib::FileExists( filename_full.c_str() ) )
+        {
+          if ( compilercfg.VerbosityLevel >= 10 )
+            INFO_PRINT << "Searching for " << try_filename_full << "\n";
+          if ( Clib::FileExists( try_filename_full.c_str() ) )
+          {
+            if ( compilercfg.VerbosityLevel >= 10 )
+              INFO_PRINT << "Found " << try_filename_full << "\n";
+
+            filename_full = try_filename_full;
+          }
+        }
+        else
+        {
+          if ( compilercfg.VerbosityLevel >= 10 )
+            INFO_PRINT << "Found " << filename_full << "\n";
+
+          if ( Clib::FileExists( try_filename_full.c_str() ) )
+            report.warning( source_location, "Found '", filename_full, "' and '", try_filename_full,
+                            "'! Will use first file!\n" );
+        }
+      }
+      else
+      {
+        filename_full = compilercfg.PolScriptRoot + path;
+
+        if ( compilercfg.VerbosityLevel >= 10 )
+        {
+          INFO_PRINT << "Searching for " << filename_full << "\n";
+          if ( Clib::FileExists( filename_full.c_str() ) )
+            INFO_PRINT << "Found " << filename_full << "\n";
+        }
+      }
+    }
+    else
+    {
+      // This is fatal because if we keep going, we'll likely report a bunch of errors
+      // that would just be noise, like missing functions or constants.
+      report.fatal( source_location, "Unable to read include file '" + include_name + "'\n" );
+    }
+  }
+  else
+  {
+    if ( compilercfg.VerbosityLevel >= 10 )
+      INFO_PRINT << "Searching for " << filename_full << "\n";
+
+    if ( !Clib::FileExists( filename_full.c_str() ) )
+    {
+      std::string try_filename_full = compilercfg.IncludeDirectory + filename_part;
+      if ( compilercfg.VerbosityLevel >= 10 )
+        INFO_PRINT << "Searching for " << try_filename_full << "\n";
+      if ( Clib::FileExists( try_filename_full.c_str() ) )
+      {
+        if ( compilercfg.VerbosityLevel >= 10 )
+          INFO_PRINT << "Found " << try_filename_full << "\n";
+
+        // cout << "Using " << try_filename << endl;
+        filename_full = try_filename_full;
+      }
+    }
+    else
+    {
+      if ( compilercfg.VerbosityLevel >= 10 )
+        INFO_PRINT << "Found " << filename_full << "\n";
+    }
+  }
+  // ERROR_PRINT << "filename_full is (1) " << filename_full << "\n";
+
+  if ( SourceFile::enforced_case_sensitivity_mismatch( source_location, filename_full, report ) )
+  {
+    return {};
+  }
+
+  filename_full = Clib::FullPath( filename_full.c_str() );
+  // ERROR_PRINT << "filename_full is (2) " << filename_full << "\n";
+
+  return boost::optional<std::string>( !filename_full.empty(), filename_full );
+}
+
+long long SourceFileProcessor::handle_use_declaration( EscriptParser::UseDeclarationContext* ctx )
+{
+  std::string modulename =
+      ( ctx->stringIdentifier()->STRING_LITERAL() ? ctx->stringIdentifier()->STRING_LITERAL()
+                                                  : ctx->stringIdentifier()->IDENTIFIER() )
+          ->getSymbol()
+          ->getText();
+  if ( modulename[0] == '\"' )
+    modulename = modulename.substr( 1, modulename.length() - 2 );
+  // INFO_PRINT << "  - " << modulename << "\n";
+  auto source_location = location_for( *ctx );
+  long long micros_used = 0;
+  use_module( modulename, source_location, &micros_used );
+  return micros_used;
+}
+
 antlrcpp::Any SourceFileProcessor::visitStatement( EscriptParser::StatementContext* ctx )
 {
   if ( auto constStatement = ctx->constStatement() )
   {
+    workspace.compiler_workspace.const_declarations.push_back(
+        tree_builder.const_declaration( constStatement ) );
   }
   else
   {
@@ -122,6 +317,8 @@ antlrcpp::Any SourceFileProcessor::visitUnambiguousStatement(
 {
   if ( auto constStatement = ctx->unambiguousConstStatement() )
   {
+    workspace.compiler_workspace.const_declarations.push_back(
+        tree_builder.const_declaration( constStatement ) );
   }
   else
   {
@@ -133,6 +330,76 @@ antlrcpp::Any SourceFileProcessor::visitUnambiguousStatement(
           std::move( statement ) );
     }
   }
+  return antlrcpp::Any();
+}
+
+antlrcpp::Any SourceFileProcessor::visitProgramDeclaration(
+    EscriptParser::ProgramDeclarationContext* ctx )
+{
+  //  << "top-level declaration: program\n";
+  if ( workspace.compiler_workspace.program )
+  {
+    report.error( location_for( *ctx ), "Multiple program statements.\n",
+                  "  Other declaration: ", workspace.compiler_workspace.program->source_location,
+                  "\n" );
+  }
+  else
+  {
+    workspace.compiler_workspace.program = tree_builder.program( ctx );
+  }
+  return antlrcpp::Any();
+}
+
+antlrcpp::Any SourceFileProcessor::visitUnambiguousProgramDeclaration(
+    EscriptParser::UnambiguousProgramDeclarationContext* ctx )
+{
+  //  << "top-level declaration: program\n";
+  if ( workspace.compiler_workspace.program )
+  {
+    report.error( location_for( *ctx ), "Multiple program statements.\n",
+                  "  Other declaration: ", workspace.compiler_workspace.program->source_location,
+                  "\n" );
+  }
+  else
+  {
+    workspace.compiler_workspace.program = tree_builder.program( ctx );
+  }
+  return antlrcpp::Any();
+}
+
+antlrcpp::Any SourceFileProcessor::visitIncludeDeclaration(
+    EscriptParser::IncludeDeclarationContext* ctx )
+{
+  long long us_counted = 0;
+  handle_include_declaration( ctx, &us_counted );
+  if ( is_src )
+    profile.ast_src_micros.fetch_sub( us_counted );
+  else
+    profile.ast_inc_us.fetch_sub( us_counted );
+  return antlrcpp::Any();
+}
+
+antlrcpp::Any SourceFileProcessor::visitUseDeclaration( EscriptParser::UseDeclarationContext* ctx )
+{
+  long long elapsed = handle_use_declaration( ctx );
+  if ( is_src )
+    profile.ast_src_micros.fetch_sub( elapsed );
+  else
+    profile.ast_inc_us.fetch_sub( elapsed );
+  return antlrcpp::Any();
+}
+
+antlrcpp::Any SourceFileProcessor::visitFunctionDeclaration(
+    EscriptParser::FunctionDeclarationContext* ctx )
+{
+  workspace.function_resolver.register_available_user_function( location_for( *ctx ), ctx );
+  return antlrcpp::Any();
+}
+
+antlrcpp::Any SourceFileProcessor::visitUnambiguousFunctionDeclaration(
+    EscriptParser::UnambiguousFunctionDeclarationContext* ctx )
+{
+  workspace.function_resolver.register_available_user_function( location_for( *ctx ), ctx );
   return antlrcpp::Any();
 }
 
--- pol-core/bscript/compiler/astbuilder/SourceFileProcessor.h
+++ pol-core/bscript/compiler/astbuilder/SourceFileProcessor.h
@@ -23,10 +23,30 @@ public:
   void use_module( const std::string& name, SourceLocation& including_location,
                    long long* micros_counted = nullptr );
   void process_source( SourceFile& );
+  void process_include( long long* us_counted, SourceFile& );
+
+  void handle_include_declaration( EscriptGrammar::EscriptParser::IncludeDeclarationContext*,
+                                   long long* us_spent );
+  boost::optional<std::string> locate_include_file( const SourceLocation& source_location,
+                                                    const std::string& include_name );
+
+  long long handle_use_declaration( EscriptGrammar::EscriptParser::UseDeclarationContext* );
 
   antlrcpp::Any visitStatement( EscriptGrammar::EscriptParser::StatementContext* ) override;
   antlrcpp::Any visitUnambiguousStatement(
       EscriptGrammar::EscriptParser::UnambiguousStatementContext* ) override;
+  antlrcpp::Any visitProgramDeclaration(
+      EscriptGrammar::EscriptParser::ProgramDeclarationContext* ) override;
+  antlrcpp::Any visitUnambiguousProgramDeclaration(
+      EscriptGrammar::EscriptParser::UnambiguousProgramDeclarationContext* ) override;
+  antlrcpp::Any visitIncludeDeclaration(
+      EscriptGrammar::EscriptParser::IncludeDeclarationContext* ) override;
+  antlrcpp::Any visitUseDeclaration(
+      EscriptGrammar::EscriptParser::UseDeclarationContext* ) override;
+  antlrcpp::Any visitFunctionDeclaration(
+      EscriptGrammar::EscriptParser::FunctionDeclarationContext* ) override;
+  antlrcpp::Any visitUnambiguousFunctionDeclaration(
+      EscriptGrammar::EscriptParser::UnambiguousFunctionDeclarationContext* ) override;
 
   SourceLocation location_for( antlr4::ParserRuleContext& ) const;
 
