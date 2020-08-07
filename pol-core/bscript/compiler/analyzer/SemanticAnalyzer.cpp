0b966eb591f0bd6a14728ece4130cbf5d78bf62e
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:20 -0700

    Remainder analyzer SemanticAnalyzer

--- pol-core/bscript/compiler/analyzer/SemanticAnalyzer.cpp
+++ pol-core/bscript/compiler/analyzer/SemanticAnalyzer.cpp
@@ -3,35 +3,478 @@
 #include "compiler/Report.h"
 #include "compiler/ast/TopLevelStatements.h"
 #include "compiler/model/CompilerWorkspace.h"
+#include <boost/range/adaptor/reversed.hpp>
+
+#include "FlowControlScope.h"
+#include "LocalVariableScope.h"
+#include "Scopes.h"
+#include "compiler/Report.h"
+#include "compiler/ast/Argument.h"
+#include "compiler/ast/BasicForLoop.h"
+#include "compiler/ast/BreakStatement.h"
+#include "compiler/ast/CaseDispatchGroup.h"
+#include "compiler/ast/CaseDispatchGroups.h"
+#include "compiler/ast/CaseDispatchSelectors.h"
+#include "compiler/ast/CaseStatement.h"
+#include "compiler/ast/ConstDeclaration.h"
+#include "compiler/ast/ContinueStatement.h"
+#include "compiler/ast/CstyleForLoop.h"
+#include "compiler/ast/DoWhileLoop.h"
+#include "compiler/ast/ForeachLoop.h"
+#include "compiler/ast/FunctionBody.h"
+#include "compiler/ast/FunctionCall.h"
+#include "compiler/ast/FunctionParameterDeclaration.h"
+#include "compiler/ast/FunctionParameterList.h"
+#include "compiler/ast/FunctionReference.h"
+#include "compiler/ast/GetMember.h"
+#include "compiler/ast/Identifier.h"
+#include "compiler/ast/ModuleFunctionDeclaration.h"
+#include "compiler/ast/Program.h"
+#include "compiler/ast/ProgramParameterDeclaration.h"
+#include "compiler/ast/ProgramParameterList.h"
+#include "compiler/ast/RepeatUntilLoop.h"
+#include "compiler/ast/TopLevelStatements.h"
+#include "compiler/ast/UserFunction.h"
+#include "compiler/ast/VarStatement.h"
+#include "compiler/ast/WhileLoop.h"
+#include "compiler/astbuilder/SimpleValueCloner.h"
+#include "compiler/model/CompilerWorkspace.h"
+#include "compiler/model/FunctionLink.h"
+#include "compiler/model/Variable.h"
+#include "compiler/optimizer/ConstantValidator.h"
 
 namespace Pol::Bscript::Compiler
 {
-SemanticAnalyzer::SemanticAnalyzer( Report& report )
-  : report( report )
+SemanticAnalyzer::SemanticAnalyzer( Constants& constants, Report& report )
+  : constants( constants ),
+    report( report ),
+    globals( true, report ),
+    locals( false, report ),
+    break_scopes( locals, report ),
+    continue_scopes( locals, report ),
+    scopes( locals, report )
 {
 }
 
 SemanticAnalyzer::~SemanticAnalyzer() = default;
 
-void SemanticAnalyzer::register_const_declarations( CompilerWorkspace& /*workspace*/ )
+void SemanticAnalyzer::register_const_declarations( CompilerWorkspace& workspace )
 {
+  for ( auto& constant : workspace.const_declarations )
+  {
+    workspace.constants.create( *constant );
+  }
 }
 
 void SemanticAnalyzer::analyze( CompilerWorkspace& workspace )
 {
   workspace.top_level_statements->accept( *this );
+  if ( auto& program = workspace.program )
+  {
+    program->accept( *this );
+  }
+  for ( auto& user_function : workspace.user_functions )
+  {
+    user_function->accept( *this );
+  }
+
+  workspace.global_variable_names = globals.get_names();
+}
+
+void SemanticAnalyzer::visit_basic_for_loop( BasicForLoop& node )
+{
+  if ( locals.find( node.identifier ) )
+  {
+    report.error( node, "FOR iterator '", node.identifier, "' hides a local variable.\n" );
+    return;
+  }
+
+  node.first().accept( *this );
+  node.last().accept( *this );
+
+  LocalVariableScope scope( scopes, node.debug_variables );
+  scope.create( node.identifier, false, false, node.source_location );
+  scope.create( "_" + node.identifier + "_end", false, false, node.source_location );
+
+  FlowControlScope break_scope( break_scopes, node.source_location, node.get_label(),
+                                node.break_label );
+  FlowControlScope continue_scope( continue_scopes, node.source_location, node.get_label(),
+                                   node.continue_label );
+
+  node.block().accept( *this );
+}
+
+void SemanticAnalyzer::visit_block( Block& block )
+{
+  LocalVariableScope scope( scopes, block.debug_variables );
+
+  visit_children( block );
+
+  block.locals_in_block = scope.get_block_locals();
+}
+
+void SemanticAnalyzer::visit_break_statement( BreakStatement& node )
+{
+  // INFO_PRINT << "sem an break: label = " << node.label << "\n";
+  if ( auto scope = break_scopes.find( node.label ) )
+  {
+    node.flow_control_label = scope->flow_control_label;
+    node.local_variables_to_remove = locals.count() - scope->local_variables_size;
+  }
+  else
+  {
+    if ( !node.label.empty() && break_scopes.any() )
+      report.error( node, "Label '", node.label, "' not found for break.\n" );
+    else
+      report.error( node, "Cannot break here.\n" );
+  }
+}
+
+void SemanticAnalyzer::visit_case_statement( CaseStatement& case_ast )
+{
+  // INFO_PRINT << "before:\n" << case_ast.to_string_tree() << "\n";
+  FlowControlScope break_scope( break_scopes, case_ast.source_location, case_ast.get_label(),
+                                case_ast.break_label );
+
+  visit_children( case_ast );
+  // INFO_PRINT << "after:\n" << case_ast.to_string_tree() << "\n";
+}
+
+void SemanticAnalyzer::visit_case_dispatch_group( CaseDispatchGroup& dispatch_group )
+{
+  FlowControlScope break_scope( break_scopes, dispatch_group.source_location, "",
+                                dispatch_group.break_label );
+
+  visit_children( dispatch_group );
+}
+
+
+class CaseDispatchSelectorAnalyzer : public NodeVisitor
+{
+public:
+  explicit CaseDispatchSelectorAnalyzer( Report& report ) : report( report ) {}
+
+  void visit_identifier( Identifier& identifier ) override
+  {
+    report.error( identifier, "Case selector '", identifier.name, "' is not a constant.\n" );
+  }
+
+private:
+  Report& report;
+};
+
+void SemanticAnalyzer::visit_case_dispatch_selectors( CaseDispatchSelectors& selectors )
+{
+  visit_children( selectors );
+
+  CaseDispatchSelectorAnalyzer selector_analyzer( report );
+  selectors.accept( selector_analyzer );
+}
+
+void SemanticAnalyzer::visit_continue_statement( ContinueStatement& node )
+{
+  if ( auto scope = continue_scopes.find( node.label ) )
+  {
+    node.flow_control_label = scope->flow_control_label;
+    node.local_variables_to_remove = locals.count() - scope->local_variables_size;
+  }
+  else
+  {
+    if ( !node.label.empty() && break_scopes.any() )
+      report.error( node, "Label '", node.label, "' not found for continue.\n" );
+    else
+      report.error( node, "Cannot continue here.\n" );
+  }
+}
+
+void SemanticAnalyzer::visit_cstyle_for_loop( CstyleForLoop& loop )
+{
+  visit_loop_statement( loop );
+}
+
+void SemanticAnalyzer::visit_do_while_loop( DoWhileLoop& do_while )
+{
+  visit_loop_statement( do_while );
+}
+
+void SemanticAnalyzer::visit_foreach_loop( ForeachLoop& node )
+{
+  node.expression().accept( *this );
+
+  LocalVariableScope scope( scopes, node.debug_variables );
+  scope.create( node.iterator_name, false, false, node.source_location );
+  scope.create( "_" + node.iterator_name + "_expr", false, false, node.source_location );
+  scope.create( "_" + node.iterator_name + "_iter", false, false, node.source_location );
+
+  FlowControlScope break_scope( break_scopes, node.source_location, node.get_label(),
+                                node.break_label );
+  FlowControlScope continue_scope( continue_scopes, node.source_location, node.get_label(),
+                                   node.continue_label );
+
+  node.block().accept( *this );
+}
+
+void SemanticAnalyzer::visit_function_call( FunctionCall& fc )
+{
+  // here we turn the arguments passed (which can be named or positional)
+  // into the final_arguments vector, which is just one parameter per
+  // argument, in the correct order.
+
+  typedef std::map<std::string, std::unique_ptr<Expression>> ArgumentList;
+  ArgumentList arguments_passed;
+
+  bool any_named = false;
+
+  std::vector<std::unique_ptr<Argument>> arguments = fc.take_arguments();
+  auto parameters = fc.parameters();
+
+  for ( auto& arg_unique_ptr : arguments )
+  {
+    auto& arg = *arg_unique_ptr;
+    std::string arg_name = arg.identifier;
+    if ( arg_name.empty() )
+    {
+      if ( any_named )
+      {
+        report.error( arg, "Unnamed args cannot follow named args.\n" );
+        return;
+      }
+      arg_name = parameters[arguments_passed.size()].get().name;
+    }
+    else
+    {
+      any_named = true;
+    }
+    if ( arguments_passed.find( arg_name ) != arguments_passed.end() )
+    {
+      report.error( arg, "Parameter '", arg_name, "' passed more than once.\n" );
+      return;
+    }
+
+    arguments_passed[arg_name] = arg.take_expression();
+  }
+
+  std::vector<std::unique_ptr<Node>> final_arguments;
+
+  for ( auto& param_ref : parameters )
+  {
+    FunctionParameterDeclaration& param = param_ref.get();
+    auto itr = arguments_passed.find( param.name );
+    if ( itr == arguments_passed.end() )
+    {
+      if ( auto default_value = param.default_value() )
+      {
+        SimpleValueCloner cloner( report, default_value->source_location );
+        auto final_argument = cloner.clone( *default_value );
+
+        if ( final_argument )
+        {
+          final_arguments.push_back( std::move( final_argument ) );
+        }
+        else
+        {
+          report.error( param, "Unable to create argument from default parameter.\n" );
+          return;
+        }
+      }
+      else
+      {
+        report.error( fc, "Parameter ", param.name, " was not passed, and there is no default.\n" );
+        return;
+      }
+    }
+    else
+    {
+      final_arguments.push_back( std::move( ( *itr ).second ) );
+      arguments_passed.erase( itr );
+    }
+  }
+
+  for ( auto& unused_argument : arguments_passed )
+  {
+    report.error( fc, "Parameter '", unused_argument.first, "' passed by name to '",
+                  fc.method_name, "', which takes no such parameter.");
+  }
+  if ( !arguments_passed.empty() )
+    return;
+
+  if ( arguments.size() > parameters.size() )
+  {
+    report.error( fc, "Too many arguments.  Expected ", parameters.size(), ", got ",
+                  arguments.size(), ".\n" );
+    return;
+  }
+
+  fc.children = std::move( final_arguments );
+
+  // do this afterwards, so that named parameters will not be looked up as identifiers.
+  visit_children( fc );
+}
+
+void SemanticAnalyzer::visit_function_parameter_list( FunctionParameterList& node )
+{
+  for ( auto& child : boost::adaptors::reverse( node.children ) )
+  {
+    child->accept( *this );
+  }
+}
+
+void SemanticAnalyzer::visit_function_parameter_declaration( FunctionParameterDeclaration& node )
+{
+  if ( auto default_value = node.default_value() )
+  {
+    ConstantValidator validator;
+    // By accident, 0-parameter system function calls are allowed as constant values.
+    // They are not allowed as default parameters, though.
+    if ( !validator.validate( *default_value ) || dynamic_cast<FunctionCall*>( default_value ) )
+    {
+      report.error(
+          node, "Parameter '", node.name,
+          "' has a disallowed default.  Only simple operands are allowed as default arguments.\n" );
+      // but continue, to avoid unknown identifier errors
+    }
+  }
+  if ( auto existing = locals.find( node.name ) )
+  {
+    report.error( node, "Parameter '", node.name, "' already defined.\n" );
+    return;
+  }
+
+  bool warn_if_used = node.unused;
+  bool warn_if_unused = !node.unused;
+  scopes.current_local_scope()->create( node.name, warn_if_used, warn_if_unused,
+                                        node.source_location );
+}
+
+void SemanticAnalyzer::visit_function_reference( FunctionReference& node )
+{
+  if ( !node.function_link->function() )
+  {
+    report.error( node, "User function '", node.name, "' not found" );
+  }
+}
+
+void SemanticAnalyzer::visit_get_member( GetMember& node )
+{
+  visit_children( node );
+}
+
+void SemanticAnalyzer::visit_identifier( Identifier& node )
+{
+  // node.source_location.info( "sem an" );
+  // INFO_PRINT << "sem an identifier: " << node.describe() << "\n";
+  if ( auto local = locals.find( node.name ) )
+  {
+    local->mark_used();
+    node.variable = local;
+  }
+  else if ( auto global = globals.find( node.name ) )
+  {
+    node.variable = global;
+  }
+  else
+  {
+    report.error( node, "Unknown identifier '", node.name, "'.\n" );
+    return;
+  }
+  visit_children( node );
+}
+
+void SemanticAnalyzer::visit_loop_statement( LoopStatement& loop )
+{
+  FlowControlScope continue_scope( continue_scopes, loop.source_location, loop.get_label(),
+                                   loop.continue_label );
+  FlowControlScope break_scope( break_scopes, loop.source_location, loop.get_label(),
+                                loop.break_label );
+
+  visit_children( loop );
+}
+
+void SemanticAnalyzer::visit_module_function_declaration( ModuleFunctionDeclaration& )
+{
+  // INFO_PRINT << "SemanticAnalyzer::visit_module_function_declaration: " + node.describe() +
+  // "\n";
+
+  // don't generate locals for children
+  // visit_children( node );
+}
+
+void SemanticAnalyzer::visit_program( Program& program )
+{
+  LocalVariableScope scope( scopes, program.debug_variables );
+  for ( auto& child : program.parameter_list().children )
+  {
+    child->accept( *this );
+  }
+
+  visit_children( program.body() );
+
+  program.locals_in_block = scope.get_block_locals();
+}
+
+void SemanticAnalyzer::visit_program_parameter_list( ProgramParameterList& node )
+{
+  for ( auto& child : boost::adaptors::reverse( node.children ) )
+  {
+    child->accept( *this );
+  }
+}
+
+void SemanticAnalyzer::visit_program_parameter_declaration( ProgramParameterDeclaration& node )
+{
+  if ( auto existing = locals.find( node.name ) )
+  {
+    report.error( node, "Parameter '", node.name, "' already defined.\n" );
+    return;
+  }
+
+  bool warn_if_used = node.unused;
+  bool warn_if_unused = !node.unused;
+  scopes.current_local_scope()->create( node.name, warn_if_used, warn_if_unused,
+                                        node.source_location );
+}
+
+void SemanticAnalyzer::visit_repeat_until_loop( RepeatUntilLoop& node )
+{
+  visit_loop_statement( node );
+}
+
+void SemanticAnalyzer::visit_user_function( UserFunction& node )
+{
+  LocalVariableScope scope( scopes, node.debug_variables );
+
+  visit_children( node );
 }
 
 void SemanticAnalyzer::visit_var_statement( VarStatement& node )
 {
-  if ( auto existing = globals.find( node.name ) )
+  // INFO_PRINT << "sem an var " + node.name + "\n";
+  if ( constants.find( node.name ) )
   {
-    report.error( node, "Global variable '", node.name, "' already defined.\n" );
+    report.error( node, node.name, " is already a defined constant.\n" );
     return;
   }
 
-  node.variable = globals.create( node.name, 0, false, false, node.source_location );
+  if ( auto local_scope = scopes.current_local_scope() )
+  {
+    node.variable = local_scope->create( node.name, false, false, node.source_location );
+  }
+  else
+  {
+    if ( auto existing = globals.find( node.name ) )
+    {
+      report.error( node, "Global variable '", node.name, "' already defined.\n" );
+      return;
+    }
+
+    node.variable = globals.create( node.name, 0, false, false, node.source_location );
+  }
   visit_children( node );
+}
+
+void SemanticAnalyzer::visit_while_loop( WhileLoop& node )
+{
+  visit_loop_statement( node );
 }
 
 }  // namespace Pol::Bscript::Compiler
--- pol-core/bscript/compiler/analyzer/SemanticAnalyzer.h
+++ pol-core/bscript/compiler/analyzer/SemanticAnalyzer.h
@@ -8,23 +8,66 @@
 
 #include "../clib/maputil.h"
 
+#include "FlowControlScopes.h"
+#include "Scopes.h"
+#include "Variables.h"
+
 namespace Pol::Bscript::Compiler
 {
 class CompilerWorkspace;
+class Constants;
+class LoopStatement;
+class Program;
+class ProgramParameterDeclaration;
+class ProgramParameterList;
 class Report;
+class UserFunction;
 
 class SemanticAnalyzer : public NodeVisitor
 {
 public:
-  explicit SemanticAnalyzer( Report& );
+  SemanticAnalyzer( Constants&, Report& );
 
   ~SemanticAnalyzer() override;
 
   static void register_const_declarations( CompilerWorkspace& );
   void analyze( CompilerWorkspace& );
 
+  void visit_basic_for_loop( BasicForLoop& node ) override;
+  void visit_block( Block& node ) override;
+  void visit_break_statement( BreakStatement& node ) override;
+  void visit_case_statement( CaseStatement& case_ast ) override;
+  void visit_case_dispatch_group( CaseDispatchGroup& dispatch_group ) override;
+  void visit_case_dispatch_selectors( CaseDispatchSelectors& selectors ) override;
+  void visit_continue_statement( ContinueStatement& node ) override;
+  void visit_cstyle_for_loop( CstyleForLoop& loop ) override;
+  void visit_do_while_loop( DoWhileLoop& do_while ) override;
+  void visit_foreach_loop( ForeachLoop& node ) override;
+  void visit_function_call( FunctionCall& fc ) override;
+  void visit_function_parameter_list( FunctionParameterList& node ) override;
+  void visit_function_parameter_declaration( FunctionParameterDeclaration& node ) override;
+  void visit_function_reference( FunctionReference& node ) override;
+  void visit_get_member( GetMember& node ) override;
+  void visit_identifier( Identifier& node ) override;
+  void visit_loop_statement( LoopStatement& loop );
+  void visit_module_function_declaration( ModuleFunctionDeclaration& ) override;
+  void visit_program( Program& program ) override;
+  void visit_program_parameter_list( ProgramParameterList& node ) override;
+  void visit_program_parameter_declaration( ProgramParameterDeclaration& node ) override;
+  void visit_repeat_until_loop( RepeatUntilLoop& node ) override;
+  void visit_user_function( UserFunction& node ) override;
+  void visit_var_statement( VarStatement& node ) override;
+  void visit_while_loop( WhileLoop& node ) override;
+
 private:
+  Constants& constants;
   Report& report;
+
+  Variables globals;
+  Variables locals;
+  FlowControlScopes break_scopes;
+  FlowControlScopes continue_scopes;
+  Scopes scopes;
 };
 
 }  // namespace Pol::Bscript::Compiler
