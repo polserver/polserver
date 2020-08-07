24e7b71f70847ed1bd0fc1ff803cfd815d5d6e4b
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:13 -0700

    Add analyzer/ FlowControlScope

---
+++ pol-core/bscript/compiler/analyzer/FlowControlScope.cpp
@@ -0,0 +1,27 @@
+#include "FlowControlScope.h"
+
+#include "FlowControlScopes.h"
+#include "Variables.h"
+#include "compiler/model/FlowControlLabel.h"
+
+namespace Pol::Bscript::Compiler
+{
+FlowControlScope::FlowControlScope( FlowControlScopes& flow_control_scopes,
+                                    const SourceLocation& source_location, std::string name,
+                                    std::shared_ptr<FlowControlLabel> flow_control_label )
+  : source_location( source_location ),
+    name( std::move( name ) ),
+    flow_control_label( std::move( flow_control_label ) ),
+    local_variables_size( flow_control_scopes.local_variables.count() ),
+    parent_scope( flow_control_scopes.current_unnamed_scope ),
+    flow_control_scopes( flow_control_scopes )
+{
+  flow_control_scopes.push( this );
+}
+
+FlowControlScope::~FlowControlScope()
+{
+  flow_control_scopes.pop( this );
+}
+
+}  // namespace Pol::Bscript::Compiler
