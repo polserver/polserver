ea6562fda20aae04e8416cecac159fe14c7d69e8
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:14 -0700

    Compare debug info

--- pol-core/ecompile/ECompileMain.cpp
+++ pol-core/ecompile/ECompileMain.cpp
@@ -165,6 +165,12 @@ void compile_inc( const char* path )
     throw std::runtime_error( "Error compiling file" );
 }
 
+std::vector<unsigned char> file_contents( const std::string& pathname, std::ios::openmode openmode )
+{
+  std::ifstream ifs( pathname, openmode );
+  return std::vector<unsigned char>(std::istreambuf_iterator<char>( ifs ), {} );
+}
+
 std::vector<unsigned char> binary_contents( const std::string& pathname )
 {
   std::ifstream input1( pathname, std::ios::binary );
@@ -208,9 +214,13 @@ bool compare_compiler_output( const std::string& path )
   // this is why -T and -G conflict: using the same filenames for every script
   std::string og_ecl( "og-compiler.ecl");
   std::string og_lst( "og-compiler.lst");
+  std::string og_dbg("og-compiler.dbg");
+  std::string og_dbg_txt("og-compiler.dbg.txt");
 
   std::string new_ecl( "new-compiler.ecl" );
   std::string new_lst( "new-compiler.lst" );
+  std::string new_dbg("new-compiler.dbg");
+  std::string new_dbg_txt("new-compiler.dbg.txt");
 
   og_compiler.write_ecl( og_ecl );
   og_compiler.write_listing( og_lst );
@@ -254,6 +264,44 @@ bool compare_compiler_output( const std::string& path )
       throw std::runtime_error( "Compiler output mismatch" );
     }
   }
+
+  if ( compilercfg.GenerateDebugInfo )
+  {
+    og_compiler.write_dbg( og_dbg, compilercfg.GenerateDebugTextInfo );
+    new_compiler.write_dbg( new_dbg, compilercfg.GenerateDebugTextInfo );
+
+    og_program->read_dbg_file();
+    new_program->read_dbg_file();
+
+    bool filenames_match = og_program->dbg_filenames == new_program->dbg_filenames;
+    bool filenum_match = og_program->dbg_filenum == new_program->dbg_filenum;
+    bool ins_blocks_match = og_program->dbg_ins_blocks == new_program->dbg_ins_blocks;
+    bool blocks_match = og_program->blocks == new_program->blocks;
+    bool functions_match = og_program->dbg_functions == new_program->dbg_functions;
+
+    bool dbg_matches = file_contents( og_dbg, std::ios::binary) == file_contents( new_dbg, std::ios::binary );
+    bool dbg_txt_matches = file_contents( og_dbg_txt, std::ios::in) == file_contents( new_dbg_txt, std::ios::in );
+
+    INFO_PRINT << "Not expected to match exactly:\n";
+    INFO_PRINT << "  Debug Info matches:\n";
+    INFO_PRINT << "              all: " << dbg_matches << " (not expected to match)\n";
+    INFO_PRINT << "        filenames: " << filenames_match << "\n";
+    INFO_PRINT << "     file numbers: " << filenum_match << "\n";
+    INFO_PRINT << "       ins_blocks: " << ins_blocks_match << "\n";
+    INFO_PRINT << "           blocks: " << blocks_match << "\n";
+    INFO_PRINT << "        functions: " << functions_match << "\n";
+    INFO_PRINT << "  Debug Info (text) matches: " << dbg_txt_matches << "\n";
+    INFO_PRINT << "    - " << og_dbg_txt << " (not expected to match)\n";
+    INFO_PRINT << "    - " << new_dbg_txt << " (not expected to match)\n";
+    bool enough_dbg_matches = filenum_match && blocks_match && ins_blocks_match && filenames_match;
+    if ( enough_dbg_matches )
+      ++comparison.MatchingDebugOutput;
+    else
+      ++comparison.NonMatchingDebugOutput;
+    if ( !enough_dbg_matches )
+      throw std::runtime_error( "Debug info mismatch" );
+  }
+
   return true;
 }
 
