9472944aa0baab84acf0c3a10fb89a5b831adb25
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-10 16:35:35 -0700

    Remainder CaseJumpDataBlock

--- pol-core/bscript/compiler/codegen/CaseJumpDataBlock.cpp
+++ pol-core/bscript/compiler/codegen/CaseJumpDataBlock.cpp
@@ -6,11 +6,11 @@ namespace Pol::Bscript::Compiler
 {
 void CaseJumpDataBlock::on_integer_value_jump_to( int32_t value, uint16_t address )
 {
-  auto* p = reinterpret_cast<unsigned char*>( &address );
+  auto* p = reinterpret_cast<std::byte*>( &address );
   data.push_back( p[0] );
   data.push_back( p[1] );
-  data.push_back( CASE_TYPE_LONG );
-  p = reinterpret_cast<unsigned char*>( &value );
+  data.push_back( std::byte{CASE_TYPE_LONG} );
+  p = reinterpret_cast<std::byte*>( &value );
   data.push_back( p[0] );
   data.push_back( p[1] );
   data.push_back( p[2] );
@@ -23,23 +23,24 @@ void CaseJumpDataBlock::on_string_value_jump_to( const std::string& value, uint1
   {
     throw std::runtime_error( "String expressions in CASE statements must be <= 253 characters." );
   }
-  auto* p = reinterpret_cast<unsigned char*>( &address );
+  auto* p = reinterpret_cast<std::byte*>( &address );
   data.push_back( p[0] );
   data.push_back( p[1] );
-  data.push_back( static_cast<unsigned char>( value.size() ) );
+  data.push_back( static_cast<std::byte>( value.size() ) );
 
-  data.insert( data.end(), value.begin(), value.end() );
+  data.insert( data.end(), reinterpret_cast<const std::byte*>( value.c_str() ),
+               reinterpret_cast<const std::byte*>( value.c_str() + value.size() ) );
 }
 
 void CaseJumpDataBlock::on_default_jump_to( uint16_t address )
 {
-  auto* p = reinterpret_cast<unsigned char*>( &address );
+  auto* p = reinterpret_cast<std::byte*>( &address );
   data.push_back( p[0] );
   data.push_back( p[1] );
-  data.push_back( CASE_TYPE_DEFAULT );
+  data.push_back( std::byte{CASE_TYPE_DEFAULT} );
 }
 
-const std::vector<unsigned char>& CaseJumpDataBlock::get_data() const
+const std::vector<std::byte>& CaseJumpDataBlock::get_data() const
 {
   return data;
 }
--- pol-core/bscript/compiler/codegen/CaseJumpDataBlock.h
+++ pol-core/bscript/compiler/codegen/CaseJumpDataBlock.h
@@ -14,10 +14,10 @@ public:
   void on_string_value_jump_to( const std::string& value, uint16_t address );
   void on_default_jump_to( uint16_t address );
 
-  const std::vector<unsigned char>& get_data() const;
+  const std::vector<std::byte>& get_data() const;
 
 private:
-  std::vector<unsigned char> data;
+  std::vector<std::byte> data;
 };
 
 }  // namespace Pol::Bscript::Compiler
