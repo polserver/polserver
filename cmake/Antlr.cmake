69ed013a5948e0d7693fb9f154ec010671985d83
turleypol <turley@polserver.com>
2020-08-12 18:30:20 +0200

    force antlr to always build in release node also for multiconfigs set the correct c++ runtime flag for clang on windows fixed lib path for antlr in CI also test msbuild and different modes then release

--- cmake/Antlr.cmake
+++ cmake/Antlr.cmake
@@ -6,18 +6,22 @@
 #    - This is because windows headers define a macro named ERROR,
 #      and precompiled headers pull that macro in.
 # 4. Remove in runtime/CMakeList.txt the build of "antlr4_shared"
+# 5. Change in runtime/CMakeList.txt:
+# if(WIN32) #<-- this
+#  if(WITH_STATIC_CRT)
+#    target_compile_options(antlr4_static PRIVATE "/MT$<$<CONFIG:Debug>:d>")
 
 message("* antlr")
 set(ANTLR_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/../lib/antlr")
 set(ANTLR_INSTALL_DIR "${ANTLR_SOURCE_DIR}/install")
 set(ANTLR_INCLUDE_DIR "${ANTLR_INSTALL_DIR}/include/antlr4-runtime")
 if (${linux})
-  set(ANTLR_LIB "${ANTLR_SOURCE_DIR}/dist/libantlr4-runtime.a")
+  set(ANTLR_LIB "${ANTLR_INSTALL_DIR}/lib/libantlr4-runtime.a")
 else()
-  if (${clang})
-    set(ANTLR_LIB "${ANTLR_SOURCE_DIR}/dist/antlr4-runtime.lib")
+  if ("${CMAKE_GENERATOR}" MATCHES "Ninja" AND ${clang})
+    set(ANTLR_LIB "${ANTLR_INSTALL_DIR}/lib/antlr4-runtime.lib")
   else()
-    set(ANTLR_LIB "${ANTLR_SOURCE_DIR}/dist/antlr4-runtime-static.lib")
+    set(ANTLR_LIB "${ANTLR_INSTALL_DIR}/lib/antlr4-runtime-static.lib")
   endif()
 endif()
 if (NOT EXISTS ${ANTLR_LIB})
@@ -26,11 +30,15 @@ if (NOT EXISTS ${ANTLR_LIB})
     PREFIX antlr
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${ANTLR_INSTALL_DIR} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DWITH_LIBCXX=Off
     BINARY_DIR ${ANTLR_SOURCE_DIR}/build
+    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config Release
+    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --config Release --target install
+ 
     BUILD_BYPRODUCTS ${ANTLR_LIB}
     LOG_DOWNLOAD 1
     LOG_CONFIGURE 1
     LOG_BUILD 1
     LOG_INSTALL 1
+    LOG_OUTPUT_ON_FAILURE 1
   )
 else()
   message("Antlr already build")
