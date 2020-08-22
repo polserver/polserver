658a66b1494612d00a66614fce02776b4bcdc2ee
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-21 19:53:27 -0700

    Module function declarations

--- pol-core/bscript/compiler/ast/NodeVisitor.cpp
+++ pol-core/bscript/compiler/ast/NodeVisitor.cpp
@@ -1,6 +1,9 @@
 #include "NodeVisitor.h"
 
 #include "compiler/ast/FloatValue.h"
+#include "compiler/ast/FunctionParameterDeclaration.h"
+#include "compiler/ast/FunctionParameterList.h"
+#include "compiler/ast/ModuleFunctionDeclaration.h"
 #include "compiler/ast/Node.h"
 #include "compiler/ast/StringValue.h"
 #include "compiler/ast/TopLevelStatements.h"
@@ -10,6 +13,21 @@ namespace Pol::Bscript::Compiler
 {
 
 void NodeVisitor::visit_float_value( FloatValue& node )
+{
+  visit_children( node );
+}
+
+void NodeVisitor::visit_function_parameter_declaration( FunctionParameterDeclaration& node )
+{
+  visit_children( node );
+}
+
+void NodeVisitor::visit_function_parameter_list( FunctionParameterList& node )
+{
+  visit_children( node );
+}
+
+void NodeVisitor::visit_module_function_declaration( ModuleFunctionDeclaration& node )
 {
   visit_children( node );
 }
