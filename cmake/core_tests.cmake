d145c7b73b5ccf52a7f3cfa57549c07e4a79df71
turleypol <turley@polserver.com>
2020-08-23 09:02:58 +0200

    use target binary directly, so cmake can replace it with the actual path use absolute path to testsuite disabled temporary the copy of ecompile

--- cmake/core_tests.cmake
+++ cmake/core_tests.cmake
@@ -1,3 +1,4 @@
+set(testsuite ${CMAKE_CURRENT_SOURCE_DIR}/testsuite/pol)
 
 # cleanup as initial fixture, to have at the end of the run still all testdata
 if (${CMAKE_VERSION} VERSION_LESS "3.17")
@@ -15,7 +16,7 @@ set_tests_properties(cleantestdir PROPERTIES FIXTURES_SETUP client)
 
 # generate client files, minimal distro and needed core cfgs
 add_test(NAME testenv
-  COMMAND ../bin/poltool testenv
+  COMMAND poltool testenv
     outdir=coretest
     width=192
     height=192
@@ -26,28 +27,28 @@ set_tests_properties(testenv PROPERTIES DEPENDS cleantestdir)
 set_tests_properties(testenv PROPERTIES FIXTURES_SETUP client)
 
 add_test(NAME shard_cfgfiles
-  COMMAND ${CMAKE_COMMAND} -E copy ../testsuite/pol/pol.cfg ../testsuite/pol/ecompile.cfg ../testsuite/pol/uoconvert.cfg coretest
+  COMMAND ${CMAKE_COMMAND} -E copy ${testsuite}/pol.cfg ${testsuite}/ecompile.cfg ${testsuite}/uoconvert.cfg coretest
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
 set_tests_properties(shard_cfgfiles PROPERTIES DEPENDS testenv)
 set_tests_properties(shard_cfgfiles PROPERTIES FIXTURES_SETUP shard)
 
 add_test(NAME shard_testscript
-  COMMAND ${CMAKE_COMMAND} -E copy_directory ../testsuite/pol/scripts coretest/scripts
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${testsuite}/scripts coretest/scripts
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
 set_tests_properties(shard_testscript PROPERTIES DEPENDS testenv)
 set_tests_properties(shard_testscript PROPERTIES FIXTURES_SETUP shard)
 
 add_test(NAME shard_testconfig
-  COMMAND ${CMAKE_COMMAND} -E copy_directory ../testsuite/pol/config coretest/config
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${testsuite}/config coretest/config
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
 set_tests_properties(shard_testconfig PROPERTIES DEPENDS testenv)
 set_tests_properties(shard_testconfig PROPERTIES FIXTURES_SETUP shard)
 
 add_test(NAME shard_testpkgs
-  COMMAND ${CMAKE_COMMAND} -E copy_directory ../testsuite/pol/testpkgs coretest/testpkgs
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${testsuite}/testpkgs coretest/testpkgs
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
 set_tests_properties(shard_testpkgs PROPERTIES DEPENDS testenv)
@@ -56,7 +57,7 @@ set_tests_properties(shard_testpkgs PROPERTIES FIXTURES_SETUP shard)
 # uoconvert part
 
 add_test(NAME uoconvert_map
-  COMMAND ../../bin/uoconvert map britannia 
+  COMMAND uoconvert map britannia 
     width=192 height=192 
     uodata=client
     maxtileid=0x3fff
@@ -66,7 +67,7 @@ set_tests_properties( uoconvert_map PROPERTIES FIXTURES_REQUIRED client)
 set_tests_properties( uoconvert_map PROPERTIES FIXTURES_SETUP uoconvert)
 
 add_test(NAME uoconvert_statics
-  COMMAND ../../bin/uoconvert statics britannia 
+  COMMAND uoconvert statics britannia 
     uodata=client
     maxtileid=0x3fff
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/coretest
@@ -76,7 +77,7 @@ set_tests_properties( uoconvert_statics PROPERTIES FIXTURES_REQUIRED client)
 set_tests_properties( uoconvert_statics PROPERTIES FIXTURES_SETUP uoconvert)
 
 add_test(NAME uoconvert_maptile
-  COMMAND ../../bin/uoconvert maptile britannia 
+  COMMAND uoconvert maptile britannia 
     uodata=client
     maxtileid=0x3fff
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/coretest
@@ -86,7 +87,7 @@ set_tests_properties( uoconvert_maptile PROPERTIES DEPENDS uoconvert_map)
 set_tests_properties( uoconvert_maptile PROPERTIES FIXTURES_SETUP uoconvert)
 
 add_test(NAME uoconvert_tiles
-  COMMAND ../../bin/uoconvert tiles
+  COMMAND uoconvert tiles
     uodata=client
     maxtileid=0x3fff
     outdir=config
@@ -96,7 +97,7 @@ set_tests_properties( uoconvert_tiles PROPERTIES FIXTURES_REQUIRED "client;shard
 set_tests_properties( uoconvert_tiles PROPERTIES FIXTURES_SETUP uoconvert)
 
 add_test(NAME uoconvert_landtiles
-  COMMAND ../../bin/uoconvert landtiles
+  COMMAND uoconvert landtiles
     uodata=client
     maxtileid=0x3fff
     outdir=config
@@ -106,7 +107,7 @@ set_tests_properties( uoconvert_landtiles PROPERTIES FIXTURES_REQUIRED "client;s
 set_tests_properties( uoconvert_landtiles PROPERTIES FIXTURES_SETUP uoconvert)
 
 add_test(NAME uoconvert_multis
-  COMMAND ../../bin/uoconvert multis
+  COMMAND uoconvert multis
     uodata=client
     maxtileid=0x3fff
     outdir=config
@@ -116,30 +117,23 @@ set_tests_properties( uoconvert_multis PROPERTIES FIXTURES_REQUIRED "client;shar
 set_tests_properties( uoconvert_multis PROPERTIES FIXTURES_SETUP uoconvert)
 
 #TODO: on windows the binary path gets added to the cfg path entries
-if (${windows})
-add_test(NAME shard_ecompile_copy
-  COMMAND ${CMAKE_COMMAND} -E copy ../bin/ecompile.exe coretest
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-)
-else()
-add_test(NAME shard_ecompile_copy
-  COMMAND ${CMAKE_COMMAND} -E copy ../bin/ecompile coretest
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-)
-endif()
-set_tests_properties( shard_ecompile_copy PROPERTIES FIXTURES_REQUIRED shard)
-set_tests_properties( shard_ecompile_copy PROPERTIES FIXTURES_SETUP ecompile)
+#add_test(NAME shard_ecompile_copy
+#  COMMAND ${CMAKE_COMMAND} -E copy ecompile coretest
+#  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+#)
+#set_tests_properties( shard_ecompile_copy PROPERTIES FIXTURES_REQUIRED shard)
+#set_tests_properties( shard_ecompile_copy PROPERTIES FIXTURES_SETUP ecompile)
 
 add_test(NAME shard_ecompile
-  COMMAND ../coretest/ecompile -A
+  COMMAND ecompile -A
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/coretest
 )
-set_tests_properties( shard_ecompile PROPERTIES DEPENDS shard_ecompile_copy)
+#set_tests_properties( shard_ecompile PROPERTIES DEPENDS shard_ecompile_copy)
 set_tests_properties( shard_ecompile PROPERTIES FIXTURES_REQUIRED shard)
 set_tests_properties( shard_ecompile PROPERTIES FIXTURES_SETUP ecompile)
 
 add_test(NAME shard_test
-  COMMAND ../../bin/pol
+  COMMAND pol
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/coretest
 )
 set_tests_properties( shard_test PROPERTIES FIXTURES_REQUIRED "client;shard;uoconvert;ecompile")
