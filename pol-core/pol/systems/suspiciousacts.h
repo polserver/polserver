4f3da669aab581864b49043b21928065a22b0b73
Fernando Rozenblit <rozenblit@gmail.com>
2020-08-29 09:39:18 +0200

    Refactor suspicious activity warnings and suppress B1 overflow warnings (#249)
    * Refactored the reporting of suspicious activities
    * Only show warnings about B1 packet overflow if ShowWarningGump is enabled

---
+++ pol-core/pol/systems/suspiciousacts.h
@@ -0,0 +1,31 @@
+#ifndef H_SYSTEMS_SUSPICIOUSACTS
+#define H_SYSTEMS_SUSPICIOUSACTS
+
+#include "clib/rawtypes.h"
+
+namespace Pol::Network
+{
+class Client;
+}
+
+// For now the reporting is done with static functions, so a namespace is fine. We can make this a
+// class later if needed.
+namespace Pol::SuspiciousActs
+{
+void GumpResponseWasUnexpected( Pol::Network::Client* client, u32 gumpid, u32 buttonid );
+void GumpResponseHasTooManyInts( Network::Client* client );
+void GumpResponseHasTooManyIntsOrStrings( Network::Client* client );
+void GumpResponseOverflows( Network::Client* client );
+
+void DropItemButNoneGotten( Network::Client* client, u32 item_serial );
+void DropItemOtherThanGotten( Network::Client* client, u32 dropped_item_serial,
+                              u32 gotten_item_serial );
+
+void EquipItemButNoneGotten( Network::Client* client, u32 equipped_item_serial );
+void EquipItemOtherThanGotten( Network::Client* client, u32 equipped_item_serial,
+                               u32 gotten_item_serial );
+
+void OutOfSequenceCursor( Network::Client* client );
+}  // namespace Pol::SuspiciousActs
+
+#endif  // !H_SYSTEMS_SUSPICIOUSACTS
