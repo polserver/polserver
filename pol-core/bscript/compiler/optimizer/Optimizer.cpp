b508dd24aa6739a9d899604d18c60e3afdaecfaf
Eric Swanson <eric.the.unicorn@gmail.com>
2020-09-28 06:21:42 -0700

    Build ElementAssignment and SetMember in the AST builder rather than the optimizer (#332)

--- pol-core/bscript/compiler/optimizer/Optimizer.cpp
+++ pol-core/bscript/compiler/optimizer/Optimizer.cpp
@@ -19,7 +19,6 @@
 #include "compiler/ast/ValueConsumer.h"
 #include "compiler/astbuilder/SimpleValueCloner.h"
 #include "compiler/model/CompilerWorkspace.h"
-#include "compiler/optimizer/AssignmentOptimizer.h"
 #include "compiler/optimizer/BinaryOperatorOptimizer.h"
 #include "compiler/optimizer/ConstantValidator.h"
 #include "compiler/optimizer/ReferencedFunctionGatherer.h"
@@ -98,15 +97,7 @@ void Optimizer::visit_binary_operator( BinaryOperator& binary_operator )
 {
   visit_children( binary_operator );
 
-  switch ( binary_operator.token_id )
-  {
-  case TOK_ASSIGN:
-    optimized_replacement = AssignmentOptimizer( binary_operator ).optimize();
-    break;
-  default:
-    optimized_replacement = BinaryOperatorOptimizer( binary_operator, report ).optimize();
-    break;
-  }
+  optimized_replacement = BinaryOperatorOptimizer( binary_operator, report ).optimize();
 }
 
 void Optimizer::visit_branch_selector( BranchSelector& selector )
