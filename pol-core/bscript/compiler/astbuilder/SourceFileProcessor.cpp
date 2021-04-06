74d79f5a96664f18c49519624a2ee0814e2842e1
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2021-04-06 22:04:11 +0200

    Diagnostics: Improved highlighting from parse tree (#396)
    * SemanticTokensBuilder updates
    - A single lexer token can be multiple semantic tokens (eg. multi-
    line string)
    
    * Add ScopeTree to grab local variables
    
    * Change FunctionCall::parameters to return a ptr
    This internal error was previously never triggered, but now since we no
    longer "fast fail", it can occur.
    
    * Null CompilerWorkspace checks
    A CompilerWorkspace cannot be created if the source cannot be found.
    
    * Make module load failure an error, not a fatal
    This change is needed to continue diagnostics after failing to locate a
    dependent module.
    
    * Use parse tree visitor for semantic tokens
    
    * Fix nullptr issues with failed parsing
    
    * Fix memory issue with SourceFileIdentifier

--- pol-core/bscript/compiler/astbuilder/SourceFileProcessor.cpp
+++ pol-core/bscript/compiler/astbuilder/SourceFileProcessor.cpp
@@ -60,7 +60,12 @@ void SourceFileProcessor::use_module( const std::string& module_name,
   {
     // This is fatal because if we keep going, we'll likely report a bunch of errors
     // that would just be noise, like missing module function declarations or constants.
-    report.fatal( including_location, "Unable to use module '{}'.", module_name );
+    // But in diagnostics mode, we want to continue, so...
+    report.error( including_location, "Unable to use module '{}'.", module_name );
+    // Move the failed-to-load SourceFileIdentifier to the compiler to properly
+    // own diagnostic references.
+    workspace.compiler_workspace.referenced_source_file_identifiers.push_back( std::move( ident ) );
+    return;
   }
   workspace.source_files[pathname] = sf;
 
@@ -157,6 +162,8 @@ void SourceFileProcessor::handle_include_declaration( EscriptParser::IncludeDecl
     {
       report.error( source_location, "Unable to include file '{}': failed to load.",
                     canonical_include_pathname );
+      workspace.compiler_workspace.referenced_source_file_identifiers.push_back(
+          std::move( ident ) );
       return;
     }
 
