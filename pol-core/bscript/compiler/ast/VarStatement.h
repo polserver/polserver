9484a6f4e76f8165e50aec0409f3f154aa3d1782
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2021-08-19 23:42:48 +0200

    Diagnostics: Add support for completion (#410)
    * Begin work to support completion
    
    * Address review comments

--- pol-core/bscript/compiler/ast/VarStatement.h
+++ pol-core/bscript/compiler/ast/VarStatement.h
@@ -14,11 +14,12 @@ class VarStatement : public Statement
 public:
   // We have to keep track of scope in VarStatement, as class var statements are
   // "hoisted" to the top-level statements of the script.
-  VarStatement( const SourceLocation&, std::string scope, std::string name,
-                std::unique_ptr<Expression> initializer );
-  VarStatement( const SourceLocation&, std::string scope, std::string name );
-  VarStatement( const SourceLocation&, std::string scope, std::string name,
-                bool initialize_as_empty_array );
+  VarStatement( const SourceLocation& identifier_location, const SourceLocation& var_decl_location,
+                std::string scope, std::string name, std::unique_ptr<Expression> initializer );
+  VarStatement( const SourceLocation& identifier_location, const SourceLocation& var_decl_location,
+                std::string scope, std::string name );
+  VarStatement( const SourceLocation& identifier_location, const SourceLocation& var_decl_location,
+                std::string scope, std::string name, bool initialize_as_empty_array );
 
   void accept( NodeVisitor& ) override;
   void describe_to( std::string& ) const override;
@@ -28,6 +29,7 @@ public:
   const bool initialize_as_empty_array = false;
 
   std::shared_ptr<Variable> variable;
+  const SourceLocation var_decl_location;
 };
 
 }  // namespace Pol::Bscript::Compiler
