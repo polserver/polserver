e3ba024cef0e718bd85b489446359e86bbd20a6a
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2023-03-05 21:40:03 +0100

    Enable caching of cppdap on GitHub actions

--- cmake/Cppdap.cmake
+++ cmake/Cppdap.cmake
@@ -1,6 +1,6 @@
 message("* cppdap")
 
-set (CPPDAP_VERSION_REF "59819690ec4114b01aae73b4caf22650f029ce53")
+set (CPPDAP_VERSION_REF "5981969")
 set (CPPDAP_SOURCE_DIR "${POL_EXT_LIB_DIR}/cppdap-${CPPDAP_VERSION_REF}")
 
 if (${windows})
@@ -22,9 +22,9 @@ if(NOT EXISTS ${CPPDAP_LIB})
     CMAKE_ARGS       ${CPPDAP_FLAGS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
     SOURCE_DIR       "${CPPDAP_SOURCE_DIR}"
     BUILD_IN_SOURCE  1
-    INSTALL_COMMAND  "" 
+    INSTALL_COMMAND  ""
     BUILD_BYPRODUCTS "${CPPDAP_LIB}"
-  ) 
+  )
 else()
   message("  cppdap already built")
 endif()
