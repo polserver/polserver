767efbaa004cf0c1b3d05586e2fa870d9a48d58b
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2023-03-19 16:46:59 +0100

    Use mutable socket mutex

--- pol-core/pol/network/client.h
+++ pol-core/pol/network/client.h
@@ -141,8 +141,8 @@ enum class PacketLog
 class ThreadedClient
 {
 public:
-  bool isReallyConnected();
-  bool isConnected();
+  bool isReallyConnected() const;
+  bool isConnected() const;
 
   void forceDisconnect();
   void closeConnection();
@@ -182,7 +182,7 @@ public:
   std::atomic<Core::polclock_t> last_activity_at;
   std::atomic<Core::polclock_t> last_packet_at;
 
-  std::mutex _SocketMutex;
+  mutable std::mutex _SocketMutex;
 
   enum e_recv_states
   {
@@ -260,8 +260,8 @@ public:
 
   std::string ipaddrAsString() const { return session()->ipaddrAsString(); }
 
-  bool isConnected() { return session()->isConnected(); }
-  bool isReallyConnected() { return session()->isReallyConnected(); }
+  bool isConnected() const { return session()->isConnected(); }
+  bool isReallyConnected() const { return session()->isReallyConnected(); }
   void forceDisconnect() { session()->forceDisconnect(); }
 
   Core::polclock_t last_activity_at() { return session()->last_activity_at; }
@@ -280,7 +280,7 @@ protected:
   void PreDelete();
 
 public:
-  bool isActive();
+  bool isActive() const;
   void Disconnect();
 
   void unregister();  // removes updater for vitals and takes client away from clientlist
@@ -378,14 +378,14 @@ inline void ThreadedClient::forceDisconnect()
 }
 
 // Checks whether the client is disconnected, and not only marked for disconnection
-inline bool ThreadedClient::isReallyConnected()
+inline bool ThreadedClient::isReallyConnected() const
 {
   std::lock_guard<std::mutex> lock( _SocketMutex );
   return !this->disconnect && this->csocket != INVALID_SOCKET;
 }
 
 // Checks for both planned and executed disconnections
-inline bool ThreadedClient::isConnected()
+inline bool ThreadedClient::isConnected() const
 {
   return !this->preDisconnect && this->isReallyConnected();
 }
@@ -394,7 +394,7 @@ inline bool ThreadedClient::has_delayed_packets() const
   return !myClient.movementqueue.empty();
 }
 
-inline bool Client::isActive()
+inline bool Client::isActive() const
 {
   return this->ready && this->isConnected();
 }
