2c6de7c18fba4ee068383ca10d7a9a559709e897
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2023-03-23 19:46:27 +0100

    Add even more events (#515)
    * add launch request
    
    * Add custom `processes` command
    
    * fixup loop
    
    * Refactoring to move handlers to member functions
    
    * Add VariablesRequest and ScopesRequest
    
    * use std::varaint for Handles
    
    * add pollocks
    
    * refactor Handles to new file
    
    * move custom messages to proto.h
    
    * move clientthread
    
    * some formatting / cleanup
    
    * Add missing PolLock
    
    * move dap variables creation to Handles
    
    * appobj members
    
    * rename ClientThread to DebugClientThread
    
    * rename LaunchRequest args to arg
    
    * Fix compiler error on size_t

---
+++ pol-core/pol/dap/handles.h
@@ -0,0 +1,61 @@
+#ifndef DAP_HANDLES_H
+#define DAP_HANDLES_H
+
+#include <map>
+#include <variant>
+
+#include <dap/types.h>
+
+namespace dap
+{
+struct Variable;
+}  // namespace dap
+
+namespace Pol
+{
+namespace Bscript
+{
+class BObjectRef;
+class BObjectImp;
+}  // namespace Bscript
+
+namespace Network
+{
+namespace DAP
+{
+struct GlobalReference
+{
+};
+using FrameReference = std::size_t;
+using VariableReference = Pol::Bscript::BObjectRef;
+
+using Reference = std::variant<GlobalReference, FrameReference, VariableReference>;
+
+class Handles
+{
+public:
+  static constexpr int START_HANDLE = 1000;
+  Handles();
+
+  void reset();
+
+  int create( const Reference& value );
+
+  Reference* get( int handle );
+
+  /**
+   * Takes a `BObjectRef` and sets the `type` and `value` members of the `dap::Variable. Sets the
+   * `variableReference` member for structured variables (eg. structs and arrays).
+   */
+  void add_variable_details( const Bscript::BObjectRef& objref, dap::Variable& variable );
+
+  dap::array<dap::Variable> to_variables( const Bscript::BObjectRef& objref );
+
+private:
+  int _nextHandle;
+  std::map<int, Reference> _handleMap;
+};
+}  // namespace DAP
+}  // namespace Network
+}  // namespace Pol
+#endif
