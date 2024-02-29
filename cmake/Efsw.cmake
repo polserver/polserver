50fae5bf5c7230d2d04fe8fa64512f17057e1a2b
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2024-02-29 02:20:24 +0100

    Add libefsw at 3419347

---
+++ cmake/Efsw.cmake
@@ -0,0 +1,55 @@
+message("* libefsw")
+
+set (EFSW_REPOSITORY "https://github.com/SpartanJ/efsw.git")
+set (EFSW_VERSION_REF "3419347")
+
+set (EFSW_SOURCE_DIR "${POL_EXT_LIB_DIR}/efsw-${EFSW_VERSION_REF}")
+set (EFSW_INSTALL_DIR "${EFSW_SOURCE_DIR}/build")
+
+if (${windows})
+  set (EFSW_LIB "${EFSW_INSTALL_DIR}/lib/efsw-static.lib" )
+else()
+  set (EFSW_LIB "${EFSW_INSTALL_DIR}/lib/libefsw-static.a" )
+endif()
+
+if (${windows})
+  set(EFSW_FLAGS -DCMAKE_CXX_FLAGS_RELEASE="/MT" -DCMAKE_C_FLAGS_RELEASE="/MT")
+endif()
+
+set(EFSW_ARGS ${EFSW_FLAGS}
+  -DCMAKE_INSTALL_PREFIX=${EFSW_INSTALL_DIR}
+  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+  -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+  -DCMAKE_OSX_ARCHITECTURES=${PIPED_OSX_ARCHITECTURES}
+ )
+
+if(NOT EXISTS ${EFSW_LIB})
+  ExternalProject_Add(libefsw_ext
+    GIT_REPOSITORY   ${EFSW_REPOSITORY}
+    GIT_TAG          ${EFSW_VERSION_REF}
+    GIT_SHALLOW      TRUE
+    PREFIX           efsw
+    LIST_SEPARATOR |
+    CMAKE_ARGS       ${EFSW_ARGS}
+    SOURCE_DIR       "${EFSW_SOURCE_DIR}"
+    BUILD_IN_SOURCE  1
+    INSTALL_COMMAND  ${CMAKE_COMMAND} --build . --config Release --target install
+    BUILD_BYPRODUCTS ${EFSW_LIB}
+    EXCLUDE_FROM_ALL 1
+  )
+else()
+  message("  - already built")
+endif()
+
+add_library(libefsw STATIC IMPORTED)
+set_target_properties(libefsw PROPERTIES
+  IMPORTED_LOCATION ${EFSW_LIB}
+  IMPORTED_IMPLIB ${EFSW_LIB}
+  INTERFACE_INCLUDE_DIRECTORIES ${EFSW_INSTALL_DIR}/include
+  FOLDER 3rdParty
+)
+if(NOT EXISTS ${EFSW_LIB})
+  file(MAKE_DIRECTORY ${EFSW_INSTALL_DIR}/include) #directory has to exist during configure
+  add_dependencies(libefsw libefsw_ext)
+endif()
