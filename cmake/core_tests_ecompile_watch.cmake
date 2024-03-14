344b949accf7cbdce027175a5e2f0ed7f412eb7a
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2024-03-14 16:09:07 +0100

    Proper Python file naming

--- cmake/core_tests_ecompile_watch.cmake
+++ cmake/core_tests_ecompile_watch.cmake
@@ -1,6 +1,6 @@
 find_package(Python3 COMPONENTS Interpreter REQUIRED QUIET)
 execute_process(
-  COMMAND ${Python3_EXECUTABLE} ${testdir}/watch-tests.py ${ecompile}
+  COMMAND ${Python3_EXECUTABLE} ${testdir}/ecompile_watch_test.py ${ecompile}
   COMMAND_ECHO STDOUT
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   RESULT_VARIABLE res
