8e66a6600fff899e174f9c9f6495547d3b800cbb
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2024-07-19 10:38:42 +0200

    add sqlitecpp test

--- pol-core/pol/module/sqlitemod.cpp
+++ pol-core/pol/module/sqlitemod.cpp
@@ -1,5 +1,11 @@
 #include "sqlitemod.h"
+#include "bscript/berror.h"
 #include "bscript/executor.h"
+#include "clib/fileutil.h"
+#include "clib/logfacility.h"
+
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <SQLiteCpp/VariadicBind.h>
 
 #include <module_defs/sqlite.h>
 
@@ -14,6 +20,32 @@ SQLiteExecutorModule::SQLiteExecutorModule( Bscript::Executor& exec )
 
 BObjectImp* SQLiteExecutorModule::mf_Sqlite3_Test()
 {
-  return new BLong( 123 );
+  int nb = -1;
+
+  try
+  {
+    SQLite::Database db( "transaction.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE );
+
+    db.exec( "DROP TABLE IF EXISTS test" );
+
+    // Begin transaction
+    SQLite::Transaction transaction( db );
+
+    db.exec( "CREATE TABLE test (id INTEGER PRIMARY KEY, value TEXT)" );
+
+    nb = db.exec( "INSERT INTO test VALUES (NULL, \"test\")" );
+
+    // Commit transaction
+    transaction.commit();
+  }
+  catch ( std::exception& e )
+  {
+    return new BError( e.what() );
+  }
+  
+  // Delete file... for now :)
+  Clib::RemoveFile( "transaction.db3" );
+  
+  return new BLong( nb );
 }
 }  // namespace Pol::Module
