2ea0329676beafdb18655a246986405c1c02e75c
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2024-10-16 20:47:01 +0200

    Diagnostics: Improve Compiler for diagnostics (#389)
    * Allow console or diagnostics reporting
    
    * Make SourceLocation track ranges
    
    * Introduce ScopeTree
    
    * Implement ScopeTree finding for variables
    
    * Implement DiagnosticReporter
    
    * Introduce Compiler::analyze
    
    * Use strings in Report instead of chars
    
    * Remove newline endings from reporter calls
    
    * Introduce SourceFileLoader
    
    * Introduce SemanticTokensBuilder
    
    * Add -fPIC to antlr
    
    * Introduce Report[er]::clear
    
    * Allow workspace module building

--- pol-core/bscript/compiler/astbuilder/CompilerWorkspaceBuilder.h
+++ pol-core/bscript/compiler/astbuilder/CompilerWorkspaceBuilder.h
@@ -15,18 +15,21 @@ class ModuleFunctionDeclaration;
 class Profile;
 class Report;
 class SourceFileCache;
+class SourceFileLoader;
 
 class CompilerWorkspaceBuilder
 {
 public:
-  CompilerWorkspaceBuilder( SourceFileCache& em_cache, SourceFileCache& inc_cache, Profile&,
-                            Report& );
+  CompilerWorkspaceBuilder( SourceFileLoader& source_loader, SourceFileCache& em_cache,
+                            SourceFileCache& inc_cache, Profile&, Report& );
 
   std::unique_ptr<CompilerWorkspace> build( const std::string& pathname, UserFunctionInclusion );
+  std::unique_ptr<CompilerWorkspace> build_module( const std::string& pathname );
 
 private:
   void build_referenced_user_functions( BuilderWorkspace& );
 
+  SourceFileLoader& source_loader;
   SourceFileCache& em_cache;
   SourceFileCache& inc_cache;
   Profile& profile;
