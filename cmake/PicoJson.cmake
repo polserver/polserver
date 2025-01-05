cd5b69da79d2673dea7ea8ecdf6f7f3e8e499756
turleypol <turley@polserver.com>
2025-01-05 15:00:09 +0100

    updated clang, gcc to newer version updated picojson to latest updated cmake and ninja to latest

--- cmake/PicoJson.cmake
+++ cmake/PicoJson.cmake
@@ -1,4 +1,4 @@
 add_library(libpicojson INTERFACE IMPORTED)
 set_target_properties(libpicojson PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${POL_EXT_LIB_DIR}/picojson-1.3.0"
+  INTERFACE_INCLUDE_DIRECTORIES "${POL_EXT_LIB_DIR}/picojson-111c9be"
 )
