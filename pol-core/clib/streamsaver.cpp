c3e982b7b50f034d56faf9efa900ef3c8c355348
turleypol <turley@polserver.com>
2025-01-27 16:55:38 +0100

    optimized away dynamic allocations during formatting by using buffer

--- pol-core/clib/streamsaver.cpp
+++ pol-core/clib/streamsaver.cpp
@@ -12,19 +12,22 @@ StreamWriter::StreamWriter( std::ostream& stream ) : _stream( stream ) {}
 
 StreamWriter::~StreamWriter() noexcept( false )
 {
-  _stream.flush();
+  flush_file();
 }
 
 void StreamWriter::open_fstream( const std::string& filepath, std::ofstream& s )
 {
-  _buf.reset( new char[1024 * 1024] );
+  //  _buf.reset( new char[1024 * 1024] );  // 1mb buffer
   s.exceptions( std::ios_base::failbit | std::ios_base::badbit );
   s.open( filepath, std::ios::out | std::ios::trunc );
-  s.rdbuf()->pubsetbuf( _buf.get(), 1024 * 1024 );
+  //  s.rdbuf()->pubsetbuf( _buf.get(), 1024 * 1024 );
 }
 
 void StreamWriter::flush_file()
 {
+  if ( _mbuff.size() )
+    _stream << std::string_view{ _mbuff.data(), _mbuff.size() };
+  _mbuff.clear();
   _stream.flush();
 }
 
--- pol-core/clib/streamsaver.h
+++ pol-core/clib/streamsaver.h
@@ -23,37 +23,58 @@ public:
   void add( Str&& key, T&& value )
   {
     if constexpr ( !std::is_same<std::decay_t<T>, bool>::value )  // force bool to write as 0/1
-      fmt::print( _stream, "\t{}\t{}\n", key, value );
+    {
+      fmt::format_to( std::back_inserter( _mbuff ), "\t{}\t{}\n", key, value );
+    }
     else
-      fmt::print( _stream, "\t{}\t{:d}\n", key, value );
+      fmt::format_to( std::back_inserter( _mbuff ), "\t{}\t{:d}\n", key, value );
   }
   template <typename Str, typename... Args>
   void comment( Str&& format, Args&&... args )
   {
-    _stream << "# ";
+    static const std::string_view prefix{ "# " };
+    _mbuff.append( prefix.data(), prefix.data() + prefix.size() );
     if constexpr ( sizeof...( args ) == 0 )
-      _stream << format;
+    {
+      const std::string_view strv{ format };
+      _mbuff.append( strv.data(), strv.data() + strv.size() );
+    }
     else
-      fmt::print( _stream, format, args... );
-    _stream << '\n';
+      fmt::format_to( std::back_inserter( _mbuff ), format, args... );
+    _mbuff.push_back( '\n' );
   }
   template <typename Str>
   void begin( Str&& key )
   {
-    fmt::print( _stream, "{}\n{{\n", key );
+    fmt::format_to( std::back_inserter( _mbuff ), "{}\n{{\n", key );
   }
   template <typename Str, typename StrValue>
   void begin( Str&& key, StrValue&& value )
   {
-    fmt::print( _stream, "{} {}\n{{\n", key, value );
+    fmt::format_to( std::back_inserter( _mbuff ), "{} {}\n{{\n", key, value );
   }
-  void end() { _stream << "}\n\n"; }
+  void end()
+  {
+    static const std::string_view endv{ "}\n\n" };
+    _mbuff.append( endv.data(), endv.data() + endv.size() );
+    if ( _mbuff.size() > 5000 )
+    {
+      _stream << std::string_view{ _mbuff.data(), _mbuff.size() };
+      _mbuff.clear();
+    }
+  }
   void open_fstream( const std::string& filepath, std::ofstream& s );
   void flush_file();
 
 protected:
   std::ostream& _stream;
-  std::unique_ptr<char[]> _buf;
+  // formatting creates a temp buffer
+  // to prevent this format into this buffer and when full write to disk, clear of the buffer keeps
+  // the capacity
+  fmt::basic_memory_buffer<char, 5000> _mbuff;
+  // extra buffer for ofstream to be not bound to io speed during write, I think not feasible since
+  // buffer has to be quite huge to gain performance
+  //  std::unique_ptr<char[]> _buf;
 };
 
 }  // namespace Pol::Clib
