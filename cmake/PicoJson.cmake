16badeb75b4ca075a8d7340153b18bec161b4af7
turleypol <turley@polserver.com>
2025-06-25 17:18:36 +0200

    download Curl, Kaitai, PicoJson, Utf8, ZLib from github updated versions

--- cmake/PicoJson.cmake
+++ cmake/PicoJson.cmake
@@ -1,4 +1,33 @@
+set(PICO_REPO "https://github.com/kazuho/picojson")
+set(PICO_TAG "111c9be")
+
+set(PICO_SOURCE_DIR "${POL_EXT_LIB_DIR}/picojson-${PICO_TAG}")
+
+message("* libpicojson")
 add_library(libpicojson INTERFACE IMPORTED)
+
+if (NOT EXISTS "${PICO_SOURCE_DIR}")
+  ExternalProject_Add(libpicojson_ext
+    GIT_REPOSITORY   ${PICO_REPO}
+    GIT_TAG          ${PICO_TAG}
+    SOURCE_DIR  ${PICO_SOURCE_DIR}/picojson
+    PREFIX pico
+    
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+
+    LOG_DOWNLOAD 1
+    LOG_CONFIGURE 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
+    LOG_OUTPUT_ON_FAILURE 1
+    EXCLUDE_FROM_ALL 1
+  )
+else()
+  message("  - already downloaded")
+endif()
 set_target_properties(libpicojson PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${POL_EXT_LIB_DIR}/picojson-111c9be"
+  INTERFACE_INCLUDE_DIRECTORIES "${PICO_SOURCE_DIR}"
 )
+add_dependencies(libpicojson libpicojson_ext)
--- cmake/UTF8.cmake
+++ cmake/UTF8.cmake
@@ -1,4 +1,37 @@
+message("* libutf8cpp")
+
+set(UTF_REPO "https://github.com/nemtrif/utfcpp")
+set(UTF_TAG "v4.0.6")
+set(UTF_SOURCE_DIR "${POL_EXT_LIB_DIR}/utf8-${UTF_TAG}")
+set(UTF_INSTALL_DIR "${UTF_SOURCE_DIR}/install")
+
+if (NOT EXISTS "${UTF_INSTALL_DIR}")
+  ExternalProject_Add(libutf8_ext
+    GIT_REPOSITORY   ${UTF_REPO}
+    GIT_TAG          ${UTF_TAG}
+    GIT_SHALLOW      TRUE
+    SOURCE_DIR  ${UTF_SOURCE_DIR}
+    PREFIX utf
+    LIST_SEPARATOR |
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${UTF_INSTALL_DIR}
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config Release
+    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --config Release --target install
+
+    LOG_DOWNLOAD 1
+    LOG_CONFIGURE 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
+    LOG_OUTPUT_ON_FAILURE 1
+    EXCLUDE_FROM_ALL 1
+  )
+else()
+  message("  - already built")
+endif()
+file(MAKE_DIRECTORY ${UTF_INSTALL_DIR}/include) #directory has to exist during configure
+
 add_library(libutf8 INTERFACE IMPORTED)
 set_target_properties(libutf8 PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${POL_EXT_LIB_DIR}/utf8-2.3.4"
+  INTERFACE_INCLUDE_DIRECTORIES "${UTF_INSTALL_DIR}/include"
 )
+add_dependencies(libutf8 libutf8_ext)
