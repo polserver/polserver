682711c2d23f3380693c4348cc2b9b169510936d
turleypol <turley@polserver.com>
2025-07-29 14:00:58 +0200

    fix warning

--- pol-core/clib/strutil.cpp
+++ pol-core/clib/strutil.cpp
@@ -57,7 +57,9 @@ void splitnamevalue( const std::string& istr, std::string& propname, std::string
 
 void decodequotedstring( std::string& str )
 {
-  std::string tmp{};
+  if ( str.empty() )
+    return;
+  std::string tmp;
   tmp.swap( str );
   const char* s = tmp.c_str();
   str.reserve( tmp.size() );
