52f4137b56c693bc9268241b5471a0a51ac16db9
turleypol <turley@polserver.com>
2025-09-15 21:22:29 +0200

    Ci test summary (#816)
    * added parser for shardoutput which outputs markdown
    used to post a summary of all tests with output if failure
    
    * use ctest temp file as source for the summary
    
    * fixed yml syntax
    
    * silly mistakes...
    
    * break something on purpose
    
    * added to every yml
    
    * print failed tests also outside of the table
    break one test on purpose
    
    * fixed variables
    
    * use codeblock
    
    * in codeblocks actual newline needs to be used
    
    * fixed formatting?
    
    * fixed on purpose testerror
    
    * setup and cleanup ecl have no functions

---
+++ testsuite/pol/ci_summary.py
@@ -0,0 +1,93 @@
+import re
+import os
+import sys
+
+path="log/pol.log"
+if (len(sys.argv)>1):
+    path=sys.argv[1]
+if not os.path.exists(path):
+    print("no testrun")
+    sys.exit(0)
+with open(path, "r") as file:
+    content = file.readlines()
+lines=""
+tests=0
+fails=0
+output="|pkg|script|function|result|duration|output|\n"
+output+="|-|-|-|-|-|-|\n"
+res=[]
+class Test:
+    def __init__(self,name):
+        self.name=name
+        self.dur=""
+        self.result = ":white_check_mark:"
+        self.output=""
+        self.sub=[]
+
+curpkg=None
+curscript=None
+curfunc=None
+for line in content:
+    line = line.rstrip()
+    m=re.search(r"(\w+) \(testpkgs/(\w+)/\)", line)
+    if m is not None:
+        res.append(Test(m.group(2)))
+        curpkg=res[-1]
+        continue
+    m=re.search(r"\s+Calling (\w+).ecl", line)
+    if m is not None:
+        curpkg.sub.append(Test(m.group(1)))
+        curscript = curpkg.sub[-1]
+        if curscript.name in ("setup","cleanup"):
+            curfunc = curscript
+        continue
+    m=re.search(r"\s+Calling (\w+)\.\.", line)
+    if m is not None:
+        curscript.sub.append(Test(m.group(1)))
+        curfunc = curscript.sub[-1]
+        lines=""
+        continue
+    m=re.search(r"\s+failed: (.*)", line)
+    if m is not None:
+        curfunc.result = ":x:"
+        msg = m.group(1).removesuffix('\x1b[0m')
+        curfunc.output = f"{lines}failed: {msg}"
+        curfunc.dur = "-"
+        curscript.result = ":x:"
+        curpkg.result = ":x:"
+        fails+=1
+    else: 
+        m=re.search(r"\s+\.\.(.*)ms", line)
+        if m is not None:
+            dur = m.group(1)+"ms"
+            if not curfunc.dur:
+                curfunc.dur = dur
+            elif not curscript.dur:
+                curscript.dur = dur
+            elif not curpkg.dur:
+                curpkg.dur = dur
+    if m is not None:
+        tests+=1
+        lines=""
+    else:
+        lines+=line.strip()+"<br/>"
+
+failed_output =""
+for r in res:
+    output+=f"|{r.name}| | |{r.result}|{r.dur}|{r.output}|\n"
+    for s in r.sub:
+        output+=f"| |{s.name}| |{s.result}|{s.dur}|{s.output}|\n"
+        for f in s.sub:
+            if f.name=="cleanup":
+                continue
+            output+=f"| | |{f.name}|{f.result}|{f.dur}|{f.output}|\n"
+            if f.output:
+                msg=f.output.replace("<br/>","\n")
+                failed_output+=f"**{r.name}/{s.name} {f.name}**\n```\n{msg}\n```\n\n"
+print(f"<details><summary>{fails} tests failed out of {tests}</summary>")
+print("")
+print(output)
+print("</details>")
+print("")
+if failed_output:
+    print(failed_output)
