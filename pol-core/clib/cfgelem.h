b2674c1faa0532b4e6d473f4de2aae5faf6975ea
Fernando Rozenblit <rozenblit@gmail.com>
2025-11-03 18:58:16 +0100

    Add error if a config file property has curly braces on its name

--- pol-core/clib/cfgelem.h
+++ pol-core/clib/cfgelem.h
@@ -25,7 +25,6 @@ protected:
   std::string name_;
   std::string value_;
   friend class ConfigElem;
-  friend class VectorConfigElem;
 };
 
 class ConfigSource;
@@ -117,69 +116,6 @@ protected:
   Props properties;
 };
 
-class VectorConfigElem : public ConfigElemBase
-{
-public:
-  VectorConfigElem();
-  virtual ~VectorConfigElem();
-
-  friend class ConfigFile;
-  bool type_is( const char* name ) const;
-  const char* type() const;
-  const char* rest() const;
-
-  bool has_prop( const char* propname ) const;
-
-  std::string remove_string( const char* propname );
-  std::string remove_string( const char* propname, const char* dflt );
-
-  unsigned short remove_ushort( const char* propname );
-  unsigned short remove_ushort( const char* propname, unsigned short dflt );
-
-  int remove_int( const char* propname );
-  int remove_int( const char* propname, int dflt );
-
-  unsigned int remove_ulong( const char* propname );
-  unsigned int remove_ulong( const char* propname, unsigned int dflt );
-
-  bool remove_bool( const char* propname );
-  bool remove_bool( const char* propname, bool dflt );
-
-  float remove_float( const char* propname, float dflt );
-  double remove_double( const char* propname, double dflt );
-
-  void clear_prop( const char* propname );
-
-  bool remove_first_prop( std::string* propname, std::string* value );
-  bool remove_prop( const char* propname, std::string* value );
-  bool remove_prop( const char* propname, unsigned int* plong );
-  bool remove_prop( const char* propname, unsigned short* pushort );
-
-  bool read_prop( const char* propname, std::string* value ) const;
-  std::string read_string( const char* propname ) const;
-  std::string read_string( const char* propname, const char* dflt ) const;
-
-
-  void add_prop( std::string propname, std::string propval );
-  void add_prop( std::string propname, unsigned int lval );
-  void add_prop( std::string propname, unsigned short sval );
-
-  [[noreturn]] void throw_error( const std::string& errmsg ) const;
-  void warn( const std::string& errmsg ) const;
-
-  void set_rest( const char* newrest );
-  void set_source( const ConfigElem& elem );
-
-protected:
-  [[noreturn]] void prop_not_found( const char* propname ) const;
-  std::string type_;
-  std::string rest_;
-
-  typedef std::vector<ConfigProperty*> Props;
-  Props properties;
-
-  ConfigSource* source_;
-};
 }  // namespace Clib
 }  // namespace Pol
 #endif
