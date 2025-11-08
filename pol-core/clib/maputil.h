68718767bf786777fdd18343d986ca2fbbefc0ae
turleypol <turley@polserver.com>
2025-11-08 18:52:16 +0100

    flyweightstring support made it insensitive again next missing header

--- pol-core/clib/maputil.h
+++ pol-core/clib/maputil.h
@@ -16,21 +16,49 @@ namespace Pol::Clib
 template <typename T>
 concept StdString = std::same_as<T, std::string> || std::same_as<T, std::string_view>;
 
+template <typename T>
+concept FlyWeightString = requires( T x ) {
+  { x.get() } -> std::same_as<const std::string&>;
+};
+
 struct ci_cmp_pred
 {
   using is_transparent = void;
+  inline static const auto icomp = []( char x, char y )
+  { return std::tolower( x ) < std::tolower( y ); };
 
   bool operator()( StdString auto const& x1, StdString auto const& x2 ) const
   {
-    return std::ranges::lexicographical_compare( x1, x2 );
+    return std::ranges::lexicographical_compare( x1, x2, icomp );
   }
-  bool operator()( const std::string& x1, const char* x2 ) const
+  bool operator()( StdString auto const& x1, const char* x2 ) const
   {
-    return std::ranges::lexicographical_compare( x1, std::string_view{ x2 } );
+    return std::ranges::lexicographical_compare( x1, std::string_view{ x2 }, icomp );
   }
-  bool operator()( const char* x1, const std::string& x2 ) const
+  bool operator()( const char* x1, StdString auto const& x2 ) const
   {
-    return std::ranges::lexicographical_compare( std::string_view{ x1 }, x2 );
+    return std::ranges::lexicographical_compare( std::string_view{ x1 }, x2, icomp );
+  }
+
+  bool operator()( FlyWeightString auto const& x1, FlyWeightString auto const& x2 ) const
+  {
+    return std::ranges::lexicographical_compare( x1.get(), x2.get(), icomp );
+  }
+  bool operator()( FlyWeightString auto const& x1, StdString auto const& x2 ) const
+  {
+    return std::ranges::lexicographical_compare( x1.get(), x2, icomp );
+  }
+  bool operator()( StdString auto const& x1, FlyWeightString auto const& x2 ) const
+  {
+    return std::ranges::lexicographical_compare( x1, x2.get(), icomp );
+  }
+  bool operator()( FlyWeightString auto const& x1, const char* x2 ) const
+  {
+    return std::ranges::lexicographical_compare( x1.get(), std::string_view{ x2 }, icomp );
+  }
+  bool operator()( const char* x1, FlyWeightString auto const& x2 ) const
+  {
+    return std::ranges::lexicographical_compare( std::string_view{ x1 }, x2.get(), icomp );
   }
 };
 }  // namespace Pol::Clib
