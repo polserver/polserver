7c89b3b9595eecc80c47b81cc7944e9af0cd0090
turleypol <turley@polserver.com>
2026-01-18 10:04:45 +0100

    check if suggestions work

--- pol-core/clib/binaryfile.cpp
+++ pol-core/clib/binaryfile.cpp
@@ -17,7 +17,7 @@
 
 namespace Pol::Clib
 {
-BinaryFile::BinaryFile() = default;
+BinaryFile::BinaryFile() {};
 
 BinaryFile::BinaryFile( const std::string& filename, std::ios::openmode mode ) : _filename( "" )
 {
