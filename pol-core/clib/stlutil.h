18494d7269078f684da9d774d71e79ca7ea78607
turleypol <turley@polserver.com>
2026-05-17 20:34:27 +0200

    improve memory size estimations for container

--- pol-core/clib/stlutil.h
+++ pol-core/clib/stlutil.h
@@ -86,47 +86,134 @@ constexpr size_t arsize( T ( & )[N] )
 // highly implementation specific, so just a rough guess
 namespace
 {
+// std::map / std::multimap -- rb-tree based
 template <typename M>
 size_t _mapimp( const M& container )
 {
   using K = typename M::key_type;
   using V = typename M::mapped_type;
-  // use string capacity if key&value or just key is string
+  using Pair = std::pair<const K, V>;
+  constexpr size_t map_node_overhead = 32;  // 3x void* + color
+#ifdef _WIN32
+  constexpr size_t overhead = sizeof( container ) + 32;  // 16+sentinal
+#else
+  constexpr size_t overhead = sizeof( container );  // 48
+#endif
+  size_t size = overhead + ( ( map_node_overhead + sizeof( Pair ) ) * container.size() );
+
   if constexpr ( std::is_same_v<K, std::string> && std::is_same_v<V, std::string> )
   {
-    size_t size = ( ( sizeof( void* ) * 3 + 1 ) / 2 ) * container.size();
     for ( const auto& p : container )
       size += p.first.capacity() + p.second.capacity();
-    return size;
   }
   else if constexpr ( std::is_same_v<K, std::string> )
   {
-    size_t size = ( sizeof( V ) + ( sizeof( void* ) * 3 + 1 ) / 2 ) * container.size();
     for ( const auto& p : container )
       size += p.first.capacity();
-    return size;
   }
-  else
-    return ( sizeof( K ) + sizeof( V ) + ( sizeof( void* ) * 3 + 1 ) / 2 ) * container.size();
+  else if constexpr ( std::is_same_v<V, std::string> )
+  {
+    for ( const auto& p : container )
+      size += p.second.capacity();
+  }
+  return size;
 }
-template <typename M, class Func>
+
+template <typename M, typename Func>
 size_t _mapimp( const M& container, Func f )
 {
   using K = typename M::key_type;
+  using Pair = std::pair<const K, typename M::mapped_type>;
+  constexpr size_t map_node_overhead = 32;  // 3 void* + color
+#ifdef _WIN32
+  constexpr size_t overhead = sizeof( container ) + 32;  // 16+sentinal
+#else
+  constexpr size_t overhead = sizeof( container );  // 48
+#endif
+
+  size_t size = overhead + ( ( map_node_overhead + sizeof( Pair ) ) * container.size() );
+
   if constexpr ( std::is_same_v<K, std::string> )
   {
-    size_t size = ( ( sizeof( void* ) * 3 + 1 ) / 2 ) * container.size();
     for ( const auto& p : container )
       size += p.first.capacity() + f( p.second );
-    return size;
   }
   else
   {
-    size_t size = ( sizeof( K ) + ( sizeof( void* ) * 3 + 1 ) / 2 ) * container.size();
     for ( const auto& p : container )
       size += f( p.second );
-    return size;
   }
+  return size;
+}
+
+// std::unordered_map -- hash table, singly-linked nodes + bucket array
+template <typename M>
+size_t _unordered_mapimp( const M& container )
+{
+  using K = typename M::key_type;
+  using V = typename M::mapped_type;
+  using Pair = std::pair<const K, V>;
+
+  // bucket array overhead
+  // sizeof(container) = libstdc++ 56 MSVC 64
+  size_t size = sizeof( container ) + container.bucket_count() * sizeof( void* );
+  size += container.size() * ( sizeof( void* ) + sizeof( Pair ) );
+
+  if constexpr ( std::is_same_v<K, std::string> && std::is_same_v<V, std::string> )
+  {
+    for ( const auto& p : container )
+      size += p.first.capacity() + p.second.capacity();
+  }
+  else if constexpr ( std::is_same_v<K, std::string> )
+  {
+    for ( const auto& p : container )
+      size += p.first.capacity();
+  }
+  else if constexpr ( std::is_same_v<V, std::string> )
+  {
+    for ( const auto& p : container )
+      size += p.second.capacity();
+  }
+  return size;
+}
+
+template <typename S>
+size_t _unordered_setimp( const S& container )
+{
+  using T = typename S::value_type;
+#ifdef _WIN32
+  // MSVC: doubly-linked list internally
+  constexpr size_t unordered_set_node_overhead = 16;
+#else
+  // libstdc++: singly-linked list
+  constexpr size_t unordered_set_node_overhead = 8;
+#endif
+  // sizeof(container) = libstdc++ 56 MSVC 64
+  size_t size = sizeof( container ) + container.bucket_count() * sizeof( void* );
+  size += container.size() * ( unordered_set_node_overhead + sizeof( T ) );
+
+  if constexpr ( std::is_same_v<T, std::string> )
+  {
+    for ( const auto& t : container )
+      size += t.capacity();
+  }
+  return size;
+}
+
+template <typename D>
+size_t _dequeimp( const D& container )
+{
+  using T = typename D::value_type;
+#ifdef _WIN32
+  constexpr size_t block_bytes = std::max( sizeof( T ), size_t{ 16 } );
+#else
+  constexpr size_t block_bytes = std::max( sizeof( T ), size_t{ 512 } );
+#endif
+  constexpr size_t bpe = block_bytes / sizeof( T );
+  const size_t blocks = ( container.size() + bpe - 1 ) / bpe;
+  const size_t map_cost = std::max( size_t{ 8 }, blocks ) * sizeof( void* );
+  // sizeof(container) = libstdc++ 80 MSVC 40
+  return sizeof( container ) + blocks * block_bytes + map_cost;
 }
 }  // namespace
 
@@ -157,15 +244,24 @@ size_t memsize( const std::vector<T>& container, Func f )
 template <typename T>
 size_t memsize( const std::set<T>& container )
 {
+  constexpr size_t node_overhead = 32;  // 3x void* + color
+#ifdef _WIN32
+  constexpr size_t overhead = sizeof( container ) + 32;  // 16 + sentinal
+#else
+  constexpr size_t overhead = sizeof( container );  // 48
+#endif
+
   if constexpr ( std::is_same_v<T, std::string> )
   {
-    size_t size = 3 * sizeof( void* );
+    size_t size = overhead;
     for ( const auto& t : container )
-      size += t.capacity() + 3 * sizeof( void* );
+      size += node_overhead + sizeof( std::string ) + t.capacity();
     return size;
   }
   else
-    return 3 * sizeof( void* ) + container.size() * ( sizeof( T ) + 3 * sizeof( void* ) );
+  {
+    return overhead + container.size() * ( node_overhead + sizeof( T ) );
+  }
 }
 
 template <typename K, typename V, typename C>
@@ -173,25 +269,30 @@ size_t memsize( const std::map<K, V, C>& container )
 {
   return _mapimp( container );
 }
+
 template <typename K, typename V, typename C>
 size_t memsize( const std::map<const K, V, C>& container )
 {
   return _mapimp( container );
 }
-template <typename K, typename V, typename C, class Func>
+
+template <typename K, typename V, typename C, typename Func>
 size_t memsize( const std::map<K, V, C>& container, Func f )
 {
   return _mapimp( container, f );
 }
-template <typename K, typename V, typename C, class Func>
+
+template <typename K, typename V, typename C, typename Func>
 size_t memsize( const std::map<const K, V, C>& container, Func f )
 {
   return _mapimp( container, f );
 }
+
 template <typename K, typename V, typename C, class Func>
 size_t memsize_keyvalue( const std::map<K, V, C>& container, Func f )
 {
-  size_t size = ( ( sizeof( void* ) * 3 + 1 ) / 2 ) * container.size();
+  constexpr size_t map_node_overhead = 32;  // 3 void* + color
+  size_t size = map_node_overhead * container.size();
   for ( const auto& p : container )
     size += f( p.first ) + f( p.second );
   return size;
@@ -200,13 +301,13 @@ size_t memsize_keyvalue( const std::map<K, V, C>& container, Func f )
 template <typename T>
 size_t memsize( const std::unordered_set<T>& container )
 {
-  return 3 * sizeof( void* ) + container.size() * ( sizeof( T ) + 3 * sizeof( void* ) );
+  return _unordered_setimp( container );
 }
 
 template <typename K, typename V, typename C>
 size_t memsize( const std::unordered_map<K, V, C>& container )
 {
-  return _mapimp( container );
+  return _unordered_mapimp( container );
 }
 
 template <typename K, typename V, typename C>
@@ -218,20 +319,20 @@ size_t memsize( const std::multimap<K, V, C>& container )
 template <typename T>
 size_t memsize( const std::deque<T>& container )
 {
-  // roughly like a list
-  return 3 * sizeof( void* ) + container.size() * ( sizeof( T ) + 2 * sizeof( void* ) );
+  return _dequeimp( container );
 }
 
 template <typename T, typename C>
 size_t memsize( const std::queue<T, C>& container )
 {
   // assuming we use the default imp of container a deque
-  return 3 * sizeof( void* ) + container.size() * ( sizeof( T ) + 2 * sizeof( void* ) );
+  return _dequeimp( container );
 }
 
 template <typename T, typename K, typename C>
 size_t memsize( const std::priority_queue<T, K, C>& container )
 {
+  // same as vector
   return 3 * sizeof( void* ) + container.size() * sizeof( T );
 }
 
@@ -241,4 +342,3 @@ size_t memsize( const std::list<T>& container )
   return 3 * sizeof( void* ) + container.size() * ( sizeof( T ) + 2 * sizeof( void* ) );
 }
 }  // namespace Pol::Clib
-
